37541d4676747034af22e00681d0e644bd2d9f4c
Arvid Norberg <arvid@libtorrent.org>
2006-10-12 20:42:58 +0000

    fixed incorrect include of standard header with .hpp-sufix

--- include/libtorrent/kademlia/routing_table.hpp
+++ include/libtorrent/kademlia/routing_table.hpp
@@ -43,7 +43,7 @@ POSSIBILITY OF SUCH DAMAGE.
 #include <boost/utility.hpp>
 #include <boost/tuple/tuple.hpp>
 #include <boost/array.hpp>
-#include <set.hpp>
+#include <set>
 
 #include <libtorrent/kademlia/logging.hpp>
 
