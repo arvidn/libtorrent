983b953f021c3ab0c6da67ed0295731928ba6924
Arvid Norberg <arvid@libtorrent.org>
2007-03-30 20:03:18 +0000

    reverted last check-in

--- src/file.cpp
+++ src/file.cpp
@@ -204,7 +204,6 @@ namespace libtorrent
 
 		size_type read(char* buf, size_type num_bytes)
 		{
-			assert(num_bytes > 0);
 			assert(m_open_mode & mode_in);
 			assert(m_fd != -1);
 
@@ -213,7 +212,7 @@ namespace libtorrent
 #else
 			size_type ret = ::read(m_fd, buf, num_bytes);
 #endif
-			if (ret == -1 || ret == 0)
+			if (ret == -1)
 			{
 				std::stringstream msg;
 				msg << "read failed: " << strerror(errno);
