a0434eba108840fe58146a8745a143e9007f64a6
Arvid Norberg <arvid@libtorrent.org>
2007-04-12 07:02:17 +0000

    fixed timer on linux

--- include/libtorrent/time.hpp
+++ include/libtorrent/time.hpp
@@ -335,7 +335,7 @@ namespace libtorrent
 	{
 		timespec ts;
 		clock_gettime(CLOCK_MONOTONIC, &ts);
-		return ptime(boost::int64_t(ts.tv_sec) + ts.tv_nsec / 1000);
+		return ptime(boost::int64_t(ts.tv_sec) * 1000000 + ts.tv_nsec / 1000);
 	}
 
 	inline time_duration microsec(boost::int64_t s)
