6c9b61646f9457ffca7ea8fc73369636613882bc
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2007-05-11 23:41:31 +0000

    fixed a bug caused by file_pool being destructed before torrents, and torrents using the file pool in their destructors

--- branches/RC_0_12/include/libtorrent/aux_/session_impl.hpp
+++ branches/RC_0_12/include/libtorrent/aux_/session_impl.hpp
@@ -283,6 +283,14 @@ namespace libtorrent
 			io_service m_io_service;
 			asio::strand m_strand;
 
+			// the file pool that all storages in this session's
+			// torrents uses. It sets a limit on the number of
+			// open files by this session.
+			// file pool must be destructed after the torrents
+			// since they will still have references to it
+			// when they are destructed.
+			file_pool m_files;
+
 			// the bandwidth manager is responsible for
 			// handing out bandwidth to connections that
 			// asks for it, it can also throttle the
@@ -354,11 +362,6 @@ namespace libtorrent
 			// NAT or not.
 			bool m_incoming_connection;
 			
-			// the file pool that all storages in this session's
-			// torrents uses. It sets a limit on the number of
-			// open files by this session.
-			file_pool m_files;
-
 			void second_tick(asio::error_code const& e);
 			boost::posix_time::ptime m_last_tick;
 
