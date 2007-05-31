208443552f4b16ca7b245aece717931177cd0ba7
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2007-05-31 02:22:04 +0000

    fixed bug in write resume data

--- trunk/src/torrent_handle.cpp
+++ trunk/src/torrent_handle.cpp
@@ -564,9 +564,11 @@ namespace libtorrent
 				for (int j = 0; j < num_bitmask_bytes; ++j)
 				{
 					unsigned char v = 0;
-					for (int k = 0; k < 8; ++k)
+					int bits = std::min(num_blocks_per_piece - j*8, 8);
+					for (int k = 0; k < bits; ++k)
 						v |= i->info[j*8+k].finished?(1 << k):0;
 					bitmask.insert(bitmask.end(), v);
+					assert(bits == 8 || j == num_bitmask_bytes - 1);
 				}
 				piece_struct["bitmask"] = bitmask;
 
