457ef960a4e6e992cbd2c153e1969196343b1e94
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2007-06-08 23:57:22 +0000

    updated python bindings

--- trunk/bindings/python/src/session_settings.cpp
+++ trunk/bindings/python/src/session_settings.cpp
@@ -23,6 +23,20 @@ void bind_session_settings()
         .def_readwrite("peer_timeout", &session_settings::peer_timeout)
         .def_readwrite("urlseed_timeout", &session_settings::urlseed_timeout)
         .def_readwrite("urlseed_pipeline_size", &session_settings::urlseed_pipeline_size)
+        .def_readwrite("file_pool_size", &session_settings::file_pool_size)
+        .def_readwrite("allow_multiple_connections_per_ip", &session_settings::allow_multiple_connections_per_ip)
+        .def_readwrite("max_failcount", &session_settings::max_failcount)
+        .def_readwrite("min_reconnect_time", &session_settings::min_reconnect_time)
+        .def_readwrite("peer_connect_timeout", &session_settings::peer_connect_timeout)
+        .def_readwrite("ignore_limits_on_local_network", &session_settings::ignore_limits_on_local_network)
+        .def_readwrite("connection_speed", &session_settings::connection_speed)
+        .def_readwrite("send_redundant_have", &session_settings::send_redundant_have)
+        .def_readwrite("lazy_bitfields", &session_settings::lazy_bitfields)
+        .def_readwrite("inactivity_timeout", &session_settings::inactivity_timeout)
+        .def_readwrite("unchoke_interval", &session_settings::unchoke_interval)
+#ifndef TORRENT_DISABLE_DHT
+        .def_readwrite("use_dht_as_fallback", &session_settings::use_dht_as_fallback)
+#endif
         ;
     
     scope ps = class_<proxy_settings>("proxy_settings")
@@ -38,5 +52,18 @@ void bind_session_settings()
     ps.attr("socks5_pw") = (int)proxy_settings::socks5_pw;
     ps.attr("http") = (int)proxy_settings::http;
     ps.attr("http_pw") = (int)proxy_settings::http_pw;
+
+    scope pes = class_<pe_settings>("pe_settings")
+        .def_readwrite("out_enc_policy", &pe_settings::out_enc_policy)
+        .def_readwrite("in_enc_policy", &pe_settings::in_enc_policy)
+        .def_readwrite("allowed_enc_level", &pe_settings::allowed_enc_level)
+        .def_readwrite("prefer_rc4", &pe_settings::prefer_rc4)
+    ;
+
+    pes.attr("forced") = pe_settings::forced;
+    pes.attr("enabled") = pe_settings::enabled;
+    pes.attr("disabled") = pe_settings::disabled;
+    pes.attr("plaintext") = pe_settings::plaintext;
+    pes.attr("rc4") = pe_settings::rc4;
 }
 
