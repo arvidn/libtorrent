e802a74ecb6b964cc303a8d26c8713d6de77f5cb
Arvid Norberg <arvid@libtorrent.org>
2007-08-11 15:12:21 +0000

    deprecated fix for gcc (requires 4.0+)

--- include/libtorrent/config.hpp
+++ include/libtorrent/config.hpp
@@ -35,13 +35,9 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #include <boost/config.hpp>
 
-#if defined(__GNUC__)
-#define TORRENT_DEPRECATED __attribute__ ((deprecated))
-#else
-#define TORRENT_DEPRECATED
-#endif
-
 #if defined(__GNUC__) && __GNUC__ >= 4
+
+#define TORRENT_DEPRECATED __attribute__ ((deprecated))
 
 # if defined(TORRENT_BUILDING_SHARED) || defined(TORRENT_LINKING_SHARED)
 #  define TORRENT_EXPORT __attribute__ ((visibility("default")))
@@ -67,6 +63,9 @@ POSSIBILITY OF SUCH DAMAGE.
 # define TORRENT_EXPORT
 #endif
 
+#ifndef TORRENT_DEPRECATED
+#define TORRENT_DEPRECATED
+#endif
 
 #endif // TORRENT_CONFIG_HPP_INCLUDED
 
