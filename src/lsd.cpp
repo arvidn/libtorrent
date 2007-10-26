33e8b670fc546421b260fec60ff4d5e3d6fbea8a
Arvid Norberg <arvid@libtorrent.org>
2007-10-26 01:27:34 +0000

    lsd close fix

--- src/lsd.cpp
+++ src/lsd.cpp
@@ -183,6 +183,8 @@ void lsd::on_announce(udp::endpoint const& from, char* buffer
 
 void lsd::close()
 {
-	m_socket.close();
+	asio::error_code ec;
+	m_socket.close(ec);
+	m_broadcast_timer.cancel();
 }
 
