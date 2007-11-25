c18ae83146b34f67975e29f272cc6ac62c535094
Arvid Norberg <arvid@libtorrent.org>
2007-11-25 08:18:57 +0000

    added wait_for_alert() to session. If there is at least one alert in the queue, it returns the front one in the queue, if the queue is empty, it waits until a new alert is posted and returns that one, or returns 0 if the time expires first.

--- include/libtorrent/time.hpp
+++ include/libtorrent/time.hpp
@@ -98,6 +98,7 @@ namespace libtorrent
 		time_duration() {}
 		time_duration operator/(int rhs) const { return time_duration(diff / rhs); }
 		explicit time_duration(boost::int64_t d) : diff(d) {}
+		time_duration& operator-=(time_duration const& c) { diff -= c.diff; return *this; }
 		boost::int64_t diff;
 	};
 
