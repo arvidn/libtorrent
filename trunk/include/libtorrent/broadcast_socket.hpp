2a86addb17e7207cb44b9bb65e3c94e9c9c82d60
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2008-01-09 18:51:54 +0000

    broadcast socket header file update

--- trunk/include/libtorrent/broadcast_socket.hpp
+++ trunk/include/libtorrent/broadcast_socket.hpp
@@ -80,6 +80,8 @@ namespace libtorrent
 		void on_receive(socket_entry* s, asio::error_code const& ec
 			, std::size_t bytes_transferred);
 		void open_unicast_socket(io_service& ios, address const& addr);
+		void open_multicast_socket(io_service& ios, address const& addr
+			, bool loopback);
 
 		// these sockets are used to
 		// join the multicast group (on each interface)
