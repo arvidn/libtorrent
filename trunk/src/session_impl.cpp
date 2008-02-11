7f206ea8062650259af71f62f63ca13a63fe3a8a
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2008-02-11 07:18:28 +0000

    removed 'adler32' key from fastresume, since it was implemented with blocking I/O

--- trunk/src/session_impl.cpp
+++ trunk/src/session_impl.cpp
@@ -2683,21 +2683,23 @@ namespace detail
 
 					TORRENT_ASSERT(*slot_iter == p.index);
 					int slot_index = static_cast<int>(slot_iter - tmp_pieces.begin());
-					unsigned long adler
-						= torrent_ptr->filesystem().piece_crc(
-							slot_index
-							, torrent_ptr->block_size()
-							, p.info);
-
 					const entry* ad = i->find_key("adler32");
 	
-					// crc's didn't match, don't use the resume data
-					if (ad && ad->type() == entry::int_t
-						&& ad->integer() != entry::integer_type(adler))
+					if (ad && ad->type() == entry::int_t)
 					{
-						error = "checksum mismatch on piece "
-							+ boost::lexical_cast<std::string>(p.index);
-						return;
+						unsigned long adler
+							= torrent_ptr->filesystem().piece_crc(
+								slot_index
+								, torrent_ptr->block_size()
+								, p.info);
+
+						// crc's didn't match, don't use the resume data
+						if (ad->integer() != entry::integer_type(adler))
+						{
+							error = "checksum mismatch on piece "
+								+ boost::lexical_cast<std::string>(p.index);
+							return;
+						}
 					}
 
 					tmp_unfinished.push_back(p);
