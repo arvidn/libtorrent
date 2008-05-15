df2389219a7e6f393922c0a8462d725dbdc4e90c
Arvid Norberg <arvid@libtorrent.org>
2008-05-15 17:42:28 +0000

    fixed typo in torrent_info

--- src/torrent_info.cpp
+++ src/torrent_info.cpp
@@ -579,8 +579,8 @@ namespace libtorrent
 		m_comment = torrent_file.dict_find_string_value("comment.utf-8");
 		if (m_comment.empty()) m_comment = torrent_file.dict_find_string_value("comment");
 	
-		m_comment = torrent_file.dict_find_string_value("created by.utf-8");
-		if (m_comment.empty()) m_comment = torrent_file.dict_find_string_value("created by");
+		m_created_by = torrent_file.dict_find_string_value("created by.utf-8");
+		if (m_created_by.empty()) m_comment = torrent_file.dict_find_string_value("created by");
 
 		lazy_entry const* info = torrent_file.dict_find_dict("info");
 		if (info == 0)
