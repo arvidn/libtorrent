a7fb2cc96093a4d20b86f6fa81a3e153aab7bebc
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2008-05-15 16:57:35 +0000

    fixed typo

--- branches/RC_0_13/src/storage.cpp
+++ branches/RC_0_13/src/storage.cpp
@@ -499,7 +499,7 @@ namespace libtorrent
 				std::pair<iter_t, bool> ret = directories.insert((m_save_path / bp).string());
 				bp = bp.branch_path();
 			}
-#elif defined(_WIN32) && defined(UNICODE)
+#if defined(_WIN32) && defined(UNICODE)
 			try
 			{ fs::remove(safe_convert(p)); }
 			catch (std::exception& e)
@@ -516,7 +516,7 @@ namespace libtorrent
 		for (std::set<std::string>::reverse_iterator i = directories.rbegin()
 			, end(directories.rend()); i != end; ++i)
 		{
-#elif defined(_WIN32) && defined(UNICODE)
+#if defined(_WIN32) && defined(UNICODE)
 			try
 			{ fs::remove(safe_convert(*i)); }
 			catch (std::exception& e)
