ec5db43d77e1da06fb6093e4ed82142dc8c441f9
Arvid Norberg <arvid@libtorrent.org>
2008-06-03 05:21:00 +0000

    saves statistics from the last second from peer connections that are disconnected. Partly fixes #339

--- include/libtorrent/torrent.hpp
+++ include/libtorrent/torrent.hpp
@@ -160,6 +160,7 @@ namespace libtorrent
 			unfinished_pieces);
 
 		stat statistics() const { return m_stat; }
+		void add_stats(stat const& s) { m_stat += s; }
 		size_type bytes_left() const;
 		boost::tuples::tuple<size_type, size_type> bytes_done() const;
 		size_type quantized_bytes_done() const;
