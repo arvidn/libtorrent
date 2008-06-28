25dae85183ec4162b3df44e9a671d63a562621af
roman_rybalko <roman_rybalko@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2008-06-28 10:38:12 +0000

    trunk: moved to asio-1-0-0 from asio-head (get_asio & makefile fix) tests moved to "check" target

--- trunk/include/get_asio.sh
+++ trunk/include/get_asio.sh
@@ -8,6 +8,6 @@ set -e
 rm -Rf asio
 rm -Rf libtorrent/asio*
 #cvs -d :pserver:anonymous@asio.cvs.sourceforge.net:/cvsroot/asio login # uncomment this if you're the first time, use empty password
-cvs -d :pserver:anonymous@asio.cvs.sourceforge.net:/cvsroot/asio export -D 'Fri, 13 Jun 2008 14:18:47 +0400' -d asio asio/include
+cvs -d :pserver:anonymous@asio.cvs.sourceforge.net:/cvsroot/asio export -r asio-1-0-0 -d asio asio/include
 ln -s ../asio/asio libtorrent
 ln -s ../asio/asio.hpp libtorrent
