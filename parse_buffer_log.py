507d0549fc1b219d5f66af1fe168e606da227e22
Arvid Norberg <arvid@libtorrent.org>
2008-07-01 18:59:13 +0000

    added copyright notices in files missing them

--- parse_buffer_log.py
+++ parse_buffer_log.py
@@ -1,3 +1,8 @@
+#!/bin/python
+# Copyright Arvid Norberg 2008. Use, modification and distribution is
+# subject to the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 import os, sys, time
 
 lines = open(sys.argv[1], 'rb').readlines()
