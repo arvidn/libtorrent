77035aabe13befc436bb7ff62cb01a3a14115ea7
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2008-07-05 17:41:54 +0000

    made get_asio.sh script check out asio directly into the libtorrent directory. Certain tools cannot handle the symbolic links properly, such as vmware folder sharing

--- trunk/include/get_asio.sh
+++ trunk/include/get_asio.sh
@@ -5,9 +5,9 @@
 set -x
 set -e
 
-rm -Rf asio
-rm -Rf libtorrent/asio*
+rm -Rf libtorrent/asio libtorrent/asio.hpp asio
 #cvs -d :pserver:anonymous@asio.cvs.sourceforge.net:/cvsroot/asio login # uncomment this if you're the first time, use empty password
-cvs -d :pserver:anonymous@asio.cvs.sourceforge.net:/cvsroot/asio export -r asio-1-0-0 -d asio asio/include
-ln -s ../asio/asio libtorrent
-ln -s ../asio/asio.hpp libtorrent
+cvs -d :pserver:anonymous@asio.cvs.sourceforge.net:/cvsroot/asio export -r asio-1-0-0 asio/include/asio asio/include/asio.hpp 
+mv asio/include/asio libtorrent/
+mv asio/include/asio.hpp libtorrent/
+
