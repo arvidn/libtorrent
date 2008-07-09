1c45f94bda130b01c441dbabf406e9a312d0a4aa
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2008-07-09 18:01:00 +0000

    rare request optimization

--- trunk/src/peer_connection.cpp
+++ trunk/src/peer_connection.cpp
@@ -2132,6 +2132,12 @@ namespace libtorrent
 			r.length = block_size;
 
 			m_request_queue.pop_front();
+			if (t->is_seed()) continue;
+			// this can happen if a block times out, is re-requested and
+			// then arrives "unexpectedly"
+			if (t->picker().is_finished(block) || t->picker().is_downloaded(block))
+				continue;
+
 			m_download_queue.push_back(block);
 /*
 #ifdef TORRENT_VERBOSE_LOGGING
