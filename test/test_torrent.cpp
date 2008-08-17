e8d691eed9c830fa7a945add7fcee5c60ae138a9
Arvid Norberg <arvid@libtorrent.org>
2008-08-17 11:42:57 +0000

    extended the test_torrent to torrents with 0 bytes

--- test/test_torrent.cpp
+++ test/test_torrent.cpp
@@ -8,34 +8,11 @@
 #include "test.hpp"
 #include "setup_transfer.hpp"
 
-int test_main()
+using namespace libtorrent;
+
+void test_running_torrent(boost::intrusive_ptr<torrent_info> info, size_type file_size)
 {
-	using namespace libtorrent;
-
 	session ses(fingerprint("LT", 0, 1, 0, 0), std::make_pair(48130, 48140));
-
-	file_storage fs;
-	size_type file_size = 1 * 1024 * 1024 * 1024;
-	fs.add_file("test_torrent/tmp1", file_size);
-	fs.add_file("test_torrent/tmp2", file_size);
-	fs.add_file("test_torrent/tmp3", file_size);
-	libtorrent::create_torrent t(fs, 4 * 1024 * 1024);
-	t.add_tracker("http://non-existing.com/announce");
-
-	std::vector<char> piece(4 * 1024 * 1024);
-	for (int i = 0; i < int(piece.size()); ++i)
-		piece[i] = (i % 26) + 'A';
-	
-	// calculate the hash for all pieces
-	sha1_hash ph = hasher(&piece[0], piece.size()).final();
-	int num = t.num_pieces();
-	for (int i = 0; i < num; ++i)
-		t.set_hash(i, ph);
-
-	std::vector<char> tmp;
-	std::back_insert_iterator<std::vector<char> > out(tmp);
-	bencode(out, t.generate());
-	boost::intrusive_ptr<torrent_info> info(new torrent_info(&tmp[0], tmp.size()));
 
 	add_torrent_params p;
 	p.ti = info;
@@ -72,6 +49,51 @@ int test_main()
 	TEST_CHECK(st.total_wanted == file_size);
 	std::cout << "total_wanted_done: " << st.total_wanted_done << " : 0" << std::endl;
 	TEST_CHECK(st.total_wanted_done == 0);
+}
+
+int test_main()
+{
+	{
+		file_storage fs;
+		size_type file_size = 1 * 1024 * 1024 * 1024;
+		fs.add_file("test_torrent/tmp1", file_size);
+		fs.add_file("test_torrent/tmp2", file_size);
+		fs.add_file("test_torrent/tmp3", file_size);
+		libtorrent::create_torrent t(fs, 4 * 1024 * 1024);
+		t.add_tracker("http://non-existing.com/announce");
+
+		std::vector<char> piece(4 * 1024 * 1024);
+		for (int i = 0; i < int(piece.size()); ++i)
+			piece[i] = (i % 26) + 'A';
+		
+		// calculate the hash for all pieces
+		sha1_hash ph = hasher(&piece[0], piece.size()).final();
+		int num = t.num_pieces();
+		for (int i = 0; i < num; ++i)
+			t.set_hash(i, ph);
+
+		std::vector<char> tmp;
+		std::back_insert_iterator<std::vector<char> > out(tmp);
+		bencode(out, t.generate());
+		boost::intrusive_ptr<torrent_info> info(new torrent_info(&tmp[0], tmp.size()));
+
+		test_running_torrent(info, file_size);
+	}
+
+	{
+		file_storage fs;
+
+		fs.add_file("test_torrent/tmp1", 0);
+		libtorrent::create_torrent t(fs, 4 * 1024 * 1024);
+		t.add_tracker("http://non-existing.com/announce");
+
+		std::vector<char> tmp;
+		std::back_insert_iterator<std::vector<char> > out(tmp);
+		bencode(out, t.generate());
+		boost::intrusive_ptr<torrent_info> info(new torrent_info(&tmp[0], tmp.size()));
+		test_running_torrent(info, 0);
+	}
+
 	return 0;
 }
 
