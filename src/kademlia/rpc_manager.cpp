380aef220c2aaa2ea39e50b01e09e2df13f609c4
Arvid Norberg <arvid@libtorrent.org>
2008-09-15 05:05:26 +0000

    fixed DHT bug where requests that timed out would be treated as transaction_id overflows and would slow down DHT lookups considerably.

--- src/kademlia/rpc_manager.cpp
+++ src/kademlia/rpc_manager.cpp
@@ -267,7 +267,9 @@ time_duration rpc_manager::tick()
 
 	if (m_next_transaction_id == m_oldest_transaction_id) return milliseconds(timeout_ms);
 
-	std::vector<observer_ptr > timeouts;
+	std::vector<observer_ptr> timeouts;
+
+	time_duration ret = milliseconds(timeout_ms);
 
 	for (;m_next_transaction_id != m_oldest_transaction_id;
 		m_oldest_transaction_id = (m_oldest_transaction_id + 1) % max_transactions)
@@ -281,8 +283,16 @@ time_duration rpc_manager::tick()
 		time_duration diff = o->sent + milliseconds(timeout_ms) - time_now();
 		if (diff > seconds(0))
 		{
-			if (diff < seconds(1)) return seconds(1);
-			return diff;
+			if (diff < seconds(1))
+			{
+				ret = seconds(1);
+				break;
+			}
+			else
+			{
+				ret = diff;
+				break;	
+			}
 		}
 		
 		try
@@ -303,7 +313,7 @@ time_duration rpc_manager::tick()
 	// generate new requests. We need to swap, since the
 	// destrutors may add more observers to the m_aborted_transactions
 	std::vector<observer_ptr >().swap(m_aborted_transactions);
-	return milliseconds(timeout_ms);
+	return ret;
 }
 
 unsigned int rpc_manager::new_transaction_id(observer_ptr o)
