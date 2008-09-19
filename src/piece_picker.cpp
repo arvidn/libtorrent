fbec4261cd92ef27ffe0f962124266afc634a2c0
Arvid Norberg <arvid@libtorrent.org>
2008-09-19 06:46:58 +0000

    fixed assert in piece_picker

--- src/piece_picker.cpp
+++ src/piece_picker.cpp
@@ -1540,6 +1540,7 @@ namespace libtorrent
 			, end(m_downloads.end()); i != end; ++i)
 		{
 			if (!pieces[i->index]) continue;
+			if (piece_priority(i->index) == 0) continue;
 				
 			int num_blocks_in_piece = blocks_in_piece(i->index);
 			for (int j = 0; j < num_blocks_in_piece; ++j)
