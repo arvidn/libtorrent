973609caef817daa2a9c6e76000265348d13c64a
andrewresch <andrewresch@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2008-10-02 06:37:54 +0000

    update simple_client.py

--- trunk/bindings/python/simple_client.py
+++ trunk/bindings/python/simple_client.py
@@ -13,12 +13,20 @@ ses.listen_on(6881, 6891)
 e = lt.bdecode(open("test.torrent", 'rb').read())
 info = lt.torrent_info(e)
 
-h = ses.add_torrent(info, "./")
+atp = {}
+atp["ti"] = info
+atp["save_path"] = "./"
+atp["storage_mode"] = lt.storage_mode_t(1)
+atp["paused"] = False
+atp["auto_managed"] = True
+atp["duplicate_is_error"] = True
+
+h = ses.add_torrent(atp)
 
 while (not h.is_seed()):
 	s = h.status()
 
-	state_str = ['queued', 'checking', 'connecting', 'downloading metadata', \
+	state_str = ['queued', 'checking', 'downloading metadata', \
 		'downloading', 'finished', 'seeding', 'allocating']
 	print '\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
 		(s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
