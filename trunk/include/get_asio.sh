1b0fee3c18df87597815c9a51001a6da22aaf8e3
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2008-10-17 17:05:25 +0000

    updated get_asio script to download asio-1.2.0 and updated makefile to include the new asio headers

--- trunk/include/get_asio.sh
+++ trunk/include/get_asio.sh
@@ -7,7 +7,7 @@ set -e
 
 rm -Rf libtorrent/asio libtorrent/asio.hpp asio
 #cvs -d :pserver:anonymous@asio.cvs.sourceforge.net:/cvsroot/asio login # uncomment this if you're the first time, use empty password
-cvs -d :pserver:anonymous@asio.cvs.sourceforge.net:/cvsroot/asio export -r asio-1-0-0 asio/include/asio asio/include/asio.hpp 
+cvs -d :pserver:anonymous@asio.cvs.sourceforge.net:/cvsroot/asio export -r asio-1-2-0 asio/include/asio asio/include/asio.hpp 
 mv asio/include/asio libtorrent/
 mv asio/include/asio.hpp libtorrent/
 
