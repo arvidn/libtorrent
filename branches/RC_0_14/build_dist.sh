35d403dc19a5de7b5113a1687b12adeb5f75790c
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2008-11-21 04:03:59 +0000

    improved build infrastructure for the python bindings

--- branches/RC_0_14/build_dist.sh
+++ branches/RC_0_14/build_dist.sh
@@ -8,6 +8,6 @@ chmod a-x docs/*.rst docs/*.htm*
 /sw/bin/libtoolize -f
 /opt/local/bin/automake -ac
 /opt/local/bin/autoconf
-./configure --with-zlib=shipped --enable-examples=yes --enable-tests=yes --with-boost-thread=mt-1_35 --with-boost-system=mt-1_35 --with-boost-filesystem=mt-1_35 --with-boost-regex=mt-1_35 --with-boost-program-options=mt-1_35
+./configure --enable-python-binding --with-zlib=shipped --enable-examples=yes --enable-tests=yes --with-boost-python=mt-1_35 --with-boost-thread=mt-1_35 --with-boost-system=mt-1_35 --with-boost-filesystem=mt-1_35 --with-boost-regex=mt-1_35 --with-boost-program-options=mt-1_35
 make dist check
 
