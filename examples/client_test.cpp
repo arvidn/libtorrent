aced1fcac3db842d1e19f3d117ca5e5b4b9e46b0
Arvid Norberg <arvid@libtorrent.org>
2008-12-13 03:05:29 +0000

    don't show connecting peers in client_test

--- examples/client_test.cpp
+++ examples/client_test.cpp
@@ -342,7 +342,7 @@ void print_peer_info(std::ostream& out, std::vector<libtorrent::peer_info> const
 	for (std::vector<peer_info>::const_iterator i = peers.begin();
 		i != peers.end(); ++i)
 	{
-		if (i->flags & (peer_info::handshake))
+		if (i->flags & (peer_info::handshake | peer_info::connecting | peer_info::queued))
 			continue;
 
 		out.fill(' ');
