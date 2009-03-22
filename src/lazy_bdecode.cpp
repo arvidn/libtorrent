1660585a28f29a91491fa8c3668c75147a3f258b
Arvid Norberg <arvid@libtorrent.org>
2009-03-22 05:31:28 +0000

    fix to lazy_entry print function

--- src/lazy_bdecode.cpp
+++ src/lazy_bdecode.cpp
@@ -395,8 +395,8 @@ namespace libtorrent
 						&& e.list_size() < 20)
 					|| (e.list_at(0)->type() == lazy_entry::string_t
 						&& (e.list_at(0)->string_length() < 10
-							|| e.list_size() < 2))
-						&& e.list_size() < 5);
+							|| e.list_size() < 2)
+						&& e.list_size() < 5));
 				if (!one_liner) os << "\n";
 				for (int i = 0; i < e.list_size(); ++i)
 				{
