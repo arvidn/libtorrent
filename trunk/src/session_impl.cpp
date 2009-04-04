dbd4b9d4ad3a82bfc5f146b472b706515db98bc8
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2009-04-04 08:32:23 +0000

    more missing wchar_t support

--- trunk/src/session_impl.cpp
+++ trunk/src/session_impl.cpp
@@ -33,9 +33,6 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "libtorrent/pch.hpp"
 
 #include <ctime>
-#include <iostream>
-#include <fstream>
-#include <iomanip>
 #include <iterator>
 #include <algorithm>
 #include <set>
@@ -345,6 +342,7 @@ namespace aux {
 		return m_asnum_db;
 	}
 
+#ifndef BOOST_FILESYSTEM_NARROW_ONLY
 	bool session_impl::load_asnum_db(wchar_t const* file)
 	{
 		mutex_t::scoped_lock l(m_mutex);
@@ -355,14 +353,6 @@ namespace aux {
 		return m_asnum_db;
 	}
 
-	bool session_impl::load_country_db(char const* file)
-	{
-		mutex_t::scoped_lock l(m_mutex);
-		if (m_country_db) GeoIP_delete(m_country_db);
-		m_country_db = GeoIP_open(file, GEOIP_STANDARD);
-		return m_country_db;
-	}
-
 	bool session_impl::load_country_db(wchar_t const* file)
 	{
 		mutex_t::scoped_lock l(m_mutex);
@@ -370,6 +360,15 @@ namespace aux {
 		std::string utf8;
 		wchar_utf8(file, utf8);
 		m_country_db = GeoIP_open(utf8.c_str(), GEOIP_STANDARD);
+		return m_country_db;
+	}
+#endif
+
+	bool session_impl::load_country_db(char const* file)
+	{
+		mutex_t::scoped_lock l(m_mutex);
+		if (m_country_db) GeoIP_delete(m_country_db);
+		m_country_db = GeoIP_open(file, GEOIP_STANDARD);
 		return m_country_db;
 	}
 
