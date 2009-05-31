1451f0579bf84669e08f0a6c9bc0a6ee34c9ef20
Arvid Norberg <arvid@libtorrent.org>
2009-05-31 05:50:07 +0000

    fixed handling of dropped request

--- src/peer_connection.cpp
+++ src/peer_connection.cpp
@@ -1730,10 +1730,13 @@ namespace libtorrent
 		pending_block pending_b = *b;
 #endif
 
-		int block_index = b - m_download_queue.begin() - 1;
+		int block_index = b - m_download_queue.begin();
+		TORRENT_ASSERT(m_download_queue[block_index] == pending_b);
 		for (int i = 0; i < block_index; ++i)
 		{
 			pending_block& qe = m_download_queue[i];
+			TORRENT_ASSERT(m_download_queue[block_index] == pending_b);
+			TORRENT_ASSERT(i < block_index);
 
 #if defined TORRENT_VERBOSE_LOGGING || defined TORRENT_ERROR_LOGGING
 			(*m_logger) << time_now_string()
@@ -1755,11 +1758,12 @@ namespace libtorrent
 				m_download_queue.erase(m_download_queue.begin() + i);
 				--i;
 				--block_index;
+				TORRENT_ASSERT(m_download_queue[block_index] == pending_b);
 			}
 		}
 		TORRENT_ASSERT(int(m_download_queue.size()) > block_index + 1);
-		b = m_download_queue.begin() + (block_index + 1);
-		TORRENT_ASSERT(b->block == pending_b.block);
+		b = m_download_queue.begin() + block_index;
+		TORRENT_ASSERT(*b == pending_b);
 		
 		// if the block we got is already finished, then ignore it
 		if (picker.is_downloaded(block_finished))
