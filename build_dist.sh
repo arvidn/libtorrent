ab06ed7d3f322797b68cdb58e07afa0b0db4be4e
Arvid Norberg <arvid@libtorrent.org>
2009-08-02 00:23:07 +0000

    updated build_dist to work on new macports boost package

--- build_dist.sh
+++ build_dist.sh
@@ -3,11 +3,12 @@ rm -f config.cache
 rm -fr autom4te.cache
 chmod a-x docs/*.rst docs/*.htm*
 
-/opt/local/bin/autoheader-1.7
+/opt/local/bin/autoheader
 /opt/local/bin/aclocal -I m4
-/sw/bin/libtoolize -f
+/opt/local/bin/glibtool -f
 /opt/local/bin/automake -ac
 /opt/local/bin/autoconf
-./configure --enable-python-binding --with-zlib=shipped --enable-examples=yes --enable-tests=yes --with-boost-python=mt-1_35 --with-boost-thread=mt-1_35 --with-boost-system=mt-1_35 --with-boost-filesystem=mt-1_35 --with-boost-regex=mt-1_35 --with-boost-program-options=mt-1_35
+./configure --enable-python-binding --with-zlib=shipped --enable-examples=yes --enable-tests=yes
+# --with-boost-python=mt-1_35 --with-boost-thread=mt-1_35 --with-boost-system=mt-1_35 --with-boost-filesystem=mt-1_35 --with-boost-regex=mt-1_35 --with-boost-program-options=mt-1_35
 make dist check
 
