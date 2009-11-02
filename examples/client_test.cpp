e89dc60694ab818cc19b26555f6c020f4610e96f
Arvid Norberg <arvid@libtorrent.org>
2009-11-02 20:50:54 +0000

    client_test fixes

--- examples/client_test.cpp
+++ examples/client_test.cpp
@@ -574,7 +574,7 @@ void scan_dir(std::string const& dir_path
 	error_code ec;
 	for (directory i(dir_path, ec); !i.done(); i.next(ec))
 	{
-		std::string file = i.file();
+		std::string file = combine_path(dir_path, i.file());
 		if (extension(file) != ".torrent") continue;
 
 		handles_t::iterator k = handles.find(file);
@@ -586,7 +586,7 @@ void scan_dir(std::string const& dir_path
 
 		// the file has been added to the dir, start
 		// downloading it.
-		add_torrent(ses, handles, combine_path(dir_path, file), preferred_ratio, compact_mode
+		add_torrent(ses, handles, file, preferred_ratio, compact_mode
 			, save_path, true, torrent_upload_limit, torrent_download_limit);
 		valid.insert(file);
 	}
