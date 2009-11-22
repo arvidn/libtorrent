966a29fdc3c110f86ace5c53e764113345d2e1af
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2009-11-22 23:59:26 +0000

    fixed formatting string issue

--- branches/RC_0_15/src/kademlia/rpc_manager.cpp
+++ branches/RC_0_15/src/kademlia/rpc_manager.cpp
@@ -223,7 +223,7 @@ bool rpc_manager::incoming(msg const& m)
 			reply.error_code = 203; // Protocol error
 			char msg[100];
 			snprintf(msg, sizeof(msg), "reply with invaliud transaction "
-				"id, size %d", m.transaction_id.size());
+				"id, size %d", int(m.transaction_id.size()));
 			reply.addr = m.addr;
 			reply.transaction_id = "";
 			m_send(reply);
