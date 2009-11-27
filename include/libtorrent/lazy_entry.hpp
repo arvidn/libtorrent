97d9cd44bfe285dd854cc3a13e16458305de0ee5
Arvid Norberg <arvid@libtorrent.org>
2009-11-27 07:08:47 +0000

    fixed windows build

--- include/libtorrent/lazy_entry.hpp
+++ include/libtorrent/lazy_entry.hpp
@@ -53,6 +53,8 @@ namespace libtorrent
 	// return 0 = success
 	TORRENT_EXPORT int lazy_bdecode(char const* start, char const* end, lazy_entry& ret, int depth_limit = 1000);
 
+	struct lazy_dict_entry;
+
 	struct TORRENT_EXPORT lazy_entry
 	{
 		enum entry_type_t
@@ -132,13 +134,7 @@ namespace libtorrent
 		lazy_entry const* dict_find_list(char const* name) const;
 		lazy_entry const* dict_find_string(char const* name) const;
 
-		std::pair<std::string, lazy_entry const*> dict_at(int i) const
-		{
-			TORRENT_ASSERT(m_type == dict_t);
-			TORRENT_ASSERT(i < m_size);
-			std::pair<char const*, lazy_entry> const& e = m_data.dict[i];
-			return std::make_pair(std::string(e.first, e.second.m_begin - e.first), &e.second);
-		}
+		std::pair<std::string, lazy_entry const*> dict_at(int i) const;
 
 		int dict_size() const
 		{
@@ -218,10 +214,11 @@ namespace libtorrent
 		entry_type_t m_type;
 		union data_t
 		{
-			std::pair<char const*, lazy_entry>* dict;
+			lazy_dict_entry* dict;
 			lazy_entry* list;
 			char const* start;
 		} m_data;
+
 		int m_size; // if list or dictionary, the number of items
 		int m_capacity; // if list or dictionary, allocated number of items
 		// used for dictionaries and lists to record the range
@@ -232,6 +229,12 @@ namespace libtorrent
 		// non-copyable
 		lazy_entry(lazy_entry const&);
 		lazy_entry const& operator=(lazy_entry const&);
+	};
+
+	struct lazy_dict_entry
+	{
+		char const* name;
+		lazy_entry val;
 	};
 
 	TORRENT_EXPORT std::string print_entry(lazy_entry const& e);
