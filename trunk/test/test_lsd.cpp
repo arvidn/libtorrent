c5496b44bc610d261c9e48ac55875364e32b5704
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2009-12-02 02:51:26 +0000

    optimized local peer discovery unittest

--- trunk/test/test_lsd.cpp
+++ trunk/test/test_lsd.cpp
@@ -45,75 +45,42 @@ void test_lsd()
 
 	session ses1(fingerprint("LT", 0, 1, 0, 0), std::make_pair(48100, 49000), "0.0.0.0", 0);
 	session ses2(fingerprint("LT", 0, 1, 0, 0), std::make_pair(49100, 50000), "0.0.0.0", 0);
-	session ses3(fingerprint("LT", 0, 1, 0, 0), std::make_pair(50100, 51000), "0.0.0.0", 0);
-
-	// this is to avoid everything finish from a single peer
-	// immediately. To make the swarm actually connect all
-	// three peers before finishing.
-	float rate_limit = 180000;
-	ses1.set_upload_rate_limit(int(rate_limit));
-	ses2.set_download_rate_limit(int(rate_limit));
-	ses3.set_download_rate_limit(int(rate_limit));
-	ses2.set_upload_rate_limit(int(rate_limit / 2));
-	ses3.set_upload_rate_limit(int(rate_limit / 2));
 
 	session_settings settings;
 	settings.allow_multiple_connections_per_ip = true;
-	settings.ignore_limits_on_local_network = false;
 	ses1.set_settings(settings);
 	ses2.set_settings(settings);
-	ses3.set_settings(settings);
 
 	ses1.start_lsd();
 	ses2.start_lsd();
-	ses3.start_lsd();
-
-#ifndef TORRENT_DISABLE_ENCRYPTION
-	pe_settings pes;
-	pes.out_enc_policy = pe_settings::forced;
-	pes.in_enc_policy = pe_settings::forced;
-	ses1.set_pe_settings(pes);
-	ses2.set_pe_settings(pes);
-	ses3.set_pe_settings(pes);
-#endif
-
 	torrent_handle tor1;
 	torrent_handle tor2;
-	torrent_handle tor3;
 
-	boost::tie(tor1, tor2, tor3) = setup_transfer(&ses1, &ses2, &ses3, true, false, false, "_lsd");
+	using boost::tuples::ignore;
+	boost::tie(tor1, tor2, ignore) = setup_transfer(&ses1, &ses2, 0, true, false, false, "_lsd");
 
 	for (int i = 0; i < 30; ++i)
 	{
 		print_alerts(ses1, "ses1", true);
 		print_alerts(ses2, "ses2", true);
-		print_alerts(ses3, "ses3", true);
 
 		torrent_status st1 = tor1.status();
 		torrent_status st2 = tor2.status();
-		torrent_status st3 = tor3.status();
 
 		std::cerr
 			<< "\033[33m" << int(st1.upload_payload_rate / 1000.f) << "kB/s "
-			<< st1.num_peers << ": "
 			<< "\033[32m" << int(st2.download_payload_rate / 1000.f) << "kB/s "
 			<< "\033[31m" << int(st2.upload_payload_rate / 1000.f) << "kB/s "
 			<< "\033[0m" << int(st2.progress * 100) << "% "
-			<< st2.num_peers << " - "
-			<< "\033[32m" << int(st3.download_payload_rate / 1000.f) << "kB/s "
-			<< "\033[31m" << int(st3.upload_payload_rate / 1000.f) << "kB/s "
-			<< "\033[0m" << int(st3.progress * 100) << "% "
-			<< st3.num_peers
 			<< std::endl;
 
-		if (tor2.is_seed() && tor3.is_seed()) break;
+		if (tor2.is_seed() /*&& tor3.is_seed()*/) break;
 		test_sleep(1000);
 	}
 
 	TEST_CHECK(tor2.is_seed());
-	TEST_CHECK(tor3.is_seed());
 
-	if (tor2.is_seed() && tor3.is_seed()) std::cerr << "done\n";
+	if (tor2.is_seed()) std::cerr << "done\n";
 }
 
 int test_main()
