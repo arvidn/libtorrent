04d31cea6e96c3feada8f17abfb98d4f48371be4
Arvid Norberg <arvid@libtorrent.org>
2010-02-18 06:45:07 +0000

    support option to report redundant bytes or not to tracker as well as 'corrupt' argument

--- src/http_tracker_connection.cpp
+++ src/http_tracker_connection.cpp
@@ -139,7 +139,8 @@ namespace libtorrent
 			char str[1024];
 			const bool stats = tracker_req().send_stats;
 			snprintf(str, sizeof(str), "&peer_id=%s&port=%d&uploaded=%"PRId64
-				"&downloaded=%"PRId64"&left=%"PRId64"&compact=1&numwant=%d&key=%x&no_peer_id=1"
+				"&downloaded=%"PRId64"&left=%"PRId64"&corrupt="PRId64"&compact=1"
+				"&numwant=%d&key=%x&no_peer_id=1"
 				, escape_string((const char*)&tracker_req().pid[0], 20).c_str()
 				// the i2p tracker seems to verify that the port is not 0,
 				// even though it ignores it otherwise
@@ -147,6 +148,7 @@ namespace libtorrent
 				, stats ? tracker_req().uploaded : 0
 				, stats ? tracker_req().downloaded : 0
 				, stats ? tracker_req().left : 0
+				, stats ? tracker_req().corrupt : 0
 				, tracker_req().num_want
 				, tracker_req().key);
 			url += str;
