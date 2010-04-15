20abaf01faeff00f7ffae2a766214f7f03643c17
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2010-04-15 03:29:34 +0000

    fixed deadlock when starting torrents with certain invalid tracker URLs

--- branches/RC_0_15/src/http_tracker_connection.cpp
+++ branches/RC_0_15/src/http_tracker_connection.cpp
@@ -222,7 +222,7 @@ namespace libtorrent
 
 	void http_tracker_connection::on_connect(http_connection& c)
 	{
-    	error_code ec;
+		error_code ec;
 		tcp::endpoint ep = c.socket().remote_endpoint(ec);
 		m_tracker_ip = ep.address();
 		boost::shared_ptr<request_callback> cb = requester();
