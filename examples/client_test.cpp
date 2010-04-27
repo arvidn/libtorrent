2705859bb4d6234b7481236fcb5fea321df09ef0
Arvid Norberg <arvid@libtorrent.org>
2010-04-27 18:22:59 +0000

    fixed bug where trackers wouldn't be retried if they failed

--- examples/client_test.cpp
+++ examples/client_test.cpp
@@ -1432,7 +1432,7 @@ int main(int argc, char* argv[])
 					snprintf(str, sizeof(str), "%2d %-55s fails: %-3d %s %s \"%s\" %s\n"
 						, i->tier, i->url.c_str(), i->fails, i->verified?"OK ":"-  "
 						, i->updating?"updating"
-							:!i->verified?""
+							:!i->will_announce(now)?""
 							:to_string(total_seconds(i->next_announce - now), 8).c_str()
 						, i->last_error ? i->last_error.message().c_str() : ""
 						, i->message.c_str());
