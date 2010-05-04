0d96dc158500f986fb2350aa976149367cf2381b
daniel_wallin <daniel_wallin@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2010-05-04 09:50:24 +0000

    Clear the user provided read buffers when closing.

--- branches/libtorrent_utp/src/utp_stream.cpp
+++ branches/libtorrent_utp/src/utp_stream.cpp
@@ -849,6 +849,8 @@ void utp_socket_impl::destroy()
 	cancel_handlers(m_error, true);
 
 	m_userdata = 0;
+    m_read_buffer.clear();
+    m_read_buffer_size = 0;
 
 	if (m_state == UTP_STATE_ERROR_WAIT
 		|| m_state == UTP_STATE_NONE
