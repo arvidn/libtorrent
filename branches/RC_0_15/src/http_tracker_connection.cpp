f4acc975857553ece35d0661829869271a19102b
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2010-09-23 16:38:50 +0000

    no peers from tracker when stopping is no longer an error

--- branches/RC_0_15/src/http_tracker_connection.cpp
+++ branches/RC_0_15/src/http_tracker_connection.cpp
@@ -443,7 +443,9 @@ namespace libtorrent
 		lazy_entry const* ipv6_peers = 0;
 #endif
 
-		if (peers_ent == 0 && ipv6_peers == 0)
+		// if we didn't receive any peers. We don't care if we're stopping anyway
+		if (peers_ent == 0 && ipv6_peers == 0
+			&& tracker_req().event != tracker_request::stopped)
 		{
 			fail(-1, "missing 'peers' and 'peers6' entry in tracker response"
 				, interval, min_interval);
