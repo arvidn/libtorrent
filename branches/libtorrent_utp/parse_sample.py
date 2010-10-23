847d3cb40fdeece4ccbbcdb2e9a3730bf0b486c1
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2010-10-23 16:06:36 +0000

    parse_sample update

--- branches/libtorrent_utp/parse_sample.py
+++ branches/libtorrent_utp/parse_sample.py
@@ -86,6 +86,9 @@ for l in f:
 	if 'fflush' == fun: fold = indentation
 	if 'vfprintf' == fun: fold = indentation
 	if 'fprintf' == fun: fold = indentation
+	if 'BN_mod_exp' == fun: fold = indentation
+	if 'BN_CTX_free' == fun: fold = indentation
+	if 'cerror' == fun: fold = indentation
 	
 list = []
 for k in fun_samples:
