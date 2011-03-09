6d9e4daa1f8c6d2187e0c285be395833173d6935
Arvid Norberg <arvid@libtorrent.org>
2011-03-09 03:59:59 +0000

    fixed clang build warning

--- include/libtorrent/io.hpp
+++ include/libtorrent/io.hpp
@@ -54,10 +54,22 @@ namespace libtorrent
 			for (int i = 0; i < (int)sizeof(T); ++i)
 			{
 				ret <<= 8;
-				ret |= static_cast<unsigned char>(*start);
+				ret |= static_cast<boost::uint8_t>(*start);
 				++start;
 			}
 			return ret;
+		}
+
+		template <class InIt>
+		boost::uint8_t read_impl(InIt& start, type<boost::uint8_t>)
+		{
+			return static_cast<boost::uint8_t>(*start++);
+		}
+
+		template <class InIt>
+		boost::int8_t read_impl(InIt& start, type<boost::int8_t>)
+		{
+			return static_cast<boost::int8_t>(*start++);
 		}
 
 		template <class T, class OutIt>
