e9d2d364b0ca778338f83deb9fd3a1cafc08005c
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2011-06-17 15:10:40 +0000

    fixed unaligned disk access for unbuffered I/O in windows

--- branches/RC_0_15/src/file.cpp
+++ branches/RC_0_15/src/file.cpp
@@ -496,7 +496,7 @@ namespace libtorrent
 		if (ReadFileScatter(m_file_handle, segment_array, size, 0, &ol) == 0)
 		{
 			DWORD last_error = GetLastError();
-			if (last_error != ERROR_IO_PENDING)
+			if (last_error != ERROR_IO_PENDING && last_error != ERROR_HANDLE_EOF)
 			{
 				TORRENT_ASSERT(GetLastError() != ERROR_BAD_ARGUMENTS);
 				ec = error_code(GetLastError(), get_system_category());
@@ -505,9 +505,12 @@ namespace libtorrent
 			}
 			if (GetOverlappedResult(m_file_handle, &ol, &ret, true) == 0)
 			{
-				ec = error_code(GetLastError(), get_system_category());
-				CloseHandle(ol.hEvent);
-				return -1;
+				if (GetLastError() != ERROR_HANDLE_EOF)
+				{
+					ec = error_code(GetLastError(), get_system_category());
+					CloseHandle(ol.hEvent);
+					return -1;
+				}
 			}
 		}
 		CloseHandle(ol.hEvent);
