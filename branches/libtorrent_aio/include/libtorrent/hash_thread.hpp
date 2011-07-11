b3ae8f9662c5d3420c76ff7229ef492a985f816a
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2011-07-11 05:03:45 +0000

    add missing files

---
+++ branches/libtorrent_aio/include/libtorrent/hash_thread.hpp
@@ -0,0 +1,89 @@
+/*
+
+Copyright (c) 2011, Arvid Norberg
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef TORRENT_HASH_THREAD
+#define TORRENT_HASH_THREAD
+
+#include "libtorrent/config.hpp"
+#include "libtorrent/thread.hpp"
+#include <deque>
+#include <vector>
+#include <boost/detail/atomic_count.hpp>
+
+namespace libtorrent
+{
+	struct cached_piece_entry;
+	struct disk_io_thread;
+
+	struct hash_thread
+	{
+		hash_thread(disk_io_thread* d);
+		void stop();
+		bool async_hash(cached_piece_entry* p, int start, int end);
+		void set_num_threads(int i, bool wait = true);
+
+	private:
+
+		void thread_fun();
+
+		struct hash_queue_entry
+		{
+			cached_piece_entry const* piece;
+			int start;
+			int end;
+		};
+
+		void process_piece(hash_queue_entry const& e);
+
+		// the mutex only protects m_cond and m_queue
+		// all other members are only used from a single
+		// thread (the user of this class, i.e. the disk
+		// thread).
+		mutex m_mutex;
+		condition m_cond;
+		std::deque<hash_queue_entry> m_queue;
+
+		std::vector<boost::shared_ptr<thread> > m_threads;
+		// this is a counter which is atomically incremented
+		// by each thread as it's started up, in order to
+		// assign a unique id to each thread
+		boost::detail::atomic_count m_num_threads;
+
+		// used for posting completion notifications back
+		// to the disk thread
+		disk_io_thread* m_disk_thread;
+	};
+
+}
+
+#endif // TORRENT_HASH_THREAD
+
---
+++ branches/libtorrent_aio/src/hash_thread.cpp
@@ -0,0 +1,179 @@
+/*
+
+Copyright (c) 2011, Arvid Norberg
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include "libtorrent/config.hpp"
+
+#include <boost/shared_ptr.hpp>
+#include <boost/bind.hpp>
+
+#include "libtorrent/hash_thread.hpp"
+#include "libtorrent/disk_io_thread.hpp"
+#include "libtorrent/disk_io_job.hpp"
+
+namespace libtorrent
+{
+
+	const int block_size = 0x4000;
+
+	hash_thread::hash_thread(disk_io_thread* d)
+		: m_num_threads(0)
+		, m_disk_thread(d)
+	{}
+
+	// returns true if the job was submitted for async. processing
+	// and false if it was processed immediately
+	bool hash_thread::async_hash(cached_piece_entry* p, int start, int end)
+	{
+		TORRENT_ASSERT(p->hashing == -1);
+		if (p->hashing != -1) return false;
+
+		hash_queue_entry e;
+		e.piece = p;
+		e.start = start;
+		e.end = end;
+
+		if (m_num_threads == 0)
+		{
+			// if we don't have any worker threads
+			// just do the work immediately
+			process_piece(e);
+			return false;
+		}
+		else
+		{
+			// increase refcounts to make sure these blocks are not removed
+			p->hashing = start;
+			for (int i = start; i < end; ++i)
+			{
+				++p->blocks[i].refcount;
+				++p->refcount;
+			}
+
+			mutex::scoped_lock l(m_mutex);
+			m_queue.push_back(e);
+			m_cond.signal_all(l);
+			return true;
+		}
+	}
+
+	void hash_thread::set_num_threads(int i, bool wait)
+	{
+		if (i == m_num_threads) return;
+
+		if (i > m_num_threads)
+		{
+			int k = m_num_threads;
+			while (k < i)
+			{
+				m_threads.push_back(boost::shared_ptr<thread>(
+					new thread(boost::bind(&hash_thread::thread_fun, this))));
+				++k;
+			}
+		}
+		else
+		{
+			while (m_num_threads > i) { --m_num_threads; }
+			mutex::scoped_lock l(m_mutex);
+			m_cond.signal_all(l);
+			l.unlock();
+			// TODO: technically, we can't not wait, since if
+			// we destruct immediately following those threads
+			// may post messages back to the disk thread after
+			// it has been destructed
+			if (wait) for (int i = m_num_threads; i < m_threads.size(); ++i) m_threads[i]->join();
+			// this will detach the threads
+			m_threads.resize(m_num_threads);
+		}
+	}
+
+	void hash_thread::stop() { set_num_threads(0, true); }
+
+	void hash_thread::thread_fun()
+	{
+		int thread_id = (++m_num_threads) - 1;
+	
+		for (;;)
+		{
+			mutex::scoped_lock l(m_mutex);
+			while (m_queue.empty() && thread_id < m_num_threads) m_cond.wait(l);
+
+			// if the number of wanted thread is decreased,
+			// we may stop this thread
+			// when we're terminating the last hasher thread (id=0), make sure
+			// we finish up all queud jobs first
+			if ((thread_id != 0 || m_queue.empty()) && thread_id >= m_num_threads) break;
+
+			hash_queue_entry e = m_queue.front();
+			m_queue.pop_front();
+			l.unlock();
+
+			process_piece(e);
+
+			// post back to the disk thread
+			disk_io_job j;
+			j.action = disk_io_job::hash_complete;
+			j.buffer = (char*)e.piece;
+			j.piece = e.start;
+			j.offset = e.end;
+			m_disk_thread->add_job(j);
+		}
+
+#ifdef TORRENT_DEBUG
+		if (thread_id == 0)
+		{
+			// when we're terminating the last hasher thread, make sure
+			// there are no more scheduled jobs
+			mutex::scoped_lock l(m_mutex);
+			TORRENT_ASSERT(m_queue.empty());
+		}
+#endif
+	}
+
+	void hash_thread::process_piece(hash_queue_entry const& e)
+	{
+		int piece_size = e.piece->storage.get()->info()->piece_size(e.piece->piece);
+		partial_hash& ph = *e.piece->hash;
+		for (int i = e.start; i < e.end; ++i)
+		{
+			cached_block_entry& bl = e.piece->blocks[i];
+			// make sure we're not waiting for data to be read into
+			// this buffer. i.e. pending = true and dirty = false
+			TORRENT_ASSERT(!bl.pending || bl.dirty);
+			TORRENT_ASSERT(bl.buf);
+
+			int size = (std::min)(block_size, piece_size - ph.offset);
+			ph.h.update(bl.buf, size);
+			ph.offset += size;
+		}
+	}
+}
+
