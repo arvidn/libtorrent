92c61999d1ce89ab09194f939abb24f1b38c1945
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2011-09-24 09:09:35 +0000

    make average_accumulator a little bit smarter, to not report zeroes when there are no samples

--- branches/libtorrent_aio/include/libtorrent/sliding_average.hpp
+++ branches/libtorrent_aio/include/libtorrent/sliding_average.hpp
@@ -91,8 +91,11 @@ struct average_accumulator
 		int ret;
 		if (m_num_samples == 0) ret = 0;
 		else ret = int(m_sample_sum / m_num_samples);
-		m_num_samples = 0;
-		m_sample_sum = 0;
+		// in case we don't get any more samples, at least
+		// let the average roll over, but only be worth a
+		// single sample
+		m_num_samples = 1;
+		m_sample_sum = ret;
 		return ret;
 	}
 
