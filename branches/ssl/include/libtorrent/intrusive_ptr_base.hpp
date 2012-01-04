82ed0afea3449ac62a795a502ffc544630a7408e
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2012-01-04 21:12:29 +0000

    branch for proper bittorrent over SSL support

---
+++ branches/ssl/include/libtorrent/intrusive_ptr_base.hpp
@@ -0,0 +1,91 @@
+/*
+
+Copyright (c) 2007, Arvid Norberg
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef TORRENT_INTRUSIVE_PTR_BASE
+#define TORRENT_INTRUSIVE_PTR_BASE
+
+#include <boost/detail/atomic_count.hpp>
+#include <boost/checked_delete.hpp>
+#include <boost/intrusive_ptr.hpp>
+#include "libtorrent/config.hpp"
+#include "libtorrent/assert.hpp"
+
+namespace libtorrent
+{
+	template<class T>
+	struct intrusive_ptr_base
+	{
+		intrusive_ptr_base(intrusive_ptr_base<T> const&)
+			: m_refs(0) {}
+
+		intrusive_ptr_base& operator=(intrusive_ptr_base const& rhs)
+		{ return *this; }
+
+		friend void intrusive_ptr_add_ref(intrusive_ptr_base<T> const* s)
+		{
+			TORRENT_ASSERT(s != 0);
+			TORRENT_ASSERT(s->m_refs >= 0);
+			++s->m_refs;
+		}
+
+		friend void intrusive_ptr_release(intrusive_ptr_base<T> const* s)
+		{
+			TORRENT_ASSERT(s != 0);
+			TORRENT_ASSERT(s->m_refs > 0);
+			if (--s->m_refs == 0)
+				boost::checked_delete(static_cast<T const*>(s));
+		}
+
+		boost::intrusive_ptr<T> self()
+		{ return boost::intrusive_ptr<T>((T*)this); }
+
+		boost::intrusive_ptr<const T> self() const
+		{ return boost::intrusive_ptr<const T>((T const*)this); }
+
+		int refcount() const { return m_refs; }
+
+		intrusive_ptr_base(): m_refs(0) {}
+
+		// so that we can access this when logging
+#if !defined TORRENT_LOGGING \
+		&& !defined TORRENT_VERBOSE_LOGGING \
+		&& !defined TORRENT_ERROR_LOGGING
+	private:
+#endif
+		// reference counter for intrusive_ptr
+		mutable boost::detail::atomic_count m_refs;
+	};
+
+}
+
+#endif
+
---
+++ branches/ssl/test/test.hpp
@@ -0,0 +1,116 @@
+/*
+
+Copyright (c) 2008, Arvid Norberg
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef TEST_HPP
+#define TEST_HPP
+
+void report_failure(char const* str, char const* file, int line);
+
+#include <boost/config.hpp>
+#include <exception>
+#include <sstream>
+
+#if defined(_MSC_VER)
+#define COUNTER_GUARD(x)
+#else
+#define COUNTER_GUARD(type) \
+    struct BOOST_PP_CAT(type, _counter_guard) \
+    { \
+        ~BOOST_PP_CAT(type, _counter_guard()) \
+        { \
+            TEST_CHECK(counted_type<type>::count == 0); \
+        } \
+    } BOOST_PP_CAT(type, _guard)
+#endif
+
+#define TEST_REPORT_AUX(x, line, file) \
+	report_failure(x, line, file)
+
+#ifdef BOOST_NO_EXCEPTIONS
+#define TEST_CHECK(x) \
+	if (!(x)) \
+		TEST_REPORT_AUX("TEST_CHECK failed: \"" #x "\"", __FILE__, __LINE__);
+#define TEST_EQUAL(x, y) \
+	if (x != y) { \
+		std::stringstream s__; \
+		s__ << "TEST_EQUAL_ERROR: " #x ": " << x << " expected: " << y << std::endl; \
+		TEST_REPORT_AUX(s__.str().c_str(), __FILE__, __LINE__); \
+	}
+#else
+#define TEST_CHECK(x) \
+	try \
+	{ \
+		if (!(x)) \
+			TEST_REPORT_AUX("TEST_CHECK failed: \"" #x "\"", __FILE__, __LINE__); \
+	} \
+	catch (std::exception& e) \
+	{ \
+		TEST_ERROR("Exception thrown: " #x " :" + std::string(e.what())); \
+	} \
+	catch (...) \
+	{ \
+		TEST_ERROR("Exception thrown: " #x); \
+	}
+
+#define TEST_EQUAL(x, y) \
+	try { \
+		if (x != y) { \
+			std::stringstream s__; \
+			s__ << "TEST_EQUAL_ERROR: " #x ": " << x << " expected: " << y << std::endl; \
+			TEST_REPORT_AUX(s__.str().c_str(), __FILE__, __LINE__); \
+		} \
+	} \
+	catch (std::exception& e) \
+	{ \
+		TEST_ERROR("Exception thrown: " #x " :" + std::string(e.what())); \
+	} \
+	catch (...) \
+	{ \
+		TEST_ERROR("Exception thrown: " #x); \
+	}
+#endif
+
+#define TEST_ERROR(x) \
+	TEST_REPORT_AUX((std::string("ERROR: \"") + x + "\"").c_str(), __FILE__, __LINE__)
+
+#define TEST_NOTHROW(x) \
+	try \
+	{ \
+		x; \
+	} \
+	catch (...) \
+	{ \
+		TEST_ERROR("Exception thrown: " #x); \
+	}
+
+#endif // TEST_HPP
+
