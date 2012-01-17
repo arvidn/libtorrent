90bb0c5087248bf185e4436dbf7cac10dfc38a07
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2012-01-17 03:52:13 +0000

    merged changes from trunk

--- branches/libtorrent_aio/src/session_impl.cpp
+++ branches/libtorrent_aio/src/session_impl.cpp
@@ -1305,6 +1305,11 @@ namespace aux {
 			":no memory peer errors"
 			":too many peers"
 			":transport timeout peers"
+			":uTP idle"
+			":uTP syn-sent"
+			":uTP connected"
+			":uTP fin-sent"
+			":uTP close-wait"
 			"\n\n", m_stats_logger);
 	}
 #endif
@@ -3747,6 +3752,7 @@ namespace aux {
 		{
 			cache_status cs;
 			m_disk_thread.get_disk_metrics(cs);
+			session_status sst = status();
 
 			m_read_ops.add_sample((cs.reads - m_last_cache_status.reads) * 1000.0 / float(tick_interval_ms));
 			m_write_ops.add_sample((cs.writes - m_last_cache_status.writes) * 1000.0 / float(tick_interval_ms));
@@ -3922,6 +3928,12 @@ namespace aux {
 			STAT_LOG(d, m_no_memory_peers);
 			STAT_LOG(d, m_too_many_peers);
 			STAT_LOG(d, m_transport_timeout_peers);
+
+			STAT_LOG(d, sst.utp_stats.num_idle);
+			STAT_LOG(d, sst.utp_stats.num_syn_sent);
+			STAT_LOG(d, sst.utp_stats.num_connected);
+			STAT_LOG(d, sst.utp_stats.num_fin_sent);
+			STAT_LOG(d, sst.utp_stats.num_close_wait);
 
 			fprintf(m_stats_logger, "\n");
 
