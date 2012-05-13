8ead6c7efb5cf62c6ab98f859ca4001ba49bc595
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2012-05-13 17:46:58 +0000

    added a python script to collect all TODO comments from the source

---
+++ branches/libtorrent_aio/gather_todo.py
@@ -0,0 +1,51 @@
+import glob
+import os
+
+paths = ['src/*.cpp', 'src/kademlia/*.cpp', 'include/libtorrent/*.hpp', 'include/libtorrent/kademlia/*.hpp', 'include/libtorrent/aux_/*.hpp', 'include/libtorrent/extensions/*.hpp']
+
+os.system('ctags %s' % ' '.join(paths))
+
+files = []
+
+for p in paths:
+	files.extend(glob.glob(p))
+
+output = open('todo.rst', 'w+')
+
+for f in files:
+	h = open(f)
+
+	state = ''
+	line_no = 0
+
+	for line in h:
+		line_no += 1
+		line = line.strip()
+		if 'TODO:' in line and line.startswith('//'):
+			line = line.split('TODO:')[1]
+			state = 'todo'
+			headline = '%s:%d' % (f.replace('_', '\\_'), line_no)
+			print >>output, '\n' + headline
+			print >>output, ('-' * len(headline)) + '\n'
+			print >>output, line.strip()
+			continue
+			
+		if state == '': continue
+
+		if state == 'todo':
+			if line.strip().startswith('//'):
+				print >>output, line[2:].strip()
+			else:
+				state = 'context'
+				print >>output, '\n::\n'
+				print >>output, '\t%s' % line
+			continue
+
+		if state == 'context':
+			print >>output, '\t%s' % line
+			state = ''
+
+	h.close()
+
+output.close()
+
