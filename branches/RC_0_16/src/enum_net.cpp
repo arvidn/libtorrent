4a370dccf9f26e09cc244b6564e476ca116d0dc6
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2012-05-13 18:56:53 +0000

    merged fixes from trunk

--- branches/RC_0_16/src/enum_net.cpp
+++ branches/RC_0_16/src/enum_net.cpp
@@ -446,7 +446,7 @@ namespace libtorrent
 		}
 		ifconf ifc;
 		// make sure the buffer is aligned to hold ifreq structs
-		ifreq buf[30];
+		ifreq buf[40];
 		ifc.ifc_len = sizeof(buf);
 		ifc.ifc_buf = (char*)buf;
 		if (ioctl(s, SIOCGIFCONF, &ifc) < 0)
@@ -459,9 +459,19 @@ namespace libtorrent
 		char *ifr = (char*)ifc.ifc_req;
 		int remaining = ifc.ifc_len;
 
-		while (remaining > sizeof(ifreq))
+		while (remaining > 0)
 		{
 			ifreq const& item = *reinterpret_cast<ifreq*>(ifr);
+
+#ifdef _SIZEOF_ADDR_IFREQ
+			int current_size = _SIZEOF_ADDR_IFREQ(item);
+#elif defined TORRENT_BSD
+			int current_size = item.ifr_addr.sa_len + IFNAMSIZ;
+#else
+			int current_size = sizeof(ifreq);
+#endif
+
+			if (remaining < current_size) break;
 
 			if (item.ifr_addr.sa_family == AF_INET
 #if TORRENT_USE_IPV6
@@ -510,17 +520,9 @@ namespace libtorrent
 				ret.push_back(iface);
 			}
 
-#ifdef _SIZEOF_ADDR_IFREQ
-			int current_size = _SIZEOF_ADDR_IFREQ(item);
-#elif defined TORRENT_BSD
-			int current_size = item.ifr_addr.sa_len + IFNAMSIZ;
-#else
-			int current_size = sizeof(ifreq);
-#endif
 			ifr += current_size;
 			remaining -= current_size;
 		}
-		TORRENT_ASSERT(remaining == 0);
 		close(s);
 
 #elif TORRENT_USE_GETADAPTERSADDRESSES
