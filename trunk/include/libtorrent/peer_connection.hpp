309fd200918141a2bfa7c98e7502ce407c52a617
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2012-06-06 16:05:25 +0000

    add a force-flag to cancel_request

--- trunk/include/libtorrent/peer_connection.hpp
+++ trunk/include/libtorrent/peer_connection.hpp
@@ -506,7 +506,9 @@ namespace libtorrent
 		// sends a cancel message if appropriate
 		// refills the request queue, and possibly ignoring pieces requested
 		// by peers in the ignore list (to avoid recursion)
-		void cancel_request(piece_block const& b);
+		// if force is true, the blocks is also freed from the piece
+		// picker, allowing another peer to request it immediately
+		void cancel_request(piece_block const& b, bool force = false);
 		void send_block_requests();
 
 		int bandwidth_throttle(int channel) const
