a4091ff1a1226d60231ec0990cb7aeb88767bc1d
Arvid Norberg <arvid@libtorrent.org>
2012-06-26 03:34:42 +0000

    add comment

--- src/session_impl.cpp
+++ src/session_impl.cpp
@@ -3397,6 +3397,7 @@ namespace aux {
 					if (!t.is_finished())
 					{
 						// TODO: make this bias configurable
+						// TODO: also take average_peers into account, to create a bias for downloading torrents with < average peers
 						TORRENT_ASSERT(m_num_active_downloading > 0);
 						num_attempts += m_num_active_finished / m_num_active_downloading;
 					}
