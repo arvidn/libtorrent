d181b2e057769ca7c9e127dc48f90e3b2763c4c6
Arvid Norberg <arvid@libtorrent.org>
2012-09-30 20:55:12 +0000

    merge pad_file_at optimization from libtorrent_aio

--- include/libtorrent/file_storage.hpp
+++ include/libtorrent/file_storage.hpp
@@ -249,12 +249,12 @@ namespace libtorrent
 		sha1_hash hash(int index) const;
 		std::string const& symlink(int index) const;
 		time_t mtime(int index) const;
-		int file_index(int index) const;
 		size_type file_base(int index) const;
 		void set_file_base(int index, size_type off);
 		std::string file_path(int index) const;
 		std::string file_name(int index) const;
 		size_type file_size(int index) const;
+		bool pad_file_at(int index) const;
 		size_type file_offset(int index) const;
 
 		sha1_hash hash(internal_file_entry const& fe) const;
@@ -266,6 +266,7 @@ namespace libtorrent
 		std::string file_path(internal_file_entry const& fe) const;
 		std::string file_name(internal_file_entry const& fe) const;
 		size_type file_size(internal_file_entry const& fe) const;
+		bool pad_file_at(internal_file_entry const& fe) const;
 		size_type file_offset(internal_file_entry const& fe) const;
 
 #if !defined TORRENT_VERBOSE_LOGGING \
