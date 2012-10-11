8f0d039b3c11c4f12511114998760ff5c3de66a1
Arvid Norberg <arvid@libtorrent.org>
2012-10-11 02:20:16 +0000

    merged proxy test change from RC_0_16

--- test/setup_transfer.cpp
+++ test/setup_transfer.cpp
@@ -201,7 +201,12 @@ int start_proxy(int proxy_type)
 		, port, type, auth);
 
 	fprintf(stderr, "starting delegated proxy on port %d (%s %s)...\n", port, type, auth);
-	system(buf);
+	int r = system(buf);
+	if (r != 0)
+	{
+		fprintf(stderr, "failed (%d) %s\n", errno, strerror(errno));
+		exit(1);
+	}
 	fprintf(stderr, "launched\n");
 	// apparently delegate takes a while to open its listen port
 	test_sleep(500);
