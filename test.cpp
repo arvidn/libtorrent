90879c47fc07514c2092090ee8652aac194a4324
arvid <arvid@cs.umu.se>
2012-11-16 22:32:40 +0600

    support force quit by sending a second quit signal in test.cpp

--- test.cpp
+++ test.cpp
@@ -14,10 +14,16 @@
 #include <signal.h>
 
 bool quit = false;
+bool force_quit = false;
 
 void sighandler(int s)
 {
 	quit = true;
+}
+
+void sighandler_forcequit(int s)
+{
+	force_quit = true;
 }
 
 using namespace libtorrent;
@@ -27,6 +33,9 @@ int main(int argc, char *const argv[])
 	session ses(fingerprint("LT", 0, 1, 0, 0)
 		, std::make_pair(6881, 6882));
 	ses.set_alert_mask(~0);
+	settings_pack s;
+	high_performance_seed(s);
+	ses.apply_settings(s);
 
 	alert_handler alerts;
 
@@ -67,25 +76,23 @@ int main(int argc, char *const argv[])
 	bool shutting_down = false;
 	while (!quit || !resume.ok_to_quit())
 	{
-		if (ses.wait_for_alert(milliseconds(500)))
-		{
-			alert_queue.clear();
-			ses.pop_alerts(&alert_queue);
-//			for (std::deque<alert*>::iterator i = alert_queue.begin()
-//				, end(alert_queue.end()); i != end; ++i)
-//			{
-//				printf(" %s\n", (*i)->message().c_str());
-//			}
-			alerts.dispatch_alerts(alert_queue);
-		}
-		ses.post_torrent_updates();
+		usleep(1000000);
+		ses.pop_alerts(&alert_queue);
+//		for (std::deque<alert*>::iterator i = alert_queue.begin()
+//			, end(alert_queue.end()); i != end; ++i)
+//		{
+//			printf(" %s\n", (*i)->message().c_str());
+//		}
+		alerts.dispatch_alerts(alert_queue);
+		if (!shutting_down) ses.post_torrent_updates();
 		if (quit && !shutting_down)
 		{
 			resume.save_all();
 			shutting_down = true;
-			signal(SIGTERM, NULL);
-			signal(SIGINT, NULL);
+			signal(SIGTERM, &sighandler_forcequit);
+			signal(SIGINT, &sighandler_forcequit);
 		}
+		if (force_quit) break;
 	}
 
 	dlg.stop();
