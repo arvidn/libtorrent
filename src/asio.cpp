755c093c6f1f8a808704e1ce57e2bd432b48bbe3
Arvid Norberg <arvid@libtorrent.org>
2012-12-15 05:00:54 +0000

    fix visibility=hidden build in Jamfile and fix some build errors caused by it

--- src/asio.cpp
+++ src/asio.cpp
@@ -79,6 +79,16 @@
 
 #else // _MSC_VER
 
+#include "libtorrent/config.hpp"
+
+#define TORRENT_HAS_ASIO_DECL x ## BOOST_ASIO_DECL
+
+// only define BOOST_ASIO_DECL if it hasn't already been defined
+// or if it has been defined to an empty string
+#if TORRENT_HAS_ASIO_DECL == x
+#define BOOST_ASIO_DECL BOOST_SYMBOL_EXPORT
+#endif
+
 #if BOOST_VERSION >= 104500
 #include <boost/asio/impl/src.hpp>
 #elif BOOST_VERSION >= 104400
