86bc8dcb0dad9e4239c39f1855f05327eccc611f
Arvid Norberg <arvid@libtorrent.org>
2013-01-21 16:00:11 +0000

    take torrent_handle::query_pieces into account in torrent_handle::statue()

--- src/torrent.cpp
+++ src/torrent.cpp
@@ -8538,7 +8538,7 @@ namespace libtorrent
 #endif
 		}
 
-		if (has_picker())
+		if (has_picker() && (flags & torrent_handle::query_pieces))
 		{
 			st->sparse_regions = m_picker->sparse_regions();
 			int num_pieces = m_picker->num_pieces();
