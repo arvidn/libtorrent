aa1f547a8aa3ecd1c4e7b41ec99f5fe64862a2d8
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2013-02-24 01:00:20 +0000

    don't send ipv4 and ipv6 to trackers

--- trunk/src/torrent.cpp
+++ trunk/src/torrent.cpp
@@ -2335,14 +2335,6 @@ namespace libtorrent
 
 		req.event = e;
 		error_code ec;
-		if (!m_ses.m_settings.anonymous_mode)
-		{
-			tcp::endpoint ep;
-			ep = m_ses.get_ipv6_interface();
-			if (ep != tcp::endpoint()) req.ipv6 = ep.address().to_string(ec);
-			ep = m_ses.get_ipv4_interface();
-			if (ep != tcp::endpoint()) req.ipv4 = ep.address().to_string(ec);
-		}
 
 		// if we are aborting. we don't want any new peers
 		req.num_want = (req.event == tracker_request::stopped)
