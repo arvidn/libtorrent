cf710d38a166247230e63d7a53d992e2d5331147
arvidn <arvid@cs.umu.se>
2013-03-02 20:55:39 -0800

    make pam_auth a bit more flexible by assigning different permissions to different users

--- src/pam_auth.hpp
+++ src/pam_auth.hpp
@@ -35,6 +35,7 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #include "auth_interface.hpp"
 #include <string>
+#include <map>
 
 namespace libtorrent
 {
@@ -42,11 +43,25 @@ namespace libtorrent
 	{
 		pam_auth(std::string service_name);
 		~pam_auth();
+
+		// these are the permissions the user receives
+		// if successfully authenticated
+		void set_permissions(permissions_interface* perms) { m_perms = perms; }
+
+		void set_user_permissions(std::string username, permissions_interface* p)
+		{ m_users[username] = p; }
+
 		permissions_interface const* find_user(std::string username, std::string password) const;
 
 	private:
 
+		permissions_interface* m_perms;
 		std::string m_service_name;
+		// if some users have different permissions than the default
+		// users, they have an entry in this map. Users not in this
+		// map that successfully authenticate will still get the
+		// default permissions in m_perms (which defaults to full permissions)
+		std::map<std::string, permissions_interface*> m_users;
 	};
 }
 
