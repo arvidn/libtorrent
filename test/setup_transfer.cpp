b2164bc3090936c0cc8a8c1bb65e43fd9aca0e4c
Arvid Norberg <arvid@libtorrent.org>
2013-03-10 10:19:24 +0000

    fix race condition in tracker unit test

--- test/setup_transfer.cpp
+++ test/setup_transfer.cpp
@@ -454,6 +454,7 @@ void stop_tracker()
 		tracker_ios->stop();
 		tracker_server->join();
 		tracker_server.reset();
+		delete tracker_ios;
 		tracker_ios = 0;
 	}
 }
@@ -536,8 +537,9 @@ void on_udp_receive(error_code const& ec, size_t bytes_transferred, udp::endpoin
 
 void udp_tracker_thread(int* port)
 {
-	io_service ios;
-	udp::socket acceptor(ios);
+	tracker_ios = new io_service;
+
+	udp::socket acceptor(*tracker_ios);
 	error_code ec;
 	acceptor.open(udp::v4(), ec);
 	if (ec)
@@ -557,8 +559,6 @@ void udp_tracker_thread(int* port)
 	}
 	*port = acceptor.local_endpoint().port();
 
-	tracker_ios = &ios;
-
 	fprintf(stderr, "UDP tracker initialized on port %d\n", *port);
 
 	{
@@ -576,7 +576,7 @@ void udp_tracker_thread(int* port)
 		acceptor.async_receive_from(
 			asio::buffer(buffer, sizeof(buffer)), from, boost::bind(
 				&on_udp_receive, _1, _2, &from, &buffer[0], &acceptor));
-		ios.run_one(ec);
+		tracker_ios->run_one(ec);
 		if (udp_failed) return;
 
 		if (ec)
@@ -586,7 +586,7 @@ void udp_tracker_thread(int* port)
 			tracker_initialized.signal(l);
 			return;
 		}
-		ios.reset();
+		tracker_ios->reset();
 	}
 }
 
