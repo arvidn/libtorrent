ba368eda3c7a30952ff1f66d98d40e1d2b1eca4e
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2013-03-23 06:03:35 +0000

    add debug logging for do_clear_piece assert

--- branches/libtorrent_aio/src/disk_io_thread.cpp
+++ branches/libtorrent_aio/src/disk_io_thread.cpp
@@ -2469,6 +2469,12 @@ namespace libtorrent
 		}
 		// we should always be able to evict the piece, since
 		// this is a fence job
+#if defined TORRENT_DEBUG || TORRENT_RELEASE_ASSERTS
+		fprintf(stderr, "failed to evict piece:\nrefcount: %d\npiece_refcount: %d\n"
+			"num_blocks: %d\nhashing: %d\n\nhash: %p\nhash_offset: %d\n\n"
+			, pe->refcount, pe->piece_refcount, pe->num_blocks, int(pe->hashing)
+			, pe->hash, pe->hash ? pe->hash->offset : -1);
+#endif
 		TORRENT_ASSERT(false);
 		return retry_job;
 	}
