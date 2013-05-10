9579fb0a227cb92769f637502c4473b80ad64334
Arvid Norberg <arvid@libtorrent.org>
2013-05-10 05:04:42 +0000

    removed unused buggy function

--- include/libtorrent/piece_picker.hpp
+++ include/libtorrent/piece_picker.hpp
@@ -445,7 +445,6 @@ namespace libtorrent
 			void set_not_have() { index = 0; TORRENT_ASSERT(!have()); }
 			
 			bool filtered() const { return piece_priority == filter_priority; }
-			void filtered(bool f) { piece_priority = f ? filter_priority : 0; }
 			
 			//  prio 7 is always top priority
 			//  prio 0 is always -1 (don't pick)
