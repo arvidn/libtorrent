62c976172423c98167c5a713edc217afb8c63dad
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2013-05-10 17:10:56 +0000

    add cleaning script

---
+++ trunk/clean.py
@@ -0,0 +1,25 @@
+import os
+
+to_delete = [
+	'session_stats',
+	'libtorrent_logs*',
+	'round_trip_ms.log',
+	'dht.log',
+	'upnp.log',
+	'natpmp.log',
+	'bin'
+]
+
+directories = [
+	'examples',
+	'test',
+	'.',
+	'tools'
+]
+
+for d in directories:
+	for f in to_delete:
+		path = os.path.join(d, f)
+		print path
+		os.system('rm -rf %s' % path)
+
