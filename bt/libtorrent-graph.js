0aa605028cf7063ecfa33af16ebd7db755782ae8
arvidn <arvid@cs.umu.se>
2013-05-26 13:39:54 -0700

    some more work on dashboard

--- bt/libtorrent-graph.js
+++ bt/libtorrent-graph.js
@@ -1,32 +1,59 @@
-// canvas is the name of the HTML canvas element to draw on
-// data is an array of objects. Each object is a data point with a
-//   'time' field indicating the position on the x-axis and then
-//   arbitrary additional fields for the y-axis for all the graphs
-// graphs is an array of objects specifying metadata about each graph
-//   to plot. The most important field is 'name' which indicates the
-//   name of the field in the data array to use for the plot. Also specify
-//   'color' which is a string defining the color of the line (CSS style).
+/**
+\param canvas is the name of the HTML canvas element to draw on
+\param data is an array of objects. Each object is a data point with a
+   'time' field indicating the position on the x-axis and then
+   arbitrary additional fields for the y-axis for all the graphs
+\param graphs is an array of objects specifying metadata about each graph
+   to plot. The most important field is 'name' which indicates the
+   name of the field in the data array to use for the plot. Also specify
+   'color' which is a string defining the color of the line (CSS style).
+\param start_time is a number indicating the left-most position on the
+   x-axis (the time-axis). This number is arbitrary, but it must be less than
+   'now' and it must be of the same unit as the numbers in the 'time' fields
+   in 'data'.
+\param now is a number similar to start_time, but represents the right-most
+   point on the x-axis. It represents the time value for the latest sample.
+\param unit this is an optional unit of the values on the y-axis, and will
+   be printed in the right margin for each of the tics.
+\param scale this is an optional scale factor for the data. The unit will
+   automatically have an SI prefix added to it based on the scale. Specifying
+   this locks the prefix to a specific one regardless of the magnitude of
+   the data points. This is primarily useful to make a graph always specify
+   transfer rates in kB/s, in which case it should be set to 1000.
+\param multiplier this is an optional number that's multiplied to all y-values
+   before used by any part of the graphing logic. This can be used to turn a
+   scaled unit into the base SI unit, to make the applied prefixes apply correctly.
+   For instance, time values specified in microseconds could have a multiplier
+   of 0.000001 in order to turn them into seconds
+\param use_legend this is an optional boolean defaulting to false. When true,
+   a legend of the graphs is rendered in the top left corner. In this case,
+   each object in the graphs array may have a 'label' field indicating the
+   name for that graph in the legend. If no label is specified, the 'name'
+   field is used.
+*/
 
-function render_graph(canvas, data, graphs, start_time, now, unit, scale, use_legend)
+function render_graph(canvas, data, graphs, start_time, now, unit, scale, multiplier, use_legend)
 {
-	if (typeof(unit) == 'undefined') unit = 'kB/s';
-	if (typeof(scale) == 'undefined') scale = 1000;
+	if (typeof(unit) == 'undefined') unit = '';
+	if (typeof(scale) == 'undefined') scale = 1;
+	if (typeof(multiplier) == 'undefined') multiplier = 1;
 	if (typeof(use_legend) == 'undefined') use_legend = false;
 
 	var canvas = document.getElementById(canvas);
 	var ctx = canvas.getContext('2d');
 	
 	// first find the highest rate, in order to scale
-	var peak = 1 * (scale == 'auto' ? 1 : scale);
+	var peak = 0;
 	for (dp in data)
 	{
 		for (g in graphs)
 		{
 			var n = graphs[g].name;
 			if (!data[dp].hasOwnProperty(n)) continue;
-			peak = Math.max(data[dp][n], peak);
+			peak = Math.max(data[dp][n] * multiplier, peak);
 		}
 	}
+   if (peak == 0) peak = 1 * (scale == 'auto' ? 1 : scale);
 
 	ctx.clearRect(0,0,canvas.width, canvas.height);
 	ctx.save();
@@ -58,24 +85,61 @@ function render_graph(canvas, data, graphs, start_time, now, unit, scale, use_le
 		if (peak >= 1000000000)
 		{
 			scale = 1000000000;
-			unit = 'G';
 		}
 		else if (peak >= 1000000)
 		{
 			scale = 1000000;
-			unit = 'M';
 		}
 		else if (peak >= 1000)
 		{
 			scale = 1000;
-			unit = 'k';
 		}
-		else
+		else if (peak >= 1)
 		{
 			scale = 1;
-			unit = '';
+		}
+		else if (peak >= 0.001)
+		{
+			scale = 1 / 1000;
+		}
+		else if (peak >= 0.000001)
+		{
+			scale = 1 / 1000000;
+		}
+		else if (peak >= 0.000000001)
+		{
+			scale = 1 / 1000000000;
 		}
 	}
+
+	if (scale >= 1000000000)
+	{
+		unit = 'G' + unit;
+	}
+	else if (scale >= 1000000)
+	{
+		unit = 'M' + unit;
+	}
+	else if (scale >= 1000)
+	{
+		unit = 'k' + unit;
+	}
+   else if (scale >= 1)
+   {
+      // do nothing
+   }
+   else if (scale >= 0.001)
+   {
+      unit = 'm' + unit;
+   }
+   else if (scale >= 0.00001)
+   {
+      unit = 'u' + unit;
+   }
+   else if (scale >= 0.000000001)
+   {
+      unit = 'n' + unit;
+   }
 
 	var scalex = view_width / (now - start_time);
 	var scaley = view_height / peak;
@@ -104,7 +168,7 @@ function render_graph(canvas, data, graphs, start_time, now, unit, scale, use_le
 
 	ctx.setLineDash([]);
 
-	// draw the graphs for all torrents
+	// plot all the graphs
 	for (k in graphs)
 	{
 		var g = graphs[k];
@@ -115,7 +179,7 @@ function render_graph(canvas, data, graphs, start_time, now, unit, scale, use_le
 		for (i in data)
 		{
 			var time = data[i].time;
-			var y = data[i][g.name];
+			var y = data[i][g.name] * multiplier;
 			if (typeof(y) == 'undefined') continue;
    
 			if (first)
