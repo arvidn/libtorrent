fa513f188741d333b22f52659a1aa7e167290df0
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2013-06-18 05:38:09 +0000

    fix svn up command in run_regression_tests.py and clean in between each revision by default

--- trunk/tools/run_regression_tests.py
+++ trunk/tools/run_regression_tests.py
@@ -1,5 +1,33 @@
 #!/bin/python
 
+# Copyright (c) 2013, Arvid Norberg
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the distribution.
+#     * Neither the name of the author nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import run_tests
 import os
 import time
@@ -34,7 +62,7 @@ def svn_fetch(last_rev):
 	return range(last_rev + 1, revision + 1)
 
 def svn_up(revision):
-	os.system('svn up %d' % revision)
+	os.system('svn up -r %d' % revision)
 
 def print_usage():
 	print '''usage: run_regression_tests.py [options] toolset [toolset...]
@@ -46,6 +74,7 @@ between all testsers.
 options:
 
    -j<n>    use n parallel processes for running tests
+   -i       build incrementally (i.e. don't clean between checkouts)
 '''
 
 
