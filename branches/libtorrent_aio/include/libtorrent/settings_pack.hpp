59ca844425f096bddfb857d66581f03c9cbee2da
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2013-07-23 14:37:16 +0000

    fix windows build

--- branches/libtorrent_aio/include/libtorrent/settings_pack.hpp
+++ branches/libtorrent_aio/include/libtorrent/settings_pack.hpp
@@ -48,8 +48,8 @@ namespace libtorrent
 	TORRENT_EXTRA_EXPORT void initialize_default_settings(aux::session_settings& s);
 	TORRENT_EXTRA_EXPORT void apply_pack(settings_pack const* pack, aux::session_settings& sett, aux::session_impl* ses = 0);
 
-	int TORRENT_EXPORT setting_by_name(std::string const& name);
-	char const* TORRENT_EXPORT name_for_setting(int s);
+	TORRENT_EXPORT int setting_by_name(std::string const& name);
+	TORRENT_EXPORT char const* name_for_setting(int s);
 
 #ifndef TORRENT_NO_DEPRECATE
 	struct session_settings;
