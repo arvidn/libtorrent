f81bc07138def94e4b336cb5a0ad753ecb3dc28d
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2013-07-30 06:22:13 +0000

    fix part of test_primitives for windows and add more instrumentation

--- branches/libtorrent_aio/test/test_primitives.cpp
+++ branches/libtorrent_aio/test/test_primitives.cpp
@@ -1056,7 +1056,7 @@ int test_main()
 	convert_path_to_windows(p);
 	TEST_EQUAL(p, "c:\\blah\\foo\\bar\\");
 	TEST_EQUAL(resolve_file_url("file:///c:/blah/foo/bar"), "c:\\blah\\foo\\bar");
-	TEST_EQUAL(resolve_file_url("file:///c:/b%efah/foo/bar"), "c:\\b?ah\\foo\\bar");
+	TEST_EQUAL(resolve_file_url("file:///c:/b%3fah/foo/bar"), "c:\\b?ah\\foo\\bar");
 	TEST_EQUAL(resolve_file_url("file://\\c:\\b%3fah\\foo\\bar"), "c:\\b?ah\\foo\\bar");
 #else
 	TEST_EQUAL(resolve_file_url("file:///c/blah/foo/bar"), "/c/blah/foo/bar");
@@ -1494,20 +1494,27 @@ int test_main()
 
 	// file class
 	file f;
+	ec.clear();
 #if TORRENT_USE_UNC_PATHS || !defined WIN32
 	TEST_CHECK(f.open("con", file::read_write, ec));
 #else
 	TEST_CHECK(f.open("test_file", file::read_write, ec));
 #endif
+	if (ec)
+		fprintf(stderr, "open failed: [%s] %s\n", ec.category().name(), ec.message().c_str());
 	TEST_CHECK(!ec);
 	if (ec) fprintf(stderr, "%s\n", ec.message().c_str());
 	file::iovec_t b = {(void*)"test", 4};
-	TEST_CHECK(f.writev(0, &b, 1, ec) == 4);
+	TEST_EQUAL(f.writev(0, &b, 1, ec), 4);
+	if (ec)
+		fprintf(stderr, "writev failed: [%s] %s\n", ec.category().name(), ec.message().c_str());
 	TEST_CHECK(!ec);
 	char test_buf[5] = {0};
 	b.iov_base = test_buf;
 	b.iov_len = 4;
-	TEST_CHECK(f.readv(0, &b, 1, ec) == 4);
+	TEST_EQUAL(f.readv(0, &b, 1, ec), 4);
+	if (ec)
+		fprintf(stderr, "readv failed: [%s] %s\n", ec.category().name(), ec.message().c_str());
 	TEST_CHECK(!ec);
 	TEST_CHECK(strcmp(test_buf, "test") == 0);
 	f.close();
