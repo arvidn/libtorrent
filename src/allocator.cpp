5d70d6b558f2a98f15c9cef7349620be6be1657c
Arvid Norberg <arvid@libtorrent.org>
2013-09-25 07:29:32 +0000

    remove invalid assert

--- src/allocator.cpp
+++ src/allocator.cpp
@@ -100,7 +100,6 @@ namespace libtorrent
 	char* page_aligned_allocator::malloc(size_type bytes)
 	{
 		TORRENT_ASSERT(bytes >= page_size());
-		TORRENT_ASSERT(bytes % page_size() == 0);
 #ifdef TORRENT_DEBUG_BUFFERS
 		int page = page_size();
 		int num_pages = (bytes + (page-1)) / page + 2;
