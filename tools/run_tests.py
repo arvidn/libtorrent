f1c9d5f8fb4679ef0f3e7c7586ffff778ce93fce
Arvid Norberg <arvid@libtorrent.org>
2013-10-03 15:47:58 +0000

    fix issue in run_tests.py

--- tools/run_tests.py
+++ tools/run_tests.py
@@ -100,15 +100,15 @@ def run_tests(toolset, tests, features, options, test_dir, time_limit, increment
 #			p.wait()
    
 		for t in tests:
+			options_copy = options[:]
+			if t != '': options_copy.append(t)
 			if t == '':
 				t = os.path.split(os.getcwd())[1]
 				# we can't pass in a launcher when just building, that only
 				# works for actual unit tests
-				if 'launcher=valgrind' in options:
-					options = options[:]
-					options.remove('launcher=valgrind')
-			cmdline = ['bjam', '--out-xml=%s' % xml_file, '-l%d' % time_limit, '--abbreviate-paths', toolset] + options + feature_list
-			if t != '': cmdline.append(t)
+				if 'launcher=valgrind' in options_copy:
+					options_copy.remove('launcher=valgrind')
+			cmdline = ['bjam', '--out-xml=%s' % xml_file, '-l%d' % time_limit, '--abbreviate-paths', toolset] + options_copy + feature_list
 
 #			print ' '.join(cmdline)
 
