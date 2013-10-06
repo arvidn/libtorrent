c99f0c120a7dc816bb1c2b869739ee3d59c66c11
Arvid Norberg <arvid@libtorrent.org>
2013-10-06 20:13:15 +0000

    display exit code in test detail page

--- tools/parse_test_results.py
+++ tools/parse_test_results.py
@@ -266,11 +266,19 @@ for branch_name in revs:
 					print >>details_file, '<table><tr><th>%s</th></tr>' % f
 					for t in platforms[p][toolset][f]:
 						details = platforms[p][toolset][f][t]
-						if details['status'] == 0: c = 'passed'
+						exitcode = details['status']
+						if exitcode == 0: c = 'passed'
 						else: c = 'failed'
+						error_state = '%d' % exitcode
+						if exitcode == 222:
+							error_state = 'valgrind error'
+						elif exitcode == 139:
+							error_state = 'crash'
+						elif exitcode == -1073740777:
+							error_state = 'timeout'
 						log_name = os.path.join('logs-%s-%d' % (branch_name, r), p + '~' + toolset + '~' + t + '~' + f.replace(' ', '.') + '.html')
 						print >>html, '<td title="%s %s"><a class="%s" href="%s"></a></td>' % (t, f, c, log_name)
-						print >>details_file, '<tr><td class="%s"><a href="%s">%s</a></td></tr>' % (c, os.path.split(log_name)[1], t)
+						print >>details_file, '<tr><td class="%s"><a href="%s">%s [%s]</a></td></tr>' % (c, os.path.split(log_name)[1], t, error_state)
 						save_log_file(log_name, project_name, branch_name, '%s - %s' % (t, f), int(details['timestamp']), details['output'])
 					print >>details_file, '</table>'
 
