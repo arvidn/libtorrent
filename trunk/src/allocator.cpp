c9223198eb8ddb09bec6f4186f2576ff1428ff23
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2013-11-29 09:41:53 +0000

    fix debug buffer build. add new Jamfile feature for it. attempt to make it build on windows

--- trunk/src/allocator.cpp
+++ trunk/src/allocator.cpp
@@ -58,7 +58,9 @@ POSSIBILITY OF SUCH DAMAGE.
 #endif
 
 #ifdef TORRENT_DEBUG_BUFFERS
+#ifndef TORRENT_WINDOWS
 #include <sys/mman.h>
+#endif
 #include "libtorrent/size_type.hpp"
 
 struct alloc_header
@@ -71,7 +73,7 @@ struct alloc_header
 #endif
 
 #if defined TORRENT_DEBUG_BUFFERS && (defined __linux__ || (defined __APPLE__ && MAC_OS_X_VERSION_MIN_REQUIRED >= 1050))
-	void print_backtrace(char* out, int len);
+	void print_backtrace(char* out, int len, int max_depth = 0);
 #endif
 
 namespace libtorrent
@@ -111,8 +113,18 @@ namespace libtorrent
 #if defined __linux__ || (defined __APPLE__ && MAC_OS_X_VERSION_MIN_REQUIRED >= 1050)
 		print_backtrace(h->stack, sizeof(h->stack));
 #endif
+
+#ifdef TORRENT_WINDOWS
+#define mprotect(buf, size, prot) VirtualProtect(buf, size, prot, NULK)
+#define PROT_READ PAGE_READONLY
+#endif
 		mprotect(ret, page, PROT_READ);
 		mprotect(ret + (num_pages-1) * page, page, PROT_READ);
+
+#ifdef TORRENT_WINDOWS
+#undef mprotect
+#undef PROT_READ
+#endif
 //		fprintf(stderr, "malloc: %p head: %p tail: %p size: %d\n", ret + page, ret, ret + page + bytes, int(bytes));
 
 		return ret + page;
@@ -141,6 +153,12 @@ namespace libtorrent
 	{
 
 #ifdef TORRENT_DEBUG_BUFFERS
+
+#ifdef TORRENT_WINDOWS
+#define mprotect(buf, size, prot) VirtualProtect(buf, size, prot, NULK)
+#define PROT_READ PAGE_READONLY
+#define PROT_WRITE PAGE_READWRITE
+#endif
 		int page = page_size();
 		// make the two surrounding pages non-readable and -writable
 		mprotect(block - page, page, PROT_READ | PROT_WRITE);
@@ -150,6 +168,12 @@ namespace libtorrent
 		mprotect(block + (num_pages-2) * page, page, PROT_READ | PROT_WRITE);
 //		fprintf(stderr, "free: %p head: %p tail: %p size: %d\n", block, block - page, block + h->size, int(h->size));
 		h->magic = 0;
+
+#ifdef TORRENT_WINDOWS
+#undef mprotect
+#undef PROT_READ
+#undef PROT_WRITE
+#endif
 
 #if defined __linux__ || (defined __APPLE__ && MAC_OS_X_VERSION_MIN_REQUIRED >= 1050)
 		print_backtrace(h->stack, sizeof(h->stack));
