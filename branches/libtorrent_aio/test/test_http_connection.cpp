a6f820452ec99ad0bcc5c385a9ad163a2e090d6b
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2013-12-26 17:41:11 +0000

    fix test_http_connection unit test

--- branches/libtorrent_aio/test/test_http_connection.cpp
+++ branches/libtorrent_aio/test/test_http_connection.cpp
@@ -35,6 +35,7 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "libtorrent/socket_io.hpp" // print_endpoint
 #include "libtorrent/connection_queue.hpp"
 #include "libtorrent/http_connection.hpp"
+#include "libtorrent/resolver.hpp"
 #include "setup_transfer.hpp"
 
 #include <fstream>
@@ -45,6 +46,7 @@ using namespace libtorrent;
 
 io_service ios;
 connection_queue cq(ios);
+resolver res(ios);
 
 int connect_handler_called = 0;
 int handler_called = 0;
@@ -117,7 +119,7 @@ void run_test(std::string const& url, int size, int status, int connected
 		<< " error: " << (ec?ec->message():"no error") << std::endl;
 
 	boost::shared_ptr<http_connection> h(new http_connection(ios, cq
-		, &::http_handler, true, 1024*1024, &::http_connect_handler));
+		, res, &::http_handler, true, 1024*1024, &::http_connect_handler));
 	h->get(url, seconds(1), 0, &ps);
 	ios.reset();
 	error_code e;
