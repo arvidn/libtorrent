2e261a495a74228352656b3682d35cf406dcfe31
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2014-01-20 10:11:07 +0000

    regenerated html

--- trunk/include/libtorrent/utp_stream.hpp
+++ trunk/include/libtorrent/utp_stream.hpp
@@ -375,7 +375,7 @@ public:
 	std::size_t write_some(Const_Buffers const& buffers, error_code& ec)
 	{
 		TORRENT_ASSERT(false && "not implemented!");
-		// TODO: 1 implement blocking write. Low priority since it's not used (yet)
+		// TODO: implement blocking write. Low priority since it's not used (yet)
 		return 0;
 	}
 
