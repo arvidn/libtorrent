9b128a72aa29c23c8f12d2c446309174de751665
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2014-03-23 00:45:52 +0000

    add missing add_files overload to the python binding. make python version of make_torrent a bit more sophisticated

--- branches/RC_0_16/bindings/python/make_torrent.py
+++ branches/RC_0_16/bindings/python/make_torrent.py
@@ -11,7 +11,30 @@ if len(sys.argv) < 3:
 input = os.path.abspath(sys.argv[1])
 
 fs = libtorrent.file_storage()
-libtorrent.add_files(fs, input)
+
+#def predicate(f):
+#	print f
+#	return True
+#libtorrent.add_files(fs, input, predicate)
+
+parent_input = os.path.split(input)[0]
+
+for root, dirs, files in os.walk(input):
+	# skip directories starting with .
+	if os.path.split(root)[1][0] == '.': continue
+
+	for f in files:
+		# skip files starting with .
+		if f[0] == '.': continue
+
+		# skip thumbs.db on windows
+		if f == 'Thumbs.db': continue
+
+		fname = os.path.join(root[len(parent_input)+1:], f)
+		size = os.path.getsize(os.path.join(parent_input, fname))
+		print '%10d kiB  %s' % (size / 1024, fname)
+		fs.add_file(fname, size);
+
 if fs.num_files() == 0:
 	print 'no files added'
 	sys.exit(1)
@@ -21,7 +44,10 @@ t = libtorrent.create_torrent(fs, 0, 4 * 1024 * 1024)
 t.add_tracker(sys.argv[2])
 t.set_creator('libtorrent %s' % libtorrent.version)
 
-libtorrent.set_piece_hashes(t, os.path.split(input)[0], lambda x: sys.stderr.write('.'))
+libtorrent.set_piece_hashes(t, parent_input, lambda x: sys.stderr.write('.'))
 sys.stderr.write('\n')
 
-print libtorrent.bencode(t.generate())
+f = open('out.torrent', 'wb+')
+print >>f, libtorrent.bencode(t.generate())
+f.close()
+
