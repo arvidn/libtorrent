fc60391f5322d193ea2f65d3b4c11ae46f0fc03a
Arvid Norberg <arvid@libtorrent.org>
2014-05-03 06:31:42 +0000

    fix exports for unit tests

--- include/libtorrent/ConvertUTF.h
+++ include/libtorrent/ConvertUTF.h
@@ -131,31 +131,32 @@ typedef enum {
 	lenientConversion
 } ConversionFlags;
 
-ConversionResult ConvertUTF8toUTF16 (
+TORRENT_EXTRA_EXPORT ConversionResult ConvertUTF8toUTF16 (
 		const UTF8** sourceStart, const UTF8* sourceEnd, 
 		UTF16** targetStart, UTF16* targetEnd, ConversionFlags flags);
 
-ConversionResult ConvertUTF16toUTF8 (
+TORRENT_EXTRA_EXPORT ConversionResult ConvertUTF16toUTF8 (
 		const UTF16** sourceStart, const UTF16* sourceEnd, 
 		UTF8** targetStart, UTF8* targetEnd, ConversionFlags flags);
 		
-ConversionResult ConvertUTF8toUTF32 (
+TORRENT_EXTRA_EXPORT ConversionResult ConvertUTF8toUTF32 (
 		const UTF8** sourceStart, const UTF8* sourceEnd, 
 		UTF32** targetStart, UTF32* targetEnd, ConversionFlags flags);
 
-ConversionResult ConvertUTF32toUTF8 (
+TORRENT_EXTRA_EXPORT ConversionResult ConvertUTF32toUTF8 (
 		const UTF32** sourceStart, const UTF32* sourceEnd, 
 		UTF8** targetStart, UTF8* targetEnd, ConversionFlags flags);
 		
-ConversionResult ConvertUTF16toUTF32 (
+TORRENT_EXTRA_EXPORT ConversionResult ConvertUTF16toUTF32 (
 		const UTF16** sourceStart, const UTF16* sourceEnd, 
 		UTF32** targetStart, UTF32* targetEnd, ConversionFlags flags);
 
-ConversionResult ConvertUTF32toUTF16 (
+TORRENT_EXTRA_EXPORT ConversionResult ConvertUTF32toUTF16 (
 		const UTF32** sourceStart, const UTF32* sourceEnd, 
 		UTF16** targetStart, UTF16* targetEnd, ConversionFlags flags);
 
-Boolean isLegalUTF8Sequence(const UTF8 *source, const UTF8 *sourceEnd);
+TORRENT_EXTRA_EXPORT Boolean isLegalUTF8Sequence(const UTF8 *source,
+		const UTF8 *sourceEnd);
 
 #ifdef __cplusplus
 }
