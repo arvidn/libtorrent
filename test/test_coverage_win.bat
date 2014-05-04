863a046e68621a2084ffd2aa6f2b8404897841a8
Arvid Norberg <arvid@libtorrent.org>
2014-05-04 00:44:30 +0000

    don't include deprecated functions in test coverage

--- test/test_coverage_win.bat
+++ test/test_coverage_win.bat
@@ -1,7 +1,7 @@
 REM this is just to get libtorrent built
-bjam -j8 msvc-12.0 boost=source invariant-checks=off asserts=off link=shared boost-link=shared test_primitives linkflags=/profile
+bjam -j8 msvc-12.0 boost=source invariant-checks=off asserts=off link=shared boost-link=shared test_primitives linkflags=/profile deprecated-functions=off
 
-Vsinstr -coverage ..\bin\msvc-12.0\debug\asserts-off\boost-link-shared\boost-source\debug-iterators-on\export-extra-on\invariant-checks-off\threading-multi\torrent.dll @vsinstr_excludes.rsp
+Vsinstr -coverage ..\bin\msvc-12.0\debug\asserts-off\boost-link-shared\boost-source\debug-iterators-on\deprecated-functions-off\export-extra-on\invariant-checks-off\threading-multi\torrent.dll @vsinstr_excludes.rsp
 
 REM prepare for running all the tests again
 del /s /q bin
@@ -13,7 +13,7 @@ set path=%path%;c:\Program Files (x86)\Microsoft Visual Studio 12.0\Team Tools\P
 Start vsperfmon -coverage -output:test.coverage
 
 REM Now we run all the unit tests to record test coverage
-bjam -j8 msvc-12.0 boost=source invariant-checks=off asserts=off link=shared boost-link=shared linkflags=/profile
+bjam -j8 msvc-12.0 boost=source invariant-checks=off asserts=off link=shared boost-link=shared linkflags=/profile deprecated-functions=off
 
 vsperfcmd -shutdown
 
