e291ab32907b10ff3aa448aace5420cc5fad4702
Arvid Norberg <arvid@libtorrent.org>
2014-06-08 21:15:34 +0000

    minor streaming improvements

--- src/torrent.cpp
+++ src/torrent.cpp
@@ -8793,6 +8793,11 @@ namespace libtorrent
 					, boost::bind(&peer_connection::download_queue_time, _1, 16*1024)
 					< boost::bind(&peer_connection::download_queue_time, _2, 16*1024));
 			}
+
+			// if this peer's download time exceeds 2 seconds, we're done.
+			// We don't want to build unreasonably long request queues
+			if (!peers.empty() && peers[0]->download_queue_time() > milliseconds(2000))
+				break;
 		}
 
 		// commit all the time critical requests
