2babbbcbc5802a5bbe47d7a3c06b6049d5256077
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2014-06-30 08:41:33 +0000

    fix bug in udp_socket when changing socks5 proxy quickly

--- trunk/src/udp_socket.cpp
+++ trunk/src/udp_socket.cpp
@@ -924,9 +924,15 @@ void udp_socket::on_connect(int ticket)
 		return;
 	}
 
-
 	if (m_abort) return;
 	if (is_closed()) return;
+
+	if (m_connection_ticket != -1)
+	{
+		// there's already an outstanding connect. Cancel it.
+		m_socks5_sock.close();
+		m_connection_ticket = -1;
+	}
 
 #if defined TORRENT_ASIO_DEBUGGING
 	add_outstanding_async("udp_socket::on_connected");
@@ -940,10 +946,10 @@ void udp_socket::on_connect(int ticket)
 	++m_outstanding_connect;
 #endif
 	m_socks5_sock.async_connect(tcp::endpoint(m_proxy_addr.address(), m_proxy_addr.port())
-		, boost::bind(&udp_socket::on_connected, this, _1));
+		, boost::bind(&udp_socket::on_connected, this, _1, ticket));
 }
 
-void udp_socket::on_connected(error_code const& e)
+void udp_socket::on_connected(error_code const& e, int ticket)
 {
 #if defined TORRENT_ASIO_DEBUGGING
 	complete_async("udp_socket::on_connected");
@@ -962,15 +968,13 @@ void udp_socket::on_connected(error_code const& e)
 	CHECK_MAGIC;
 
 	TORRENT_ASSERT(is_single_thread());
-	if (m_connection_ticket >= 0)
+	if (m_cc.done(ticket))
 	{
-		if (m_cc.done(m_connection_ticket))
+		// if the tickets mismatch, another connection attempt
+		// was initiated while waiting for this one to complete.
+		if (ticket == m_connection_ticket)
 			m_connection_ticket = -1;
 	}
-
-	if (m_abort) return;
-
-	if (e == asio::error::operation_aborted) return;
 
 	// we just called done, which means on_timeout
 	// won't be called. Decrement the outstanding
@@ -987,6 +991,14 @@ void udp_socket::on_connected(error_code const& e)
 		+ m_outstanding_resolve
 		+ m_outstanding_connect_queue
 		+ m_outstanding_socks);
+
+	if (e == asio::error::operation_aborted) return;
+
+	// if ticket != m_connection_ticket, it means m_connection_ticket
+	// will not have been reset, and it means we are still waiting
+	// for a connection attempt.
+	if (m_connection_ticket != -1) return;
+
 	if (m_abort) return;
 
 	if (e)
