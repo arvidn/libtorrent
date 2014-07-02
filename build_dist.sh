b0f91b8ed5a6e9d0e1fd398a6c84a5365d8470e0
Arvid Norberg <arvid@libtorrent.org>
2014-07-02 08:41:52 +0000

    fix openssl usage in unit tests makefile

--- build_dist.sh
+++ build_dist.sh
@@ -17,6 +17,6 @@ rm -f bindings/python/Makefile bindings/python/Makefile.in
 chmod a-x docs/*.rst docs/*.htm* src/*.cpp include/libtorrent/*.hpp
 
 ./autotool.sh
-./configure --enable-python-binding --enable-examples=yes --enable-tests=yes --with-boost-system=mt --with-boost-python=mt --enable-tests
-make V=1 -j8 distcheck
+./configure --enable-python-binding --enable-examples=yes --enable-encryption --enable-tests=yes --with-boost-system=mt --with-boost-python=mt --enable-tests
+make V=1 -j8 check distcheck
 
