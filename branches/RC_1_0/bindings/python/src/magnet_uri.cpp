b51f9d83043903ed406f3e5802d0344d096279e6
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2014-07-05 04:19:39 +0000

    fix compiler warnings

--- branches/RC_1_0/bindings/python/src/magnet_uri.cpp
+++ branches/RC_1_0/bindings/python/src/magnet_uri.cpp
@@ -24,11 +24,13 @@ namespace {
 
         allow_threading_guard guard;
 
+		  p.url = uri;
+
 #ifndef BOOST_NO_EXCEPTIONS
-        return add_magnet_uri(s, uri, p);
+        return s.add_torrent(p);
 #else
         error_code ec;
-        return add_magnet_uri(s, uri, p, ec);
+        return s.add_torrent(p, ec);
 #endif
     }
 #endif
