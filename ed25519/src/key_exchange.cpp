e3ff0823b8fbaaf27939ededb2cadadf5d3e17be
Arvid Norberg <arvid@libtorrent.org>
2014-07-12 05:55:40 +0000

    made ed25519 build as c++ to be able to use boost.config export and import macros. fixed CMakeList.txt

--- ed25519/src/key_exchange.cpp
+++ ed25519/src/key_exchange.cpp
@@ -1,7 +1,8 @@
-#include "ed25519.h"
+#include "ed25519.hpp"
 #include "fe.h"
 
-void ed25519_key_exchange(unsigned char *shared_secret, const unsigned char *public_key, const unsigned char *private_key) {
+void ed25519_key_exchange(unsigned char *shared_secret
+	, const unsigned char *public_key, const unsigned char *private_key) {
     unsigned char e[32];
     unsigned int i;
     
