70bce5929fb8d4e9167b79d18a7a4a6236b960bc
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2014-07-22 01:21:17 +0000

    separate out TORRENT_EXPORT from config.hpp for ed25519 build

--- branches/RC_1_0/include/libtorrent/config.hpp
+++ branches/RC_1_0/include/libtorrent/config.hpp
@@ -48,6 +48,8 @@ POSSIBILITY OF SUCH DAMAGE.
 #include <stdio.h> // for snprintf
 #include <limits.h> // for IOV_MAX
 
+#include "libtorrent/export.hpp"
+
 #if defined TORRENT_DEBUG_BUFFERS && !defined TORRENT_DISABLE_POOL_ALLOCATOR
 #error TORRENT_DEBUG_BUFFERS only works if you also disable pool allocators with TORRENT_DISABLE_POOL_ALLOCATOR
 #endif
@@ -91,40 +93,6 @@ POSSIBILITY OF SUCH DAMAGE.
 #define PRIx64 "llx"
 #define PRIu32 "u"
 #endif
-#endif
-
-// backwards compatibility with older versions of boost
-#if !defined BOOST_SYMBOL_EXPORT && !defined BOOST_SYMBOL_IMPORT
-# if defined _MSC_VER || defined __MINGW32__
-#  define BOOST_SYMBOL_EXPORT __declspec(dllexport)
-#  define BOOST_SYMBOL_IMPORT __declspec(dllimport)
-# elif __GNU__ >= 4
-#  define BOOST_SYMBOL_EXPORT __attribute__((visibility("default")))
-#  define BOOST_SYMBOL_IMPORT __attribute__((visibility("default")))
-# else
-#  define BOOST_SYMBOL_EXPORT
-#  define BOOST_SYMBOL_IMPORT
-# endif
-#endif
-
-#if defined TORRENT_BUILDING_SHARED
-# define TORRENT_EXPORT BOOST_SYMBOL_EXPORT
-#elif defined TORRENT_LINKING_SHARED
-# define TORRENT_EXPORT BOOST_SYMBOL_IMPORT
-#endif
-
-// when this is specified, export a bunch of extra
-// symbols, mostly for the unit tests to reach
-#if TORRENT_EXPORT_EXTRA
-# if defined TORRENT_BUILDING_SHARED
-#  define TORRENT_EXTRA_EXPORT BOOST_SYMBOL_EXPORT
-# elif defined TORRENT_LINKING_SHARED
-#  define TORRENT_EXTRA_EXPORT BOOST_SYMBOL_IMPORT
-# endif
-#endif
-
-#ifndef TORRENT_EXTRA_EXPORT
-# define TORRENT_EXTRA_EXPORT
 #endif
 
 // ======= GCC =========
@@ -441,10 +409,6 @@ int snprintf(char* buf, int len, char const* fmt, ...)
 
 #ifndef TORRENT_HAS_FALLOCATE
 #define TORRENT_HAS_FALLOCATE 1
-#endif
-
-#ifndef TORRENT_EXPORT
-# define TORRENT_EXPORT
 #endif
 
 #ifndef TORRENT_DEPRECATED_PREFIX
