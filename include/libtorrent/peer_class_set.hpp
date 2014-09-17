c471bcb49f3a5ed46edfc22b68a51f704425c45f
Arvid Norberg <arvid@libtorrent.org>
2014-09-17 07:23:41 +0000

    optimize peer classes a bit

--- include/libtorrent/peer_class_set.hpp
+++ include/libtorrent/peer_class_set.hpp
@@ -34,7 +34,7 @@ POSSIBILITY OF SUCH DAMAGE.
 #define TORRENT_PEER_CLASS_SET_HPP_INCLUDED
 
 #include "libtorrent/peer_class.hpp"
-#include <vector>
+#include <boost/array.hpp>
 
 namespace libtorrent {
 
@@ -42,24 +42,27 @@ namespace libtorrent {
 	// to it. Most notably, peer connections and torrents derive from this.
 	struct peer_class_set
 	{
+		peer_class_set() : m_size(0) {}
 		void add_class(peer_class_pool& pool, peer_class_t c);
 		bool has_class(peer_class_t c) const;
 		void remove_class(peer_class_pool& pool, peer_class_t c);
-		int num_classes() const { return m_class.size(); }
+		int num_classes() const { return m_size; }
 		peer_class_t class_at(int i) const
 		{
-			TORRENT_ASSERT(i >= 0 && i < int(m_class.size()));
+			TORRENT_ASSERT(i >= 0 && i < int(m_size));
 			return m_class[i];
 		}
 
 	private:
 
-		// if this object belongs to any peer-class, this
-		// vector contains all class IDs. Each ID refers
-		// to a an entry in m_ses.m_peer_classes which
-		// holds the metadata about the class. Classes
-		// affect bandwidth limits among other things
-		std::vector<peer_class_t> m_class;
+		// the number of elements used in the m_class array
+		boost::uint8_t m_size;
+
+		// if this object belongs to any peer-class, this vector contains all
+		// class IDs. Each ID refers to a an entry in m_ses.m_peer_classes which
+		// holds the metadata about the class. Classes affect bandwidth limits
+		// among other things
+		boost::array<peer_class_t, 15> m_class;
 	};
 }
 
