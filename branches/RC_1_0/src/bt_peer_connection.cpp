ed6bbcea214f1f81c4cb7556af2119dfaeddc95a
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2014-11-06 17:53:02 +0000

    merge bugfix from RC_0_16

--- branches/RC_1_0/src/bt_peer_connection.cpp
+++ branches/RC_1_0/src/bt_peer_connection.cpp
@@ -2429,7 +2429,7 @@ namespace libtorrent
 			if (is_disconnecting()) return;
 			
 			// read dh key, generate shared secret
-			if (m_dh_key_exchange->compute_secret(recv_buffer.begin) == -1)
+			if (m_dh_key_exchange->compute_secret(recv_buffer.begin) != 0)
 			{
 				disconnect(errors::no_memory);
 				return;
