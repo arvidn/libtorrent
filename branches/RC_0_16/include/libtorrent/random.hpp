23995a13db8f9f64e609b712615917c61da1523f
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2014-11-30 00:12:41 +0000

    fix test_ssl in non-ssl builds. fix python binding build without exceptions. fix test relying on random_seed(). fix .regression configuration to match build options in this branch

--- branches/RC_0_16/include/libtorrent/random.hpp
+++ branches/RC_0_16/include/libtorrent/random.hpp
@@ -1,7 +1,8 @@
+#include "libtorrent/config.hpp"
 #include <boost/cstdint.hpp>
 
 namespace libtorrent
 {
-	void random_seed(boost::uint32_t v);
+	TORRENT_EXTRA_EXPORT void random_seed(boost::uint32_t v);
 	boost::uint32_t random();
 }
