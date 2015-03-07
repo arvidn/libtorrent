3aa36c391c3f09396cae9f02c0c972a99e773e43
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2015-03-07 19:30:55 +0000

    extend bdecode unit test coverage

--- branches/bdecoder/test/test_bdecode.cpp
+++ branches/bdecoder/test/test_bdecode.cpp
@@ -983,8 +983,25 @@ int test_main()
 		TEST_EQUAL(print_entry(e2), str2);
 	}
 
-	// TODO: 4 make sure clear actually clears everything
-	// (size and token cache)
+	// make sure it's safe to reuse bdecode_nodes after clear() is called
+	{
+		char b1[] = "d1:ai1e1:b3:foo1:cli1ei2ee1:dd1:xi1eee";
+		char b2[] = "li1ei2ee";
+
+		bdecode_node e;
+		error_code ec;
+		int ret = bdecode(b1, b1 + sizeof(b1)-1, e, ec);
+		TEST_EQUAL(ret, 0);
+		TEST_EQUAL(e.type(), bdecode_node::dict_t);
+		TEST_EQUAL(e.dict_size(), 4);
+		TEST_EQUAL(e.dict_at(1).first, "b");
+
+		ret = bdecode(b2, b2 + sizeof(b2)-1, e, ec);
+		TEST_EQUAL(ret, 0);
+		TEST_EQUAL(e.type(), bdecode_node::list_t);
+		TEST_EQUAL(e.list_size(), 2);
+		TEST_EQUAL(e.list_int_value_at(1), 2);
+	}
 
 	return 0;
 }
