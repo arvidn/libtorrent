96b9d2612c41d8b1d7f1b83d6dbb7df121d14651
arvidn <arvidn@a83610d8-ad2a-0410-a6ab-fc0612d85776>
2015-03-07 22:04:13 +0000

    unit tests

--- branches/bdecoder/test/test_bdecode.cpp
+++ branches/bdecoder/test/test_bdecode.cpp
@@ -1028,8 +1028,26 @@ int test_main()
 		TEST_EQUAL(e3.dict_at(1).first, "b");
 	}
 
+	// non-owning references
+	{
+		char b1[] = "d1:ai1e1:b3:foo1:cli1ei2ee1:dd1:xi1eee";
+
+		bdecode_node e1;
+		error_code ec;
+		int ret = bdecode(b1, b1 + sizeof(b1)-1, e1, ec);
+		TEST_EQUAL(ret, 0);
+		TEST_EQUAL(e1.type(), bdecode_node::dict_t);
+
+		bdecode_node e2 = e1.non_owning();
+
+		TEST_EQUAL(e2.type(), bdecode_node::dict_t);
+
+		e1.clear();
+
+		// e2 is invalid now
+	}
+
 	// TODO: test switch_underlying_buffer
-	// TODO: non_owning()
 
 	return 0;
 }
