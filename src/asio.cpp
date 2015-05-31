91197005e9dd4f0e3ceb996091b07d6f6ed040c9
Arvid Norberg <arvid@libtorrent.org>
2015-05-31 09:02:42 +0000

    more msvc fixes

--- src/asio.cpp
+++ src/asio.cpp
@@ -1,14 +1,13 @@
 // builds all boost.asio source as a separate compilation unit
 #include <boost/version.hpp>
+#include <boost/preprocessor/facilities/is_empty.hpp>
 
 #include "libtorrent/config.hpp"
 
 // only define BOOST_ASIO_DECL if it hasn't already been defined
 // or if it has been defined to an empty string
-#define TORRENT_IS_EMPTY_IMPL(VAL)  VAL ## 1
-#define TORRENT_IS_EMPTY(VAL) TORRENT_IS_EMPTY_IMPL(VAL)
 
-#if !defined BOOST_ASIO_DECL || TORRENT_IS_EMPTY(BOOST_ASIO_DECL) == 1
+#if !defined BOOST_ASIO_DECL || !BOOST_PP_IS_EMPTY(BOOST_ASIO_DECL)
 #ifdef BOOST_ASIO_DECL
 #undef BOOST_ASIO_DECL
 #endif
