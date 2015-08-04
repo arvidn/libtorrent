94c59354fa51a57d179cafc615cf18f5f6958c72
Alden Torres <aldenml@gmail.com>
2015-08-03 23:01:46 -0400

    Removed minimal optimization inside silly_hash to avoid another include.

--- src/file.cpp
+++ src/file.cpp
@@ -62,7 +62,6 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #ifdef TORRENT_DISK_STATS
 #include "libtorrent/io.hpp"
-#include "libtorrent/storage.hpp"
 #endif
 
 #include <sys/stat.h>
@@ -1275,8 +1274,6 @@ namespace libtorrent
 #ifdef TORRENT_DISK_STATS
 	boost::uint32_t silly_hash(std::string const& str)
 	{
-		if (!default_storage::disk_write_access_log()) return 0;
-
 		boost::uint32_t ret = 1;
 		for (int i = 0; i < str.size(); ++i)
 		{
