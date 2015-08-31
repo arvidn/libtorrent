2382dd8d61b8a725ac00c6ff7585e3b74d403bc3
arvidn <arvid@libtorrent.org>
2015-08-30 20:41:21 -0400

    remove TODO comment

--- include/libtorrent/linked_list.hpp
+++ include/libtorrent/linked_list.hpp
@@ -37,7 +37,6 @@ POSSIBILITY OF SUCH DAMAGE.
 
 namespace libtorrent
 {
-	// TODO: 3 enable_if T derives from list_node<T>
 	template <typename T>
 	struct list_node
 	{
@@ -65,6 +64,8 @@ namespace libtorrent
 	};
 
 	// TOOD: 3 add a unit test
+	// T must derive from list_node<T>. Having an enable_if here would require T
+	// to be a complete type, which is a bit too restrictive.
 	template <typename T>
 	struct linked_list
 	{
