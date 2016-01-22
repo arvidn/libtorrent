388652223c83f09d9d6dc0651d6194824f0c25d8
arvidn <arvid@cs.umu.se>
2016-01-22 17:20:59 -0500

    fix

--- tools/fuzz_torrent.cpp
+++ tools/fuzz_torrent.cpp
@@ -136,7 +136,7 @@ void print_string(std::string& output, std::string str)
 	{
 		static mt19937 random_engine(str_seed);
 		uniform_int_distribution<boost::uint8_t> d(0, 255);
-		for (int i = 0; i < str.size(); ++i)
+		for (int i = 0; i < int(str.size()); ++i)
 			str[i] = d(random_engine);
 
 		print_ascii_number(output, str.size());
