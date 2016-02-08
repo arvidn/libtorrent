e7a0a9f49a8ba5c9b0a8e48f06fff4f1a8ba5501
arvidn <arvid@cs.umu.se>
2016-02-08 17:56:59 -0500

    fix properly deprecating resolve-countries feature

--- include/libtorrent/torrent.hpp
+++ include/libtorrent/torrent.hpp
@@ -1501,6 +1501,8 @@ namespace libtorrent
 		// paused or auto_managed from the resume data
 		const bool m_override_resume_data:1;
 
+#ifndef TORRENT_NO_DEPRECATE
+#ifndef TORRENT_DISABLE_RESOLVE_COUNTRIES
 		// this is true while there is a country
 		// resolution in progress. To avoid flodding
 		// the DNS request queue, only one ip is resolved
@@ -1510,6 +1512,8 @@ namespace libtorrent
 		// this is true if the user has enabled
 		// country resolution in this torrent
 		bool m_resolve_countries:1;
+#endif
+#endif
 
 		// set to false when saving resume data. Set to true
 		// whenever something is downloaded
