a9d12766d47272ea401063c56319f1f002e599e6
arvidn <arvid@cs.umu.se>
2016-03-20 16:14:29 -0400

    fix typo in hard_link()

--- src/file.cpp
+++ src/file.cpp
@@ -538,7 +538,7 @@ namespace libtorrent
 		// TODO: 3 find out what error code is reported when the filesystem
 		// does not support hard links.
 		DWORD error = GetLastError();
-		if (error != ERROR_NOT_SUPPORTED || error != ERROR_ACCESS_DENIED)
+		if (error != ERROR_NOT_SUPPORTED && error != ERROR_ACCESS_DENIED)
 		{
 			// it's possible CreateHardLink will copy the file internally too,
 			// if the filesystem does not support it.
