d2f5a61da88200f441dd2a9e34a218c169dca0af
arvidn <arvid@cs.umu.se>
2016-04-06 19:09:42 -0400

    add more slack to utp test

--- test/test_utp.cpp
+++ test/test_utp.cpp
@@ -111,7 +111,7 @@ void test_transfer()
 	boost::tie(tor1, tor2, ignore) = setup_transfer(&ses1, &ses2, 0
 		, true, false, true, "_utp", 0, &t, false, &atp);
 
-	for (int i = 0; i < 6; ++i)
+	for (int i = 0; i < 12; ++i)
 	{
 		print_alerts(ses1, "ses1", true, true, true);
 		print_alerts(ses2, "ses2", true, true, true);
