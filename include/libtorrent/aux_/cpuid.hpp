97c536ae7064b3c2f155536272aa84e63ee3d09b
arvidn <arvid@cs.umu.se>
2016-06-28 20:32:05 -0400

    fixed warnings

--- include/libtorrent/aux_/cpuid.hpp
+++ include/libtorrent/aux_/cpuid.hpp
@@ -38,10 +38,10 @@ POSSIBILITY OF SUCH DAMAGE.
 namespace libtorrent { namespace aux
 {
 	// initialized by static initializers (in cpuid.cpp)
-	TORRENT_EXTRA_EXPORT extern bool sse42_support;
-	TORRENT_EXTRA_EXPORT extern bool mmx_support;
-	TORRENT_EXTRA_EXPORT extern bool arm_neon_support;
-	TORRENT_EXTRA_EXPORT extern bool arm_crc32c_support;
+	TORRENT_EXTRA_EXPORT extern bool const sse42_support;
+	TORRENT_EXTRA_EXPORT extern bool const mmx_support;
+	TORRENT_EXTRA_EXPORT extern bool const arm_neon_support;
+	TORRENT_EXTRA_EXPORT extern bool const arm_crc32c_support;
 } }
 
 #endif // TORRENT_CPUID_HPP_INCLUDED
