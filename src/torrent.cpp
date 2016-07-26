48ca20968e66d73464e85cc0dca8b1fa8cebc7f5
terry zhao <zhaohaiyang_1988@163.com>
2016-07-26 13:13:28 +0800

    fix value of current_tracker when all tracker failed (#932)
    fix torrent_status::current_tracker when all trackers fail.

--- src/torrent.cpp
+++ src/torrent.cpp
@@ -12099,7 +12099,8 @@ namespace libtorrent
 			std::vector<announce_entry>::const_iterator i;
 			for (i = m_trackers.begin(); i != m_trackers.end(); ++i)
 			{
-				if (!i->updating) continue;
+				if (i->updating) continue;
+				if (!i->verified) continue;
 				st->current_tracker = i->url;
 				break;
 			}
