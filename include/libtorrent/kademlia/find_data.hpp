1a0c6f5e61913f434e4fa6453ea20770c499d307
Alden Torres <aldenml@gmail.com>
2016-09-11 20:49:15 -0400

    more dht log optimizations (#1079)

--- include/libtorrent/kademlia/find_data.hpp
+++ include/libtorrent/kademlia/find_data.hpp
@@ -57,7 +57,7 @@ struct find_data : traversal_algorithm
 {
 	typedef std::function<void(std::vector<std::pair<node_entry, std::string>> const&)> nodes_callback;
 
-	find_data(node& node, node_id const& target
+	find_data(node& dht_node, node_id const& target
 		, nodes_callback const& ncallback);
 
 	void got_write_token(node_id const& n, std::string write_token);
