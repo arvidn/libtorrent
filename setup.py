0b704d1725ebf0bafc3f4f3c603a079f72348a96
Stuart Axon <stuaxo2@yahoo.com>
2016-09-21 21:46:20 +0100

    Workaround for python3 in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 
 import os
+import sys
+
+if sys.version_info >= (3,0):
+    def execfile(filename):
+        exec(compile(open(filename, "rb").read(), filename, 'exec'), globals, locals)
+
 os.chdir('bindings/python')
 execfile('setup.py')
