41c29834969709762960b095d449e59498cdac12
Stuart Axon <stuaxo2@yahoo.com>
2016-09-21 21:53:03 +0100

    Update setup.py
    globals and locals are functions - should have checked before chucking this in.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 if sys.version_info >= (3,0):
     def execfile(filename):
-        exec(compile(open(filename, "rb").read(), filename, 'exec'), globals, locals)
+        exec(compile(open(filename, "rb").read(), filename, 'exec'), globals(), locals())
 
 os.chdir('bindings/python')
 execfile('setup.py')
