e6328ee5240abeba3022e3d410108715e0cc3444
Stuart Axon <stuaxo2@yahoo.com>
2016-09-22 10:26:47 +0100

    Update setup.py
    Much simpler / nicer.

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@
 import os
 import sys
 
-if sys.version_info >= (3,0):
-    def execfile(filename):
-        exec(compile(open(filename, "rb").read(), filename, 'exec'), globals(), locals())
-
 os.chdir('bindings/python')
-execfile('setup.py')
+with open('setup.py) as filename:
+    exec(filename.read())
