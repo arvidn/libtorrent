857a14f8e2045d30ea119220b98c36ac00f78785
tnextday <fw2k4@163.com>
2016-11-15 09:41:00 +0800

    update the m_last_sent timestamp when keepalive has been written.

--- src/peer_connection.cpp
+++ src/peer_connection.cpp
@@ -6518,7 +6518,9 @@ namespace libtorrent
 		peer_log(peer_log_alert::outgoing_message, "KEEPALIVE");
 #endif
 
-		write_keepalive();
+		if (write_keepalive()) {
+			m_last_sent = aux::time_now();
+		}
 	}
 
 	bool peer_connection::is_seed() const
