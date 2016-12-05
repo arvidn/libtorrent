4142dfe5eb5acee6e4473bb673e3a84bf500daa2
Alden Torres <aldenml@gmail.com>
2016-12-05 14:49:45 -0500

    headers and code cleanup in udp_socket code

--- include/libtorrent/aux_/session_impl.hpp
+++ include/libtorrent/aux_/session_impl.hpp
@@ -40,6 +40,7 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "libtorrent/torrent_peer.hpp"
 #include "libtorrent/torrent_peer_allocator.hpp"
 #include "libtorrent/performance_counters.hpp" // for counters
+#include "libtorrent/aux_/allocating_handler.hpp"
 
 #ifdef TORRENT_USE_OPENSSL
 #include "libtorrent/ssl_stream.hpp"
