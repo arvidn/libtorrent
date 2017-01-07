63cb07d29f73af2b13c02ee94c9d326dc20030e9
arvidn <arvid@cs.umu.se>
2017-01-07 01:49:42 -0500

    fix tracker interval test

--- test/test.cpp
+++ test/test.cpp
@@ -50,7 +50,7 @@ void report_failure(char const* err, char const* file, int line)
 {
 	char buf[500];
 	snprintf(buf, sizeof(buf), "\x1b[41m***** %s:%d \"%s\" *****\x1b[0m\n", file, line, err);
-	fprintf(stderr, "\n%s\n", buf);
+	printf("\n%s\n", buf);
 	failure_strings.push_back(buf);
 	++_g_test_failures;
 }
