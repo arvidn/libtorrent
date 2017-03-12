6d2d736cecce0af274dd651dd1f562716b625d92
arvidn <arvid@cs.umu.se>
2017-03-12 13:03:26 -0400

    fix test_ssl.cpp build with newer versions of boost

--- test/test_ssl.cpp
+++ test/test_ssl.cpp
@@ -51,7 +51,6 @@ POSSIBILITY OF SUCH DAMAGE.
 #include <boost/asio/connect.hpp>
 
 #ifdef TORRENT_USE_OPENSSL
-#include <boost/asio/ssl/error.hpp> // for asio::error::get_ssl_category()
 #include <boost/asio/ssl.hpp>
 
 #include "libtorrent/aux_/disable_warnings_pop.hpp"
