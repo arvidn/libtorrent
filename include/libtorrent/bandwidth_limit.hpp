85cf521145e421934406d2d1e4a2108a98f72136
d-komarov <d.ma.johnny@gmail.com>
2017-07-12 07:35:12 +0300

    fix bandwith rate limit calculation (#2134)
    back-port of fc0cbfb7891b34541065d3ae763ade9cc8a95a10

--- include/libtorrent/bandwidth_limit.hpp
+++ include/libtorrent/bandwidth_limit.hpp
@@ -47,7 +47,7 @@ namespace libtorrent {
 // member of peer_connection
 struct TORRENT_EXTRA_EXPORT bandwidth_channel
 {
-	static const int inf = boost::integer_traits<int>::const_max;
+	static boost::int32_t const inf = boost::integer_traits<boost::int32_t>::const_max;
 
 	bandwidth_channel();
 
@@ -55,8 +55,9 @@ struct TORRENT_EXTRA_EXPORT bandwidth_channel
 	void throttle(int limit);
 	int throttle() const
 	{
-		TORRENT_ASSERT_VAL(m_limit < INT_MAX, m_limit);
-		return int(m_limit);
+		TORRENT_ASSERT_VAL(m_limit >= 0, m_limit);
+		TORRENT_ASSERT_VAL(m_limit < inf, m_limit);
+		return m_limit;
 	}
 
 	int quota_left() const;
@@ -96,10 +97,9 @@ private:
 
 	// the limit is the number of bytes
 	// per second we are allowed to use.
-	boost::int64_t m_limit;
+	boost::int32_t m_limit;
 };
 
 }
 
 #endif
-
