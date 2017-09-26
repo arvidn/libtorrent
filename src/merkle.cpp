ba7f90c35cf9e5a1127348ca3a9b3e3e17f9ace8
Steven Siloti <ssiloti@bittorrent.com>
2017-09-26 10:42:19 -0700

    add utility functions to build merkle trees

--- src/merkle.cpp
+++ src/merkle.cpp
@@ -31,17 +31,26 @@ POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include "libtorrent/aux_/merkle.hpp"
+#include "libtorrent/aux_/vector.hpp"
 
 namespace libtorrent {
 
-	int merkle_get_parent(int tree_node)
+	int merkle_to_flat_index(int const layer, int const offset)
+	{
+		TORRENT_ASSERT(layer >= 0);
+		TORRENT_ASSERT(offset >= 0);
+		TORRENT_ASSERT(layer < int(sizeof(int) * 8));
+		return (1 << layer) - 1 + offset;
+	}
+
+	int merkle_get_parent(int const tree_node)
 	{
 		// node 0 doesn't have a parent
 		TORRENT_ASSERT(tree_node > 0);
 		return (tree_node - 1) / 2;
 	}
 
-	int merkle_get_sibling(int tree_node)
+	int merkle_get_sibling(int const tree_node)
 	{
 		// node 0 doesn't have a sibling
 		TORRENT_ASSERT(tree_node > 0);
@@ -50,20 +59,120 @@ namespace libtorrent {
 		return tree_node + ((tree_node&1)?1:-1);
 	}
 
-	int merkle_num_nodes(int leafs)
+	int merkle_get_first_child(int const tree_node)
 	{
-		TORRENT_ASSERT(leafs > 0);
-		return (leafs << 1) - 1;
+		return tree_node * 2 + 1;
 	}
 
-	int merkle_num_leafs(int pieces)
+	int merkle_num_nodes(int const leafs)
 	{
-		TORRENT_ASSERT(pieces > 0);
+		TORRENT_ASSERT(leafs > 0);
+		TORRENT_ASSERT(leafs <= (std::numeric_limits<int>::max() / 2) + 1);
+		// This is a way to calculate: (leafs << 1) - 1 without requiring an extra
+		// bit in the far left. The first 1 we subtract is worth 2 after we
+		// multiply by 2, so by just adding back one, we have effectively
+		// subtracted one from the result of multiplying by 2
+		return ((leafs - 1) << 1) + 1;
+	}
+
+	int merkle_num_leafs(int const blocks)
+	{
+		TORRENT_ASSERT(blocks > 0);
+		TORRENT_ASSERT(blocks <= std::numeric_limits<int>::max() / 2);
 		// round up to nearest 2 exponent
 		int ret = 1;
-		while (pieces > ret) ret <<= 1;
+		while (blocks > ret) ret <<= 1;
 		return ret;
 	}
 
+	int merkle_num_layers(int leaves)
+	{
+		// leaves must be a power of 2
+		TORRENT_ASSERT((leaves & (leaves - 1)) == 0);
+		int layers = 0;
+		while (leaves > 1)
+		{
+			++layers;
+			leaves >>= 1;
+		}
+		return layers;
+	}
+
+	void merkle_fill_tree(span<sha256_hash> tree, int const num_leafs)
+	{
+		merkle_fill_tree(tree, num_leafs, merkle_num_nodes(num_leafs) - num_leafs);
+	}
+
+	void merkle_fill_tree(span<sha256_hash> tree, int const num_leafs, int level_start)
+	{
+		TORRENT_ASSERT(level_start >= 0);
+		TORRENT_ASSERT(num_leafs >= 1);
+
+		int level_size = num_leafs;
+		while (level_size > 1)
+		{
+			int parent = merkle_get_parent(level_start);
+			for (int i = level_start; i < level_start + level_size; i += 2, ++parent)
+			{
+				hasher256 h;
+				h.update(tree[i]);
+				h.update(tree[i + 1]);
+				tree[parent] = h.final();
+			}
+			level_start = merkle_get_parent(level_start);
+			level_size /= 2;
+		}
+		TORRENT_ASSERT(level_size == 1);
+	}
+
+	void merkle_clear_tree(span<sha256_hash> tree, int const num_leafs, int level_start)
+	{
+		TORRENT_ASSERT(num_leafs >= 1);
+		TORRENT_ASSERT(level_start > 0);
+		TORRENT_ASSERT(level_start < tree.size());
+		TORRENT_ASSERT(level_start + num_leafs <= tree.size());
+		// the range of nodes must be within a single level
+		TORRENT_ASSERT(merkle_get_layer(level_start) == merkle_get_layer(level_start + num_leafs - 1));
+
+		int level_size = num_leafs;
+		for (;;)
+		{
+			for (int i = level_start; i < level_start + level_size; ++i)
+				tree[i].clear();
+			if (level_size == 1) break;
+			level_start = merkle_get_parent(level_start);
+			level_size /= 2;
+		}
+		TORRENT_ASSERT(level_size == 1);
+	}
+
+	sha256_hash merkle_root(span<sha256_hash const> leaves, sha256_hash const& pad)
+	{
+		int const num_blocks = int(leaves.size());
+		int const num_leafs = merkle_num_leafs(num_blocks);
+		int const num_nodes = merkle_num_nodes(num_leafs);
+		int const first_leaf = num_nodes - num_leafs;
+		aux::vector<sha256_hash> merkle_tree(num_nodes);
+		for (int i = 0; i < num_blocks; ++i)
+			merkle_tree[first_leaf + i] = leaves[i];
+		for (int i = num_blocks; i < num_leafs; ++i)
+			merkle_tree[first_leaf + i] = pad;
+
+		merkle_fill_tree(merkle_tree, num_leafs);
+		return merkle_tree[0];
+	}
+
+	int merkle_get_layer(int idx)
+	{
+		TORRENT_ASSERT(idx >= 0);
+		int layer = 1;
+		while (idx > (1 << layer) - 2) layer++;
+		return layer - 1;
+	}
+
+	int merkle_get_layer_offset(int idx)
+	{
+		return idx - ((1 << merkle_get_layer(idx)) - 1);
+	}
 }
 
--- test/test_merkle.cpp
+++ test/test_merkle.cpp
@@ -32,11 +32,12 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #include "test.hpp"
 #include "libtorrent/aux_/merkle.hpp"
+#include <iostream>
 
-TORRENT_TEST(merkle)
+using namespace lt;
+
+TORRENT_TEST(num_leafs)
 {
-	using namespace lt;
-
 	// test merkle_*() functions
 
 	// this is the structure:
@@ -64,7 +65,10 @@ TORRENT_TEST(merkle)
 	TEST_EQUAL(merkle_num_leafs(16), 16);
 	TEST_EQUAL(merkle_num_leafs(17), 32);
 	TEST_EQUAL(merkle_num_leafs(18), 32);
+}
 
+TORRENT_TEST(get_parent)
+{
 	// parents
 	TEST_EQUAL(merkle_get_parent(1), 0);
 	TEST_EQUAL(merkle_get_parent(2), 0);
@@ -80,7 +84,10 @@ TORRENT_TEST(merkle)
 	TEST_EQUAL(merkle_get_parent(12), 5);
 	TEST_EQUAL(merkle_get_parent(13), 6);
 	TEST_EQUAL(merkle_get_parent(14), 6);
+}
 
+TORRENT_TEST(get_sibling)
+{
 	// siblings
 	TEST_EQUAL(merkle_get_sibling(1), 2);
 	TEST_EQUAL(merkle_get_sibling(2), 1);
@@ -96,12 +103,371 @@ TORRENT_TEST(merkle)
 	TEST_EQUAL(merkle_get_sibling(12), 11);
 	TEST_EQUAL(merkle_get_sibling(13), 14);
 	TEST_EQUAL(merkle_get_sibling(14), 13);
+}
 
-	// total number of nodes given the number of leafs
+TORRENT_TEST(num_nodes)
+{
+	// total number of nodes given the number of leaves
 	TEST_EQUAL(merkle_num_nodes(1), 1);
 	TEST_EQUAL(merkle_num_nodes(2), 3);
 	TEST_EQUAL(merkle_num_nodes(4), 7);
 	TEST_EQUAL(merkle_num_nodes(8), 15);
 	TEST_EQUAL(merkle_num_nodes(16), 31);
+}
+
+TORRENT_TEST(get_layer)
+{
+	// this is the structure:
+	//             0
+	//      1              2
+	//   3      4       5       6
+	//  7 8    9 10   11 12   13 14
+
+	TEST_EQUAL(merkle_get_layer(0), 0);
+	TEST_EQUAL(merkle_get_layer(1), 1);
+	TEST_EQUAL(merkle_get_layer(2), 1);
+	TEST_EQUAL(merkle_get_layer(3), 2);
+	TEST_EQUAL(merkle_get_layer(4), 2);
+	TEST_EQUAL(merkle_get_layer(5), 2);
+	TEST_EQUAL(merkle_get_layer(6), 2);
+	TEST_EQUAL(merkle_get_layer(7), 3);
+	TEST_EQUAL(merkle_get_layer(8), 3);
+	TEST_EQUAL(merkle_get_layer(9), 3);
+	TEST_EQUAL(merkle_get_layer(10), 3);
+	TEST_EQUAL(merkle_get_layer(11), 3);
+	TEST_EQUAL(merkle_get_layer(12), 3);
+	TEST_EQUAL(merkle_get_layer(13), 3);
+	TEST_EQUAL(merkle_get_layer(14), 3);
+	TEST_EQUAL(merkle_get_layer(15), 4);
+}
+
+TORRENT_TEST(get_layer_offset)
+{
+	// given a node index, how many steps from the left of the tree is that node?
+	TEST_EQUAL(merkle_get_layer_offset(0), 0);
+	TEST_EQUAL(merkle_get_layer_offset(1), 0);
+	TEST_EQUAL(merkle_get_layer_offset(2), 1);
+	TEST_EQUAL(merkle_get_layer_offset(3), 0);
+	TEST_EQUAL(merkle_get_layer_offset(4), 1);
+	TEST_EQUAL(merkle_get_layer_offset(5), 2);
+	TEST_EQUAL(merkle_get_layer_offset(6), 3);
+	TEST_EQUAL(merkle_get_layer_offset(7), 0);
+	TEST_EQUAL(merkle_get_layer_offset(8), 1);
+	TEST_EQUAL(merkle_get_layer_offset(9), 2);
+	TEST_EQUAL(merkle_get_layer_offset(10), 3);
+	TEST_EQUAL(merkle_get_layer_offset(11), 4);
+	TEST_EQUAL(merkle_get_layer_offset(12), 5);
+	TEST_EQUAL(merkle_get_layer_offset(13), 6);
+	TEST_EQUAL(merkle_get_layer_offset(14), 7);
+	TEST_EQUAL(merkle_get_layer_offset(15), 0);
+}
+
+TORRENT_TEST(merkle_num_layers)
+{
+	TEST_EQUAL(merkle_num_layers(0), 0);
+	TEST_EQUAL(merkle_num_layers(1), 0);
+	TEST_EQUAL(merkle_num_layers(2), 1);
+	TEST_EQUAL(merkle_num_layers(4), 2);
+	TEST_EQUAL(merkle_num_layers(8), 3);
+	TEST_EQUAL(merkle_num_layers(16), 4);
+}
+
+TORRENT_TEST(merkle_get_first_child)
+{
+	// this is the structure:
+	//             0
+	//      1              2
+	//   3      4       5       6
+	//  7 8    9 10   11 12   13 14
+	TEST_EQUAL(merkle_get_first_child(0), 1);
+	TEST_EQUAL(merkle_get_first_child(1), 3);
+	TEST_EQUAL(merkle_get_first_child(2), 5);
+	TEST_EQUAL(merkle_get_first_child(3), 7);
+	TEST_EQUAL(merkle_get_first_child(4), 9);
+	TEST_EQUAL(merkle_get_first_child(5), 11);
+	TEST_EQUAL(merkle_get_first_child(6), 13);
+	TEST_EQUAL(merkle_get_first_child(7), 15);
+	TEST_EQUAL(merkle_get_first_child(8), 17);
+	TEST_EQUAL(merkle_get_first_child(9), 19);
+	TEST_EQUAL(merkle_get_first_child(10), 21);
+	TEST_EQUAL(merkle_get_first_child(11), 23);
+	TEST_EQUAL(merkle_get_first_child(12), 25);
+	TEST_EQUAL(merkle_get_first_child(13), 27);
+	TEST_EQUAL(merkle_get_first_child(14), 29);
+	TEST_EQUAL(merkle_get_first_child(15), 31);
+	TEST_EQUAL(merkle_get_first_child(16), 33);
+}
+
+TORRENT_TEST(merkle_to_flat_index)
+{
+	TEST_EQUAL(merkle_to_flat_index(0, 0), 0);
+	TEST_EQUAL(merkle_to_flat_index(1, 0), 1);
+	TEST_EQUAL(merkle_to_flat_index(1, 1), 2);
+	TEST_EQUAL(merkle_to_flat_index(2, 0), 3);
+	TEST_EQUAL(merkle_to_flat_index(2, 1), 4);
+	TEST_EQUAL(merkle_to_flat_index(2, 2), 5);
+	TEST_EQUAL(merkle_to_flat_index(2, 3), 6);
+	TEST_EQUAL(merkle_to_flat_index(3, 0), 7);
+	TEST_EQUAL(merkle_to_flat_index(3, 1), 8);
+	TEST_EQUAL(merkle_to_flat_index(3, 2), 9);
+	TEST_EQUAL(merkle_to_flat_index(3, 3), 10);
+	TEST_EQUAL(merkle_to_flat_index(3, 4), 11);
+	TEST_EQUAL(merkle_to_flat_index(3, 5), 12);
+	TEST_EQUAL(merkle_to_flat_index(3, 6), 13);
+	TEST_EQUAL(merkle_to_flat_index(3, 7), 14);
+}
+
+namespace {
+sha256_hash H(sha256_hash left, sha256_hash right)
+{
+	hasher256 st;
+	st.update(left);
+	st.update(right);
+	return st.final();
+}
+}
+
+using v = std::vector<sha256_hash>;
+sha256_hash const a("11111111111111111111111111111111");
+sha256_hash const b("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+sha256_hash const c("cccccccccccccccccccccccccccccccc");
+sha256_hash const d("dddddddddddddddddddddddddddddddd");
+sha256_hash const e("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+sha256_hash const f("ffffffffffffffffffffffffffffffff");
+sha256_hash const g("gggggggggggggggggggggggggggggggg");
+sha256_hash const h("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+
+// 0 and 1
+sha256_hash const o;
+sha256_hash const l("11111111111111111111111111111111");
+
+// combinations
+sha256_hash const ab = H(a,b);
+sha256_hash const cd = H(c,d);
+sha256_hash const ef = H(e,f);
+sha256_hash const gh = H(g,h);
+
+sha256_hash const ad = H(ab,cd);
+sha256_hash const eh = H(ef,gh);
+
+sha256_hash const ah = H(ad,eh);
+
+TORRENT_TEST(merkle_fill_tree)
+{
+	// fill whole tree
+	{
+		v tree{
+		       o,
+		   o,      o,
+		  o, o,   o, o,
+		a,b,c,d,e,f,g,h};
+
+		merkle_fill_tree(tree, 8, 7);
+
+		TEST_CHECK((tree ==
+		     v{
+		       ah,
+		   ad,     eh,
+		 ab, cd, ef, gh,
+		a,b,c,d,e,f,g,h}));
+	}
+
+	// fill left side of the tree
+	{
+		v tree{
+		       o,
+		   o,      o,
+		 ab,cd,   o, o,
+		a,b,c,d,o,o,o,o};
+
+		merkle_fill_tree(tree, 4, 7);
+
+		TEST_CHECK((tree ==
+		     v{o,
+		   ad,     o,
+		 ab, cd, o, o,
+		a,b,c,d,o,o,o,o}));
+	}
+
+	// fill right side of the tree
+	{
+		v tree{
+		       o,
+		   o,      o,
+		 o,  o,   o, o,
+		o,o,o,o,a,b,c,d};
+
+		merkle_fill_tree(tree, 4, 11);
+
+		TEST_CHECK((tree ==
+		     v{o,
+		   o,      ad,
+		 o,  o,  ab,cd,
+		o,o,o,o,a,b,c,d}));
+	}
+
+	// fill shallow left of the tree
+	{
+		v tree{
+		       o,
+		   o,      o,
+		 a,  b,   o, o,
+		o,o,o,o,o,o,o,o};
+
+		merkle_fill_tree(tree, 2, 3);
+
+		TEST_CHECK((tree ==
+		     v{o,
+		   ab,     o,
+		 a,  b,   o, o,
+		o,o,o,o,o,o,o,o}));
+	}
+
+	// fill shallow right of the tree
+	{
+		v tree{
+		       o,
+		   o,      o,
+		 o,  o,   a, b,
+		o,o,o,o,o,o,o,o};
+
+		merkle_fill_tree(tree, 2, 5);
+
+		TEST_CHECK((tree ==
+		     v{o,
+		   o,      ab,
+		 o,  o,   a, b,
+		o,o,o,o,o,o,o,o}));
+	}
+}
+
+TORRENT_TEST(merkle_root)
+{
+	// all leaves in the tree
+	TEST_CHECK(merkle_root(v{a,b,c,d,e,f,g,h}, o) == ah);
+
+	// not power-of-two number of leaves
+	TEST_CHECK(merkle_root(v{a,b,c,d,e,f}, o) == H(ad, H(ef, H(o, o))));
+
+	// very small tree
+	TEST_CHECK(merkle_root(v{a,b}, o) == ab);
+}
+
+namespace {
+void print_tree(span<sha256_hash const> tree)
+{
+	int const num_leafs = static_cast<int>((tree.size() + 1) / 2);
+	int spacing = num_leafs;
+	int const num_levels = merkle_num_layers(num_leafs) + 1;
+	int layer_width = 1;
+	int node = 0;
+	for (int i = 0; i < num_levels; ++i)
+	{
+		for (int k = 0; k < layer_width; ++k)
+		{
+			for (int j = 0; j < spacing; ++j) std::cout << ' ';
+			std::cout << (tree[node] == sha256_hash() ? '0' : '1');
+			for (int j = 0; j < spacing - 1; ++j) std::cout << ' ';
+			++node;
+		}
+		std::cout << '\n';
+		layer_width *= 2;
+		spacing /= 2;
+	}
+	std::cout << '\n';
+}
+}
+
+TORRENT_TEST(merkle_clear_tree)
+{
+	// test clearing the whole tree
+	{
+		v tree{l,
+		   l,      l,
+		  l, l,   l, l,
+		l,l,l,l,l,l,l,l};
+
+		print_tree(tree);
+		merkle_clear_tree(tree, 8, 7);
+		print_tree(tree);
+
+		TEST_CHECK((tree ==
+		     v{o,
+		   o,      o,
+		  o, o,   o, o,
+		o,o,o,o,o,o,o,o}));
+	}
+
+	// test clearing the left side of the tree
+	{
+		v tree{l,
+		   l,      l,
+		  l, l,   l, l,
+		l,l,l,l,l,l,l,l};
+
+		print_tree(tree);
+		merkle_clear_tree(tree, 4, 7);
+		print_tree(tree);
+
+		TEST_CHECK((tree ==
+		     v{l,
+		   o,      l,
+		  o, o,   l, l,
+		o,o,o,o,l,l,l,l}));
+	}
+
+	// test clearing the right side of the tree
+	{
+		v tree{l,
+		   l,      l,
+		  l, l,   l, l,
+		l,l,l,l,l,l,l,l};
+
+		print_tree(tree);
+		merkle_clear_tree(tree, 4, 11);
+		print_tree(tree);
+
+		TEST_CHECK((tree ==
+		     v{l,
+		   l,      o,
+		  l, l,   o, o,
+		l,l,l,l,o,o,o,o}));
+	}
+
+	// test clearing shallow left
+	{
+		v tree{l,
+		   l,      l,
+		  l, l,   l, l,
+		l,l,l,l,l,l,l,l};
+
+		print_tree(tree);
+		merkle_clear_tree(tree, 2, 3);
+		print_tree(tree);
+
+		TEST_CHECK((tree ==
+		     v{l,
+		   o,      l,
+		  o, o,   l, l,
+		l,l,l,l,l,l,l,l}));
+	}
+
+	// test clearing shallow right
+	{
+		v tree{l,
+		   l,      l,
+		  l, l,   l, l,
+		l,l,l,l,l,l,l,l};
+
+		print_tree(tree);
+		merkle_clear_tree(tree, 2, 5);
+		print_tree(tree);
+
+		TEST_CHECK((tree ==
+		     v{l,
+		   l,      o,
+		  l, l,   o, o,
+		l,l,l,l,l,l,l,l}));
+	}
 }
 
