40811dd381d88d8003bfd672beaad472052cf061
pavel.pimenov <pavel.pimenov@gmail.com>
2018-01-07 20:31:26 +0300

    add TORRENT_USE_ASSERTS

--- src/bt_peer_connection.cpp
+++ src/bt_peer_connection.cpp
@@ -414,14 +414,19 @@ namespace {
 
 		if (!m_supports_fast) return;
 
+#if TORRENT_USE_ASSERTS
+		std::shared_ptr<torrent> t = associated_torrent().lock();
+		TORRENT_ASSERT(t);
+		TORRENT_ASSERT(t->valid_metadata());
+#endif
+
 #ifndef TORRENT_DISABLE_LOGGING
 		if (should_log(peer_log_alert::outgoing_message))
 		{
+#if !TORRENT_USE_ASSERTS
 			std::shared_ptr<torrent> t = associated_torrent().lock();
-			TORRENT_ASSERT(t);
-			TORRENT_ASSERT(t->valid_metadata());
-
-			peer_log(peer_log_alert::outgoing_message, "SUGGEST"
+#endif
+            peer_log(peer_log_alert::outgoing_message, "SUGGEST"
 				, "piece: %d num_peers: %d", static_cast<int>(piece)
 				, t->has_picker() ? t->picker().get_availability(piece) : -1);
 		}
