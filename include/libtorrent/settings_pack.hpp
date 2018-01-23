2a4f0569001fd8216901e1377a1f173a29aa71d7
arvidn <arvid@libtorrent.org>
2018-01-23 19:30:41 +0100

    update documentation

--- include/libtorrent/settings_pack.hpp
+++ include/libtorrent/settings_pack.hpp
@@ -502,10 +502,10 @@ namespace libtorrent
 			no_recheck_incomplete_resume,
 
 			// ``anonymous_mode`` defaults to false. When set to true, the client
-			// tries to hide its identity to a certain degree. The peer-ID will no
-			// longer include the client's fingerprint. The user-agent will be
-			// reset to an empty string. Trackers will only be used if they are
-			// using a proxy server. The listen sockets are closed, and incoming
+			// tries to hide its identity to a certain degree. The user-agent will be
+			// reset to an empty string (except for private torrents). Trackers
+			// will only be used if they are using a proxy server.
+			// The listen sockets are closed, and incoming
 			// connections will only be accepted through a SOCKS5 or I2P proxy (if
 			// a peer proxy is set up and is run on the same machine as the
 			// tracker proxy). Since no incoming connections are accepted,
