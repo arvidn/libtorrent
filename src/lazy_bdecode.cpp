b9aa9a622c0933a079652d83dbb4e6c626ab627d
Alden Torres <aldenml@gmail.com>
2018-05-14 11:17:30 -0400

    changes by review

--- src/lazy_bdecode.cpp
+++ src/lazy_bdecode.cpp
@@ -265,8 +265,8 @@ namespace {
 		}
 		else if (int(m_size) == this->capacity())
 		{
-			int const capacity = this->capacity() * lazy_entry_grow_factor / 100;
-			auto* tmp = new (std::nothrow) lazy_dict_entry[std::size_t(capacity + 1)];
+			std::size_t const capacity = std::size_t(this->capacity()) * lazy_entry_grow_factor / 100;
+			auto* tmp = new (std::nothrow) lazy_dict_entry[capacity + 1];
 			if (tmp == nullptr) return nullptr;
 			std::move(m_data.dict, m_data.dict + m_size + 1, tmp);
 
@@ -435,8 +435,8 @@ namespace {
 		}
 		else if (int(m_size) == this->capacity())
 		{
-			int const capacity = this->capacity() * lazy_entry_grow_factor / 100;
-			lazy_entry* tmp = new (std::nothrow) lazy_entry[std::size_t(capacity + 1)];
+			std::size_t const capacity = std::size_t(this->capacity()) * lazy_entry_grow_factor / 100;
+			lazy_entry* tmp = new (std::nothrow) lazy_entry[capacity + 1];
 			if (tmp == nullptr) return nullptr;
 			std::move(m_data.list, m_data.list + m_size + 1, tmp);
 
