4f23b896b68a01095ddb2a688d0994bcfb1f80a7
arvidn <arvid@cs.umu.se>
2018-11-09 09:15:51 +0100

    back-port the new test SSL cert from master

--- test/ssl/regenerate_test_certificate.sh
+++ test/ssl/regenerate_test_certificate.sh
@@ -2,26 +2,26 @@
 
 set -e
 
-PATH=$PATH:/usr/local/etc/openssl/misc
+PATH=$PATH:/usr/local/etc/openssl/misc:/usr/lib/ssl/misc
 
 rm -rf demoCA
 
-printf "\n\n   ======== \x1b[33muse passphrase: \"test\" \x1b[0m=========\n\n"
-printf "\n\n   ======== \x1b[33muse common name: \"test\" \x1b[0m=========\n\n"
+printf "\n\n   ======== \e[33muse passphrase: \"test\" \e[0m=========\n\n"
+printf "\n\n   ======== \e[33muse common name: \"test\" \e[0m=========\n\n"
 
-CA.sh -newca
+CA.pl -newca
 
 cp ./demoCA/cacert.pem root_ca_cert.pem
 cp ./demoCA/private/cakey.pem root_ca_private.pem
 
-printf "\n\n   ======== \x1b[33muse passphrase: \"test\" \x1b[0m=========\n\n"
-printf "\n\n   ======== \x1b[33muse common name: * \x1b[0m=========\n\n"
+printf "\n\n   ======== \e[33muse passphrase: \"test\" \e[0m=========\n\n"
+printf "\n\n   ======== \e[33muse common name: * \e[0m=========\n\n"
 
-CA.sh -newreq
+CA.pl -newreq
 
 cp newkey.pem peer_private_key.pem
 
-CA.sh -sign
+CA.pl -sign
 
 cp newcert.pem peer_certificate.pem
 
