546fcae60c27a99a89742ff9faf6d20750c88b17
arvidn <arvid@cs.umu.se>
2018-11-20 15:44:55 +0100

    make settings_pack enums specify a narrower underlying type and remove deprecated, internal enums

--- include/libtorrent/aux_/proxy_settings.hpp
+++ include/libtorrent/aux_/proxy_settings.hpp
@@ -34,6 +34,7 @@ POSSIBILITY OF SUCH DAMAGE.
 #define TORRENT_PROXY_SETTINGS_HPP_INCLUDED
 
 #include "libtorrent/config.hpp"
+#include "libtorrent/settings_pack.hpp"
 
 #include <string>
 
@@ -67,57 +68,9 @@ namespace aux {
 		std::string username;
 		std::string password;
 
-#if TORRENT_ABI_VERSION == 1
-		// the type of proxy to use. Assign one of these to the
-		// proxy_settings::type field.
-		enum proxy_type
-		{
-			// This is the default, no proxy server is used, all other fields are
-			// ignored.
-			none,
-
-			// The server is assumed to be a `SOCKS4 server`_ that requires a
-			// username.
-			//
-			// .. _`SOCKS4 server`: http://www.ufasoft.com/doc/socks4_protocol.htm
-			socks4,
-
-			// The server is assumed to be a SOCKS5 server (`RFC 1928`_) that does
-			// not require any authentication. The username and password are
-			// ignored.
-			//
-			// .. _`RFC 1928`: http://www.faqs.org/rfcs/rfc1928.html
-			socks5,
-
-			// The server is assumed to be a SOCKS5 server that supports plain
-			// text username and password authentication (`RFC 1929`_). The
-			// username and password specified may be sent to the proxy if it
-			// requires.
-			//
-			// .. _`RFC 1929`: http://www.faqs.org/rfcs/rfc1929.html
-			socks5_pw,
-
-			// The server is assumed to be an HTTP proxy. If the transport used
-			// for the connection is non-HTTP, the server is assumed to support
-			// the CONNECT_ method. i.e. for web seeds and HTTP trackers, a plain
-			// proxy will suffice. The proxy is assumed to not require
-			// authorization. The username and password will not be used.
-			//
-			// .. _CONNECT: http://tools.ietf.org/html/draft-luotonen-web-proxy-tunneling-01
-			http,
-
-			// The server is assumed to be an HTTP proxy that requires user
-			// authorization. The username and password will be sent to the proxy.
-			http_pw,
-
-			// route through an i2p SAM proxy
-			i2p_proxy
-		};
-#endif
-
-		// tells libtorrent what kind of proxy server it is. See proxy_type
+		// tells libtorrent what kind of proxy server it is. See proxy_type_t
 		// enum for options
-		std::uint8_t type = 0;
+		settings_pack::proxy_type_t type = settings_pack::none;
 
 		// the port the proxy server is running on
 		std::uint16_t port = 0;
