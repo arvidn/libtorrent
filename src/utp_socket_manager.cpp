21d9f7589dc04ad1307d9ec9acbd5550dd3e5b0d
arvidn <arvid@libtorrent.org>
2018-12-21 20:16:19 +0100

    improve deferred ACK logic in utp socket manager

--- src/utp_socket_manager.cpp
+++ src/utp_socket_manager.cpp
@@ -268,6 +268,12 @@ namespace libtorrent
 			return utp_incoming_packet(m_last_socket, p, size, ep, receive_time);
 		}
 
+		if (m_deferred_ack)
+		{
+			utp_send_ack(m_deferred_ack);
+			m_deferred_ack = NULL;
+		}
+
 		std::pair<socket_map_t::iterator, socket_map_t::iterator> r =
 			m_utp_sockets.equal_range(id);
 
@@ -353,15 +359,10 @@ namespace libtorrent
 
 	void utp_socket_manager::socket_drained()
 	{
-		// flush all deferred acks
-
-		std::vector<utp_socket_impl*> deferred_acks;
-		m_deferred_acks.swap(deferred_acks);
-		for (std::vector<utp_socket_impl*>::iterator i = deferred_acks.begin()
-			, end(deferred_acks.end()); i != end; ++i)
+		if (m_deferred_ack)
 		{
-			utp_socket_impl* s = *i;
-			utp_send_ack(s);
+			utp_send_ack(m_deferred_ack);
+			m_deferred_ack = NULL;
 		}
 
 		std::vector<utp_socket_impl*> drained_event;
@@ -376,9 +377,8 @@ namespace libtorrent
 
 	void utp_socket_manager::defer_ack(utp_socket_impl* s)
 	{
-		TORRENT_ASSERT(std::find(m_deferred_acks.begin(), m_deferred_acks.end(), s)
-			== m_deferred_acks.end());
-		m_deferred_acks.push_back(s);
+		TORRENT_ASSERT(m_deferred_ack == NULL);
+		m_deferred_ack = s;
 	}
 
 	void utp_socket_manager::subscribe_drained(utp_socket_impl* s)
