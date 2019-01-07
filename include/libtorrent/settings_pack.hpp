5a9608e97d226cf5c9cb3993e9495019114d3b35
arvidn <arvid@libtorrent.org>
2019-01-07 01:27:02 +0100

    only allow cwnd to be reduced so often

--- include/libtorrent/settings_pack.hpp
+++ include/libtorrent/settings_pack.hpp
@@ -1608,6 +1608,11 @@ namespace libtorrent
 			// systems.
 			close_file_interval,
 
+			// When uTP experiences packet loss, it will reduce the congestion
+			// window, and not reduce it again for this many milliseconds, even if
+			// experiencing another lost packet.
+			utp_cwnd_reduce_timer,
+
 			max_int_setting_internal
 		};
 
