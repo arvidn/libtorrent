0a59d28edd18d951ae8f2314ed394e8d982c9146
arvidn <arvid@libtorrent.org>
2019-02-14 14:12:50 +0100

    fix seed_mode flag

--- include/libtorrent/torrent.hpp
+++ include/libtorrent/torrent.hpp
@@ -67,7 +67,7 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "libtorrent/linked_list.hpp"
 #include "libtorrent/debug.hpp"
 #include "libtorrent/piece_block.hpp"
-#include "libtorrent/disk_interface.hpp" // for status_t
+#include "libtorrent/disk_interface.hpp"
 #include "libtorrent/aux_/file_progress.hpp"
 #include "libtorrent/aux_/suggest_piece.hpp"
 #include "libtorrent/units.hpp"
