d6bf5334abc407063772380f5aacda00204b5659
arvidn <arvid@libtorrent.org>
2019-03-16 00:23:46 +0100

    move merkle functions into merkle.cpp

--- test/test_merkle.cpp
+++ test/test_merkle.cpp
@@ -33,10 +33,10 @@ POSSIBILITY OF SUCH DAMAGE.
 #include "test.hpp"
 #include "libtorrent/aux_/merkle.hpp"
 
-TORRENT_TEST(merkle)
-{
-	using namespace lt;
+using namespace lt;
 
+TORRENT_TEST(num_leafs)
+{
 	// test merkle_*() functions
 
 	// this is the structure:
@@ -64,7 +64,10 @@ TORRENT_TEST(merkle)
 	TEST_EQUAL(merkle_num_leafs(16), 16);
 	TEST_EQUAL(merkle_num_leafs(17), 32);
 	TEST_EQUAL(merkle_num_leafs(18), 32);
+}
 
+TORRENT_TEST(get_parent)
+{
 	// parents
 	TEST_EQUAL(merkle_get_parent(1), 0);
 	TEST_EQUAL(merkle_get_parent(2), 0);
@@ -80,7 +83,10 @@ TORRENT_TEST(merkle)
 	TEST_EQUAL(merkle_get_parent(12), 5);
 	TEST_EQUAL(merkle_get_parent(13), 6);
 	TEST_EQUAL(merkle_get_parent(14), 6);
+}
 
+TORRENT_TEST(get_sibling)
+{
 	// siblings
 	TEST_EQUAL(merkle_get_sibling(1), 2);
 	TEST_EQUAL(merkle_get_sibling(2), 1);
@@ -96,12 +102,60 @@ TORRENT_TEST(merkle)
 	TEST_EQUAL(merkle_get_sibling(12), 11);
 	TEST_EQUAL(merkle_get_sibling(13), 14);
 	TEST_EQUAL(merkle_get_sibling(14), 13);
+}
 
+TORRENT_TEST(num_nodes)
+{
 	// total number of nodes given the number of leafs
 	TEST_EQUAL(merkle_num_nodes(1), 1);
 	TEST_EQUAL(merkle_num_nodes(2), 3);
 	TEST_EQUAL(merkle_num_nodes(4), 7);
 	TEST_EQUAL(merkle_num_nodes(8), 15);
 	TEST_EQUAL(merkle_num_nodes(16), 31);
+}
+
+TORRENT_TEST(get_layer)
+{
+	// this is the structure:
+	//             0
+	//      1              2
+	//   3      4       5       6
+	//  7 8    9 10   11 12   13 14
+
+	TEST_EQUAL(merkle_get_layer(0), 0);
+	TEST_EQUAL(merkle_get_layer(1), 1);
+	TEST_EQUAL(merkle_get_layer(2), 1);
+	TEST_EQUAL(merkle_get_layer(3), 2);
+	TEST_EQUAL(merkle_get_layer(4), 2);
+	TEST_EQUAL(merkle_get_layer(5), 2);
+	TEST_EQUAL(merkle_get_layer(6), 2);
+	TEST_EQUAL(merkle_get_layer(7), 3);
+	TEST_EQUAL(merkle_get_layer(8), 3);
+	TEST_EQUAL(merkle_get_layer(9), 3);
+	TEST_EQUAL(merkle_get_layer(10), 3);
+	TEST_EQUAL(merkle_get_layer(11), 3);
+	TEST_EQUAL(merkle_get_layer(12), 3);
+	TEST_EQUAL(merkle_get_layer(13), 3);
+	TEST_EQUAL(merkle_get_layer(14), 3);
+	TEST_EQUAL(merkle_get_layer(15), 4);
+}
+TORRENT_TEST(get_layer_offset)
+{
+	TEST_EQUAL(merkle_get_layer_offset(0), 0);
+	TEST_EQUAL(merkle_get_layer_offset(1), 0);
+	TEST_EQUAL(merkle_get_layer_offset(2), 1);
+	TEST_EQUAL(merkle_get_layer_offset(3), 0);
+	TEST_EQUAL(merkle_get_layer_offset(4), 1);
+	TEST_EQUAL(merkle_get_layer_offset(5), 2);
+	TEST_EQUAL(merkle_get_layer_offset(6), 3);
+	TEST_EQUAL(merkle_get_layer_offset(7), 0);
+	TEST_EQUAL(merkle_get_layer_offset(8), 1);
+	TEST_EQUAL(merkle_get_layer_offset(9), 2);
+	TEST_EQUAL(merkle_get_layer_offset(10), 3);
+	TEST_EQUAL(merkle_get_layer_offset(11), 4);
+	TEST_EQUAL(merkle_get_layer_offset(12), 5);
+	TEST_EQUAL(merkle_get_layer_offset(13), 6);
+	TEST_EQUAL(merkle_get_layer_offset(14), 7);
+	TEST_EQUAL(merkle_get_layer_offset(15), 0);
 }
 
