4387d44a30d986139c2434d746a787bad678254c
arvidn <arvid@libtorrent.org>
2019-03-15 22:59:25 +0100

    fix python bindings for peer_info

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -712,6 +712,27 @@ class test_error_code(unittest.TestCase):
         self.assertEqual(lt.system_category().name(), 'system')
 
 
+class test_peer_info(unittest.TestCase):
+
+    def test_peer_info_members(self):
+
+        p = lt.peer_info()
+
+        print(p.client)
+        print(p.pieces)
+        print(p.pieces)
+        print(p.last_request)
+        print(p.last_active)
+        print(p.flags)
+        print(p.source)
+        print(p.pid)
+        print(p.downloading_piece_index)
+        print(p.ip)
+        print(p.local_endpoint)
+        print(p.read_state)
+        print(p.write_state)
+
+
 if __name__ == '__main__':
     print(lt.__version__)
     shutil.copy(os.path.join('..', '..', 'test', 'test_torrents',
