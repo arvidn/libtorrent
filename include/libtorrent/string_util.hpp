43bd9883bd74b5b5fe118b917bf93362634bec64
arvidn <arvid@libtorrent.org>
2019-03-18 17:52:55 +0100

    remove old, hacky and unused split_string() function

--- include/libtorrent/string_util.hpp
+++ include/libtorrent/string_util.hpp
@@ -64,7 +64,6 @@ namespace libtorrent {
 	TORRENT_EXTRA_EXPORT bool is_space(char c);
 	TORRENT_EXTRA_EXPORT char to_lower(char c);
 
-	TORRENT_EXTRA_EXPORT int split_string(char const** tags, int buf_size, char* in);
 	TORRENT_EXTRA_EXPORT bool string_begins_no_case(char const* s1, char const* s2);
 	TORRENT_EXTRA_EXPORT bool string_equal_no_case(string_view s1, string_view s2);
 
