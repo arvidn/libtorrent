38ca4113eacf67f5e6addc57f9638fd4f8455af3
Arvid Norberg <arvid@cs.umu.se>
2019-03-23 11:27:48 +0100

    fix python binding for create_torrent

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -32,6 +32,7 @@ class test_create_torrent(unittest.TestCase):
 
     def test_from_torrent_info(self):
         ti = lt.torrent_info('unordered.torrent')
+        print(ti.ssl_cert())
         ct = lt.create_torrent(ti)
         entry = ct.generate()
         content = lt.bencode(entry).strip()
@@ -41,6 +42,19 @@ class test_create_torrent(unittest.TestCase):
             print(file_content)
             print(entry)
             self.assertEqual(content, file_content)
+
+    def test_from_scratch(self):
+        fs = lt.file_storage()
+        fs.add_file('test/file1', 1000)
+        fs.add_file('test/file2', 2000)
+        ct = lt.create_torrent(fs)
+        ct.add_url_seed('foo')
+        ct.add_http_seed('bar')
+        ct.add_tracker('bar')
+        ct.set_root_cert('1234567890')
+        ct.add_collection('1337')
+        entry = ct.generate()
+        print(entry)
 
 
 class test_session_stats(unittest.TestCase):
