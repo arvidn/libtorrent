82c94e1137bbc5c677726127d21f1deaaa671614
arvidn <arvid@libtorrent.org>
2019-07-12 10:00:49 -0400

    add test vectors for sha-256 hasher

--- test/test_hasher.cpp
+++ test/test_hasher.cpp
@@ -35,6 +35,8 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #include "test.hpp"
 
+#include <iostream>
+
 using namespace lt;
 
 namespace
@@ -42,25 +44,28 @@ namespace
 // test vectors from RFC 3174
 // http://www.faqs.org/rfcs/rfc3174.html
 
-char const* test_array[4] =
+struct test_vector_t
 {
-	"abc",
-	"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
-	"a",
-	"0123456701234567012345670123456701234567012345670123456701234567"
+	string_view input;
+	int repetitions;
+	string_view hex_output;
 };
 
-long int repeat_count[4] = { 1, 1, 1000000, 10 };
+std::array<test_vector_t, 4> sha1_vectors = {{
+	{"abc", 1, "A9993E364706816ABA3E25717850C26C9CD0D89D"},
+	{"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq", 1, "84983E441C3BD26EBAAE4AA1F95129E5E54670F1"},
+	{"a", 1000000, "34AA973CD4C4DAA4F61EEB2BDBAD27316534016F"},
+	{"0123456701234567012345670123456701234567012345670123456701234567", 10, "DEA356A2CDDD90C7A7ECEDC5EBB563934F460452"}
+}};
 
-char const* result_array[4] =
-{
-	"A9993E364706816ABA3E25717850C26C9CD0D89D",
-	"84983E441C3BD26EBAAE4AA1F95129E5E54670F1",
-	"34AA973CD4C4DAA4F61EEB2BDBAD27316534016F",
-	"DEA356A2CDDD90C7A7ECEDC5EBB563934F460452"
-};
+// https://www.dlitz.net/crypto/shad256-test-vectors/
+std::array<test_vector_t, 3> sha256_vectors = {{
+	{"abc", 1, "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"},
+	{"\xde\x18\x89\x41\xa3\x37\x5d\x3a\x8a\x06\x1e\x67\x57\x6e\x92\x6d", 1, "067c531269735ca7f541fdaca8f0dc76305d3cada140f89372a410fe5eff6e4d"},
+	{"a", 1000000, "cdc76e5c9914fb9281a1c7e284d73e67f1809a48a497200e046d39ccc7112cd0"},
+}};
 
-void test_vector(std::string s, std::string output, int const n = 1)
+void test_vector(string_view s, string_view output, int const n = 1)
 {
 	hasher h;
 	for (int i = 0; i < n; i++)
@@ -80,14 +85,14 @@ void test_vector(std::string s, std::string output, int const n = 1)
 
 TORRENT_TEST(hasher)
 {
-	for (int test = 0; test < 4; ++test)
+	for (auto const& t : sha1_vectors)
 	{
 		hasher h;
-		for (int i = 0; i < repeat_count[test]; ++i)
-			h.update(test_array[test], int(std::strlen(test_array[test])));
+		for (int i = 0; i < t.repetitions; ++i)
+			h.update(t.input.data(), int(t.input.size()));
 
 		sha1_hash result;
-		aux::from_hex({result_array[test], 40}, result.data());
+		aux::from_hex(t.hex_output, result.data());
 		TEST_CHECK(result == h.final());
 	}
 }
@@ -123,3 +128,16 @@ TORRENT_TEST(hasher_test_vec1)
 		, 16777216
 	);
 }
+
+TORRENT_TEST(hasher256)
+{
+	for (auto const& t : sha256_vectors)
+	{
+		hasher256 h;
+		for (int i = 0; i < t.repetitions; ++i)
+			h.update(t.input.data(), int(t.input.size()));
+
+		TEST_EQUAL(t.hex_output, aux::to_hex(h.final()));
+	}
+}
+
