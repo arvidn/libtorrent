a71d424e557203ab3eb3154fd2b5335a1478c221
pixelherodev <pleasantatk@gmail.com>
2019-09-29 04:23:55 -0400

    Cleanup example

--- examples/bt-get.cpp
+++ examples/bt-get.cpp
@@ -54,24 +54,22 @@ int main(int argc, char const* argv[]) try
 	lt::add_torrent_params atp = lt::parse_magnet_uri(argv[1]);
 	atp.save_path = "."; // save in current dir
 	lt::torrent_handle h = ses.add_torrent(std::move(atp));
-
-	for (;;) {
+	
+	bool done = false;
+	
+	while (!done) {
 		std::vector<lt::alert*> alerts;
 		ses.pop_alerts(&alerts);
-
 		for (lt::alert const* a : alerts) {
 			std::cout << a->message() << std::endl;
 			// if we receive the finished alert or an error, we're done
-			if (lt::alert_cast<lt::torrent_finished_alert>(a)) {
-				goto done;
-			}
-			if (lt::alert_cast<lt::torrent_error_alert>(a)) {
-				goto done;
+			if (lt::alert_cast<lt::torrent_finished_alert>(a) ||
+			    		lt::alert_cast<lt::torrent_error_alert>(a)) {
+				done = true;
 			}
 		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(200));
 	}
-	done:
 	std::cout << "done, shutting down" << std::endl;
 }
 catch (std::exception& e)
