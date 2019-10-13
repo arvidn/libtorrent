d5c411a462a157a716265d0a5daa7ce89450f6ad
arvidn <arvid@libtorrent.org>
2019-10-14 01:15:14 +0200

    fix making ssl_stream movable

--- include/libtorrent/ssl_stream.hpp
+++ include/libtorrent/ssl_stream.hpp
@@ -70,9 +70,10 @@ class ssl_stream
 public:
 
 	explicit ssl_stream(io_context& io_context, ssl::context& ctx)
-		: m_sock(io_context, ctx)
-	{
-	}
+		: m_sock(new ssl::stream<Stream>(io_context, ctx))
+	{}
+
+	ssl_stream(ssl_stream&&) = default;
 
 	using sock_type = typename boost::asio::ssl::stream<Stream>;
 	using next_layer_type = typename sock_type::next_layer_type;
@@ -80,18 +81,18 @@ public:
 	using endpoint_type = typename Stream::endpoint_type;
 	using protocol_type = typename Stream::protocol_type;
 	using executor_type = typename sock_type::executor_type;
-	executor_type get_executor() { return m_sock.get_executor(); }
+	executor_type get_executor() { return m_sock->get_executor(); }
 
 	void set_host_name(std::string const& name)
 	{
-		aux::openssl_set_tlsext_hostname(m_sock.native_handle(), name.c_str());
+		aux::openssl_set_tlsext_hostname(m_sock->native_handle(), name.c_str());
 	}
 
 	template <class T>
 	void set_verify_callback(T const& fun, error_code& ec)
-	{ m_sock.set_verify_callback(fun, ec); }
+	{ m_sock->set_verify_callback(fun, ec); }
 
-	SSL* native_handle() { return m_sock.native_handle(); }
+	SSL* native_handle() { return m_sock->native_handle(); }
 
 	template <class Handler>
 	void async_connect(endpoint_type const& endpoint, Handler const& h)
@@ -100,7 +101,7 @@ public:
 		// 1. connect to peer
 		// 2. perform SSL client handshake
 
-		m_sock.next_layer().async_connect(endpoint, wrap_allocator(
+		m_sock->next_layer().async_connect(endpoint, wrap_allocator(
 			[this](error_code const& ec, Handler hn) {
 				connected(ec, std::move(hn));
 			}, std::move(h)));
@@ -111,7 +112,7 @@ public:
 	{
 		// this is used for accepting SSL connections
 		using std::placeholders::_1;
-		m_sock.async_handshake(ssl::stream_base::server, wrap_allocator(
+		m_sock->async_handshake(ssl::stream_base::server, wrap_allocator(
 			[this](error_code const& ec, Handler hn) {
 				handshake(ec, std::move(hn));
 			}, std::move(h)));
@@ -120,99 +121,99 @@ public:
 	void accept_handshake(error_code& ec)
 	{
 		// this is used for accepting SSL connections
-		m_sock.handshake(ssl::stream_base::server, ec);
+		m_sock->handshake(ssl::stream_base::server, ec);
 	}
 
 	template <class Handler>
 	void async_shutdown(Handler const& handler)
 	{
 		error_code ec;
-		m_sock.next_layer().cancel(ec);
-		m_sock.async_shutdown(handler);
+		m_sock->next_layer().cancel(ec);
+		m_sock->async_shutdown(handler);
 	}
 
 	void shutdown(error_code& ec)
 	{
-		m_sock.shutdown(ec);
+		m_sock->shutdown(ec);
 	}
 
 	template <class Mutable_Buffers, class Handler>
 	void async_read_some(Mutable_Buffers const& buffers, Handler const& handler)
 	{
-		m_sock.async_read_some(buffers, handler);
+		m_sock->async_read_some(buffers, handler);
 	}
 
 	template <class Mutable_Buffers>
 	std::size_t read_some(Mutable_Buffers const& buffers, error_code& ec)
 	{
-		return m_sock.read_some(buffers, ec);
+		return m_sock->read_some(buffers, ec);
 	}
 
 #ifndef BOOST_NO_EXCEPTIONS
 	template <class SettableSocketOption>
 	void set_option(SettableSocketOption const& opt)
 	{
-		m_sock.next_layer().set_option(opt);
+		m_sock->next_layer().set_option(opt);
 	}
 #endif
 
 	template <class SettableSocketOption>
 	void set_option(SettableSocketOption const& opt, error_code& ec)
 	{
-		m_sock.next_layer().set_option(opt, ec);
+		m_sock->next_layer().set_option(opt, ec);
 	}
 
 #ifndef BOOST_NO_EXCEPTIONS
 	template <class GettableSocketOption>
 	void get_option(GettableSocketOption& opt)
 	{
-		m_sock.next_layer().get_option(opt);
+		m_sock->next_layer().get_option(opt);
 	}
 #endif
 
 	template <class GettableSocketOption>
 	void get_option(GettableSocketOption& opt, error_code& ec)
 	{
-		m_sock.next_layer().get_option(opt, ec);
+		m_sock->next_layer().get_option(opt, ec);
 	}
 
 #ifndef BOOST_NO_EXCEPTIONS
 	template <class Mutable_Buffers>
 	std::size_t read_some(Mutable_Buffers const& buffers)
 	{
-		return m_sock.read_some(buffers);
+		return m_sock->read_some(buffers);
 	}
 
 	template <class IO_Control_Command>
 	void io_control(IO_Control_Command& ioc)
 	{
-		m_sock.next_layer().io_control(ioc);
+		m_sock->next_layer().io_control(ioc);
 	}
 #endif
 
 	template <class IO_Control_Command>
 	void io_control(IO_Control_Command& ioc, error_code& ec)
 	{
-		m_sock.next_layer().io_control(ioc, ec);
+		m_sock->next_layer().io_control(ioc, ec);
 	}
 
 #ifndef BOOST_NO_EXCEPTIONS
-	void non_blocking(bool b) { m_sock.next_layer().non_blocking(b); }
+	void non_blocking(bool b) { m_sock->next_layer().non_blocking(b); }
 #endif
 
 	void non_blocking(bool b, error_code& ec)
-	{ m_sock.next_layer().non_blocking(b, ec); }
+	{ m_sock->next_layer().non_blocking(b, ec); }
 
 	template <class Const_Buffers, class Handler>
 	void async_write_some(Const_Buffers const& buffers, Handler const& handler)
 	{
-		m_sock.async_write_some(buffers, handler);
+		m_sock->async_write_some(buffers, handler);
 	}
 
 	template <class Const_Buffers>
 	std::size_t write_some(Const_Buffers const& buffers, error_code& ec)
 	{
-		return m_sock.write_some(buffers, ec);
+		return m_sock->write_some(buffers, ec);
 	}
 
 	// the SSL stream may cache 17 kiB internally, and there's no way of
@@ -220,95 +221,95 @@ public:
 	// seems fine to potentially over-estimate the number of bytes available.
 #ifndef BOOST_NO_EXCEPTIONS
 	std::size_t available() const
-	{ return 17 * 1024 + const_cast<sock_type&>(m_sock).next_layer().available(); }
+	{ return 17 * 1024 + const_cast<sock_type&>(*m_sock).next_layer().available(); }
 #endif
 
 	std::size_t available(error_code& ec) const
-	{ return 17 * 1024 + const_cast<sock_type&>(m_sock).next_layer().available(ec); }
+	{ return 17 * 1024 + const_cast<sock_type&>(*m_sock).next_layer().available(ec); }
 
 #ifndef BOOST_NO_EXCEPTIONS
 	void bind(endpoint_type const& endpoint)
 	{
-		m_sock.next_layer().bind(endpoint);
+		m_sock->next_layer().bind(endpoint);
 	}
 #endif
 
 	void bind(endpoint_type const& endpoint, error_code& ec)
 	{
-		m_sock.next_layer().bind(endpoint, ec);
+		m_sock->next_layer().bind(endpoint, ec);
 	}
 
 #ifndef BOOST_NO_EXCEPTIONS
 	void open(protocol_type const& p)
 	{
-		m_sock.next_layer().open(p);
+		m_sock->next_layer().open(p);
 	}
 #endif
 
 	void open(protocol_type const& p, error_code& ec)
 	{
-		m_sock.next_layer().open(p, ec);
+		m_sock->next_layer().open(p, ec);
 	}
 
 	bool is_open() const
 	{
-		return m_sock.next_layer().is_open();
+		return m_sock->next_layer().is_open();
 	}
 
 #ifndef BOOST_NO_EXCEPTIONS
 	void close()
 	{
-		m_sock.next_layer().close();
+		m_sock->next_layer().close();
 	}
 #endif
 
 	void close(error_code& ec)
 	{
-		m_sock.next_layer().close(ec);
+		m_sock->next_layer().close(ec);
 	}
 
 #ifndef BOOST_NO_EXCEPTIONS
 	endpoint_type remote_endpoint() const
 	{
-		return const_cast<sock_type&>(m_sock).next_layer().remote_endpoint();
+		return m_sock->next_layer().remote_endpoint();
 	}
 #endif
 
 	endpoint_type remote_endpoint(error_code& ec) const
 	{
-		return const_cast<sock_type&>(m_sock).next_layer().remote_endpoint(ec);
+		return m_sock->next_layer().remote_endpoint(ec);
 	}
 
 #ifndef BOOST_NO_EXCEPTIONS
 	endpoint_type local_endpoint() const
 	{
-		return const_cast<sock_type&>(m_sock).next_layer().local_endpoint();
+		return m_sock->next_layer().local_endpoint();
 	}
 #endif
 
 	endpoint_type local_endpoint(error_code& ec) const
 	{
-		return const_cast<sock_type&>(m_sock).next_layer().local_endpoint(ec);
+		return m_sock->next_layer().local_endpoint(ec);
 	}
 
 	lowest_layer_type& lowest_layer()
 	{
-		return m_sock.lowest_layer();
+		return m_sock->lowest_layer();
 	}
 
 	lowest_layer_type const& lowest_layer() const
 	{
-		return m_sock.lowest_layer();
+		return m_sock->lowest_layer();
 	}
 
 	next_layer_type& next_layer()
 	{
-		return m_sock.next_layer();
+		return m_sock->next_layer();
 	}
 
 	next_layer_type const& next_layer() const
 	{
-		return m_sock.next_layer();
+		return m_sock->next_layer();
 	}
 
 private:
@@ -322,7 +323,7 @@ private:
 			return;
 		}
 
-		m_sock.async_handshake(ssl::stream_base::client, wrap_allocator(
+		m_sock->async_handshake(ssl::stream_base::client, wrap_allocator(
 			[this](error_code const& ec, Handler hn) {
 				handshake(ec, std::move(hn));
 			}, std::move(h)));
@@ -334,7 +335,8 @@ private:
 		h(e);
 	}
 
-	ssl::stream<Stream> m_sock;
+	// to make us movable
+	std::unique_ptr<ssl::stream<Stream>> m_sock;
 };
 
 }
