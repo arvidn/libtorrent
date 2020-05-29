0dbaf8f00b83c300b69b0b7cfbbbfefc20b74cd1
arvidn <arvid@libtorrent.org>
2020-05-29 10:49:34 +0200

    put back storage.hpp with an error message

---
+++ include/libtorrent/storage.hpp
@@ -0,0 +1,7 @@
+
+#ifndef TORRENT_STORAGE_HPP_INCLUDED
+#define TORRENT_STORAGE_HPP_INCLUDED
+
+#error "the disk I/O subsystem has been overhauled in libtorrent 2.0. storage_interface is no longer a customization point, customize disk_interface instead"
+
+#endif
