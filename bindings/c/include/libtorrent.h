9a88d19ed83eef470da1078e305f1d311b068a49
arvidn <arvid@libtorrent.org>
2020-07-10 16:57:47 +0200

    fixup C-binding

---
+++ bindings/c/include/libtorrent.h
@@ -0,0 +1,300 @@
+/*
+
+Copyright (c) 2009, Arvid Norberg
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef LIBTORRENT_H
+#define LIBTORRENT_H
+
+#include "libtorrent_settings.h"
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum tags
+{
+	TAG_END = 0,
+
+	// === add_torrent_params tags ===
+
+	// identifying the torrent to add
+	TOR_FILENAME = 0x100, // char const*
+	TOR_TORRENT, // char const*, specify size of buffer with TOR_TORRENT_SIZE
+	TOR_TORRENT_SIZE, // int
+	TOR_INFOHASH, // char const*, must point to a 20 byte array
+	TOR_MAGNETLINK, // char const*, url
+
+	TOR_TRACKER_URL, // char const*
+	TOR_RESUME_DATA, // char const*
+	TOR_RESUME_DATA_SIZE, // int
+	TOR_SAVE_PATH, // char const*
+	TOR_NAME, // char const*
+	TOR_FLAGS, // int (torrent_flags enum)
+	TOR_USER_DATA, //void*
+	TOR_STORAGE_MODE, // int (value from storage_mode_t enum)
+
+
+	// === torrent settings ===
+
+	TSET_MAX_CONNECTIONS = 0x400, // int
+	TSET_UPLOAD_RATE_LIMIT, // int (bytes per second)
+	TSET_DOWNLOAD_RATE_LIMIT, // int (bytes per second)
+	TSET_MAX_UPLOAD_SLOTS, // int
+	TSET_FLAGS, // unsigned int (bits from torrent_flags enum)
+	TSET_FLAGS_MASK, // unsigned int (bits from torrent_flags enum). This cannot be queries, only used for setting flags
+};
+
+// flags used for TOR_FLAGS
+enum torrent_flags
+{
+	TF_SEED_MODE           = 0x1,
+	TF_UPLOAD_MODE         = 0X2,
+	TF_SHARE_MODE          = 0x4,
+	TF_APPLY_IP_FILTER     = 0x8,
+	TF_PAUSED              = 0x10,
+	TF_AUTO_MANAGED        = 0x20,
+	TF_DUPLICATE_IS_ERROR  = 0x40,
+	TF_UPDATE_SUBSCRIBE    = 0x80,
+	TF_SUPER_SEEDING       = 0x100,
+	TF_SEQUENTIAL_DOWNLOAD = 0x200,
+	TF_STOP_WHEN_READY     = 0x400,
+	TF_OVERRIDE_TRACKERS   = 0x800,
+	TF_OVERRIDE_WEB_SEEDS  = 0x1000,
+	TF_NEED_SAVE_RESUME    = 0x2000,
+
+	TF_DISABLE_DHT         = 0x80000,
+	TF_DISABLE_LSD         = 0x100000,
+	TF_DISABLE_PEX         = 0x200000,
+	TF_ALL                 = 0xffffff,
+};
+
+enum category_t
+{
+	CAT_ERROR               = 0x1,
+	CAT_PEER                = 0x2,
+	CAT_PORT_MAPPING        = 0x4,
+	CAT_STORAGE             = 0x8,
+	CAT_TRACKER             = 0x10,
+	CAT_CONNECT             = 0x20,
+	CAT_STATUS              = 0x40,
+	CAT_IP_BLOCK            = 0x100,
+	CAT_PERFORMANCE_WARNING = 0x200,
+	CAT_DHT                 = 0x400,
+	CAT_STATS               = 0x800,
+	CAT_SESSION_LOG         = 0x2000,
+	CAT_TORRENT_LOG         = 0x4000,
+	CAT_PEER_LOG            = 0x8000,
+	CAT_INCOMING_REQUEST    = 0x10000,
+	CAT_DHT_LOG             = 0x20000,
+	CAT_DHT_OPERATION       = 0x40000,
+	CAT_PORT_MAPPING_LOG    = 0x80000,
+	CAT_PICKER_LOG          = 0x100000,
+	CAT_FILE_PROGRESS       = 0x200000,
+	CAT_PIECE_PROGRESS      = 0x400000,
+	CAT_UPLOAD              = 0x800000,
+	CAT_BLOCK_PROGRESS      = 0x1000000,
+};
+
+enum remove_flags
+{
+	RF_DELETE_FILES = 0x1,
+	RF_DELETE_PARTFILE = 0x2,
+};
+
+enum proxy_type_t
+{
+	proxy_none,
+	proxy_socks4,
+	proxy_socks5,
+	proxy_socks5_pw,
+	proxy_http,
+	proxy_http_pw
+};
+
+enum storage_mode_t
+{
+	SM_ALLOCATE = 0,
+	SM_SPARSE
+};
+
+enum state_t
+{
+	queued_for_checking,
+	checking_files,
+	downloading_metadata,
+	downloading,
+	finished,
+	seeding,
+	allocating,
+	checking_resume_data
+};
+
+struct torrent_status
+{
+	enum state_t state;
+	float progress;
+	char error[1024];
+	int next_announce;
+	char current_tracker[512];
+	long long total_download;
+	long long total_upload;
+	long long total_payload_download;
+	long long total_payload_upload;
+	long long total_failed_bytes;
+	long long total_redundant_bytes;
+	float download_rate;
+	float upload_rate;
+	float download_payload_rate;
+	float upload_payload_rate;
+	int num_seeds;
+	int num_peers;
+	int num_complete;
+	int num_incomplete;
+	int list_seeds;
+	int list_peers;
+	int connect_candidates;
+
+	// what to do?
+//	bitfield pieces;
+
+	int num_pieces;
+	long long total_done;
+	long long total_wanted_done;
+	long long total_wanted;
+	float distributed_copies;
+	int block_size;
+	int num_uploads;
+	int num_connections;
+	int uploads_limit;
+	int connections_limit;
+//	enum storage_mode_t storage_mode;
+	int up_bandwidth_queue;
+	int down_bandwidth_queue;
+	long long all_time_upload;
+	long long all_time_download;
+	int seed_rank;
+	int has_incoming;
+};
+
+struct libtorrent_alert;
+struct libtorrent_session;
+
+// the functions whose signature ends with::
+//
+// 	int first_tag, ...);
+//
+// takes a tag list. The tag list is a series of tag-value pairs. The tags are
+// constants identifying which property the value controls. The type of the
+// value varies between tags. The enumeration above specifies which type it
+// expects. All tag lists must always be terminated by TAG_END.
+
+// creates a session object and returns it as an opaque ``void*``. Returns
+// ``NULL`` on error. Any non-NULL return value must be freed by passing it to
+// ``session_close()``.
+
+// use SET_* tags in tag list to configure the session.
+struct libtorrent_session* session_create(int first_tag, ...);
+void session_close(struct libtorrent_session* ses);
+
+// use TOR_* tags in tag list
+// returns a torrent handle, or a negative value if an error prevented the
+// torrent from being added.
+int session_add_torrent(struct libtorrent_session* ses, int first_tag, ...);
+
+// remove specitied torrent from specified session. flags from remove_flags enum
+void session_remove_torrent(struct libtorrent_session* ses, int tor, int flags);
+
+// pass in an array of (possibly uninitialized) pointers to opaque type
+// libtorrent_alert. Pass in the number of elements the array can hold, in
+// in-out parameter ``array_size``. The number of valid alert pointers stored in
+// the array is returned back via the value ``array_size`` points to.
+int session_pop_alerts(struct libtorrent_session* ses, struct libtorrent_alert const** dest, int* array_size);
+
+// updates session settings. use ``SET_*`` tags.
+int session_set_settings(struct libtorrent_session* ses, int first_tag, ...);
+
+// reads one session setting. use ``SET_*`` tag. For string, the value buffer
+// must be large enough to hold the full string, or it will be truncated.
+// the size of the returned value will be written to the ``value_size`` out
+// parameter. Returns non-zero on failure.
+int session_get_setting(struct libtorrent_session* ses, int tag, void* value, int* value_size);
+
+// TODO: remove this in favor of post_torrent_updates()
+int torrent_get_status(int tor, struct torrent_status* s, int struct_size);
+
+// prints the alert's human readable message into buf, truncating it at ``size``
+int alert_message(struct libtorrent_alert const* alert, char* buf, int size);
+
+// returns the timestamp of when the alert was posted. The timestamp is the
+// number of microseconds since epoch.
+int64_t alert_timestamp(struct libtorrent_alert const* alert);
+
+// returns the type of the alert. defined in libtorrent_alerts.h
+int alert_type(struct libtorrent_alert const* alert);
+
+// returns the category of the alert. This is a bitmask with one or more bits
+// set from the category_t enum.
+int alert_category(struct libtorrent_alert const* alert);
+
+// if this is an alert with an associated torrent handle, return that handle.
+// Otherwise, return -1.
+int alert_torrent_handle(struct libtorrent_alert const* alert);
+
+// if ``alert`` refers to a session_stats_alert, returns a pointer to session
+// counters and NULL otherwise. The ``count`` out parameter is set to the number
+// of counters in the array. The returned array is valid until the next call to
+// session_pop_alerts(). To find a value in the array, use
+// ``find_metric_idx()``.
+int64_t const* alert_stats_counters(struct libtorrent_alert const* alert, int* count);
+
+int find_metric_idx(char const* name);
+
+// set a torrent specific setting. ``tor`` is the torrent handle whose setting
+// to change. This call can change multiple settings in a single call. The
+// variadic parameters is tag list of pairs of ``TSET_*`` enum values and the
+// corresponding ``int`` value to set.
+int torrent_set_settings(int tor, int first_tag, ...);
+
+// get a torrent specific setting. ``tor`` is the torrent handle, ``tag``
+// is a value from the ``TSET_*`` enum, indicating which setting to read.
+// ``value`` should point to a buffer large enough to receive the value
+// the size of the value written to the buffer is
+// returned in the in-out-parameter ``value_size``, which need to be initialized
+// to the buffer size.
+int torrent_get_setting(int tor, int tag, void* value, int* value_size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ bindings/c/src/library.cpp
@@ -0,0 +1,544 @@
+/*
+
+Copyright (c) 2009, Arvid Norberg
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in
+      the documentation and/or other materials provided with the distribution.
+    * Neither the name of the author nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include "libtorrent/session.hpp"
+#include "libtorrent/magnet_uri.hpp"
+#include "libtorrent/torrent_handle.hpp"
+#include "libtorrent/read_resume_data.hpp"
+#include "libtorrent/session_stats.hpp"
+#include "libtorrent/alert_types.hpp"
+
+#include <libtorrent.h>
+#include <stdarg.h>
+
+namespace {
+
+std::vector<lt::torrent_handle> handles;
+std::vector<int> free_handle_slots;
+
+int find_handle(lt::torrent_handle h)
+{
+	std::vector<lt::torrent_handle>::const_iterator i
+		= std::find(handles.begin(), handles.end(), h);
+	if (i == handles.end()) return -1;
+	return i - handles.begin();
+}
+
+lt::torrent_handle get_handle(int i)
+{
+	if (i < 0 || i >= int(handles.size())) return lt::torrent_handle();
+	return handles[i];
+}
+
+int add_handle(lt::torrent_handle const& h)
+{
+	std::vector<lt::torrent_handle>::iterator i = std::find_if(handles.begin()
+		, handles.end()
+		, [](lt::torrent_handle const& h) { return !h.is_valid(); });
+	if (i != handles.end())
+	{
+		*i = h;
+		return i - handles.begin();
+	}
+
+	if (free_handle_slots.empty())
+	{
+		handles.push_back(h);
+		return handles.size() - 1;
+	}
+
+	int const ret = free_handle_slots.back();
+	free_handle_slots.pop_back();
+	handles[ret] = h;
+	return ret;
+}
+
+void remove_handle(int const h)
+{
+	handles[h] = lt::torrent_handle{};
+	if (h == int(handles.size() - 1))
+	{
+		handles.pop_back();
+	}
+	else
+	{
+		free_handle_slots.push_back(h);
+	}
+}
+
+int set_int_value(void* dst, int* size, int val)
+{
+	if (*size < int(sizeof(int))) return -2;
+	std::memcpy(dst, &val, sizeof(int));
+	*size = sizeof(int);
+	return 0;
+}
+
+int set_str_value(void* dst, int* size, std::string val)
+{
+	if (*size <= static_cast<int>(val.size())) return -2;
+	std::memcpy(dst, val.c_str(), val.size() + 1);
+	*size = val.size() + 1;
+	return 0;
+}
+
+lt::add_torrent_params make_add_torrent_params(int tag, va_list lp)
+{
+	lt::add_torrent_params params;
+
+	char const* torrent_data = nullptr;
+	int torrent_size = 0;
+
+	char const* resume_data = nullptr;
+	int resume_size = 0;
+
+	lt::error_code ec;
+	while (tag != TAG_END)
+	{
+		switch (tag)
+		{
+			case TOR_FILENAME:
+				params.ti = std::make_shared<lt::torrent_info>(va_arg(lp, char const*), ec);
+				break;
+			case TOR_TORRENT:
+				torrent_data = va_arg(lp, char const*);
+				break;
+			case TOR_TORRENT_SIZE:
+				torrent_size = va_arg(lp, int);
+				break;
+			case TOR_INFOHASH:
+				params.info_hash = lt::sha1_hash(va_arg(lp, char const*));
+				break;
+			case TOR_MAGNETLINK:
+				parse_magnet_uri(va_arg(lp, char const*), params, ec);
+				break;
+			case TOR_TRACKER_URL:
+				params.trackers.push_back(va_arg(lp, char const*));
+				break;
+			case TOR_RESUME_DATA:
+				resume_data = va_arg(lp, char const*);
+				if (resume_data && resume_size)
+					params = lt::read_resume_data({resume_data, resume_size});
+				break;
+			case TOR_RESUME_DATA_SIZE:
+				resume_size = va_arg(lp, int);
+				if (resume_data && resume_size)
+					params = lt::read_resume_data({resume_data, resume_size});
+				break;
+			case TOR_SAVE_PATH:
+				params.save_path = va_arg(lp, char const*);
+				break;
+			case TOR_NAME:
+				params.name = va_arg(lp, char const*);
+				break;
+			case TOR_FLAGS:
+				params.flags = lt::torrent_flags_t(va_arg(lp, int));
+				break;
+			case TOR_USER_DATA:
+				params.userdata = va_arg(lp, void*);
+				break;
+			case TOR_STORAGE_MODE:
+				params.storage_mode = static_cast<lt::storage_mode_t>(va_arg(lp, int));
+				break;
+			default:
+				// ignore unknown tags
+				va_arg(lp, void*);
+				break;
+		}
+
+		tag = va_arg(lp, int);
+	}
+	va_end(lp);
+
+	if (!params.ti && torrent_data && torrent_size)
+		params.ti = std::make_shared<lt::torrent_info>(lt::span<char const>{torrent_data, torrent_size}, lt::from_span);
+
+	return params;
+}
+} // unnamed namespace
+
+// defined in src/settings.cpp
+extern lt::settings_pack make_settings(int tag, va_list);
+extern int settings_key(int tag);
+
+extern "C"
+{
+
+TORRENT_EXPORT libtorrent_session* session_create(int tag, ...)
+{
+	using namespace lt;
+
+	va_list lp;
+	va_start(lp, tag);
+	lt::settings_pack pack = make_settings(tag, lp);
+	va_end(lp);
+
+	return reinterpret_cast<libtorrent_session*>(new (std::nothrow) session(std::move(pack)));
+}
+
+TORRENT_EXPORT void session_close(libtorrent_session* ses)
+{
+	delete reinterpret_cast<lt::session*>(ses);
+}
+
+TORRENT_EXPORT int session_add_torrent(libtorrent_session* ses, int tag, ...)
+{
+	using namespace lt;
+
+	va_list lp;
+	va_start(lp, tag);
+	lt::add_torrent_params params = make_add_torrent_params(tag, lp);
+	va_end(lp);
+
+	auto* s = reinterpret_cast<lt::session*>(ses);
+	lt::error_code ec;
+	lt::torrent_handle h = s->add_torrent(params, ec);
+	if (ec) return -1;
+
+	if (!h.is_valid()) return -1;
+
+	int i = find_handle(h);
+	if (i == -1) i = add_handle(h);
+
+	return i;
+}
+
+TORRENT_EXPORT void session_remove_torrent(libtorrent_session* ses, int tor, int flags)
+{
+	lt::torrent_handle h = get_handle(tor);
+	if (!h.is_valid()) return;
+
+	remove_handle(tor);
+
+	auto* s = reinterpret_cast<lt::session*>(ses);
+	s->remove_torrent(h, lt::remove_flags_t(flags));
+}
+
+TORRENT_EXPORT int session_pop_alerts(libtorrent_session* ses, libtorrent_alert const** dest, int* len)
+{
+	auto* s = reinterpret_cast<lt::session*>(ses);
+	if (len == nullptr) return -1;
+	if (*len < 0) return -1;
+	if (*len == 0) return 0;
+
+	std::vector<lt::alert*> ret;
+	s->pop_alerts(&ret);
+
+	// TODO: figure out what to do with the alert we may have lost here. Save
+	// them to the next call somehow?
+	int const to_copy = std::min(int(ret.size()), *len);
+	std::copy(ret.begin(), ret.begin() + to_copy, reinterpret_cast<lt::alert const**>(dest));
+	*len = to_copy;
+	return 0; // for now
+}
+
+TORRENT_EXPORT int session_set_settings(libtorrent_session* ses, int tag, ...)
+{
+	va_list lp;
+	va_start(lp, tag);
+	lt::settings_pack pack = make_settings(tag, lp);
+	va_end(lp);
+
+	auto* s = reinterpret_cast<lt::session*>(ses);
+	s->apply_settings(std::move(pack));
+
+	return 0;
+}
+
+TORRENT_EXPORT int session_get_setting(libtorrent_session* ses, int tag, void* value, int* value_size)
+{
+	auto* s = reinterpret_cast<lt::session*>(ses);
+	lt::settings_pack sett = s->get_settings();
+
+	int const key = settings_key(tag);
+	if (key < 0) return key;
+
+	using sp = lt::settings_pack;
+
+	switch (key & lt::settings_pack::type_mask)
+	{
+		case sp::string_type_base:
+			return set_str_value(value, value_size, sett.get_str(key));
+		case sp::int_type_base:
+			return set_int_value(value, value_size, sett.get_int(key));
+		case sp::bool_type_base:
+			return set_int_value(value, value_size, sett.get_bool(key));
+		default:
+			return -1;
+	}
+}
+
+TORRENT_EXPORT int torrent_get_status(int tor, torrent_status* s, int struct_size)
+{
+	lt::torrent_handle h = get_handle(tor);
+	if (!h.is_valid()) return -1;
+
+	lt::torrent_status ts = h.status();
+
+	if (struct_size != sizeof(torrent_status)) return -1;
+
+	s->state = (state_t)ts.state;
+	s->progress = ts.progress;
+	std::string err_msg = ts.errc.message();
+	strncpy(s->error, err_msg.c_str(), sizeof(s->error));
+	s->next_announce = lt::total_seconds(ts.next_announce);
+	strncpy(s->current_tracker, ts.current_tracker.c_str(), sizeof(s->current_tracker));
+	s->total_download = ts.total_download;
+	s->total_upload = ts.total_upload;
+	s->total_payload_download = ts.total_payload_download;
+	s->total_payload_upload = ts.total_payload_upload;
+	s->total_failed_bytes = ts.total_failed_bytes;
+	s->total_redundant_bytes = ts.total_redundant_bytes;
+	s->download_rate = ts.download_rate;
+	s->upload_rate = ts.upload_rate;
+	s->download_payload_rate = ts.download_payload_rate;
+	s->upload_payload_rate = ts.upload_payload_rate;
+	s->num_seeds = ts.num_seeds;
+	s->num_peers = ts.num_peers;
+	s->num_complete = ts.num_complete;
+	s->num_incomplete = ts.num_incomplete;
+	s->list_seeds = ts.list_seeds;
+	s->list_peers = ts.list_peers;
+	s->connect_candidates = ts.connect_candidates;
+	s->num_pieces = ts.num_pieces;
+	s->total_done = ts.total_done;
+	s->total_wanted_done = ts.total_wanted_done;
+	s->total_wanted = ts.total_wanted;
+	s->distributed_copies = ts.distributed_copies;
+	s->block_size = ts.block_size;
+	s->num_uploads = ts.num_uploads;
+	s->num_connections = ts.num_connections;
+	s->uploads_limit = ts.uploads_limit;
+	s->connections_limit = ts.connections_limit;
+//	s->storage_mode = (storage_mode_t)ts.storage_mode;
+	s->up_bandwidth_queue = ts.up_bandwidth_queue;
+	s->down_bandwidth_queue = ts.down_bandwidth_queue;
+	s->all_time_upload = ts.all_time_upload;
+	s->all_time_download = ts.all_time_download;
+	s->seed_rank = ts.seed_rank;
+	s->has_incoming = ts.has_incoming;
+	return 0;
+}
+
+TORRENT_EXPORT int alert_message(libtorrent_alert const* alert, char* buf, int size)
+{
+	auto const* a = reinterpret_cast<lt::alert const*>(alert);
+	auto const msg = a->message();
+	std::strncpy(buf, msg.c_str(), size - 1);
+	buf[size - 1] = '\0';
+	return 0;
+}
+
+TORRENT_EXPORT int64_t const* alert_stats_counters(struct libtorrent_alert const* alert, int* count)
+{
+	auto const* a = reinterpret_cast<lt::alert const*>(alert);
+	auto const* sa = lt::alert_cast<lt::session_stats_alert>(a);
+	if (sa == nullptr) return nullptr;
+
+	lt::span<std::int64_t const> counters = sa->counters();
+	*count = int(counters.size());
+	return counters.data();
+}
+
+TORRENT_EXPORT std::int64_t alert_timestamp(struct libtorrent_alert const* alert)
+{
+	auto const* a = reinterpret_cast<lt::alert const*>(alert);
+	return std::chrono::duration_cast<std::chrono::microseconds>(
+		a->timestamp().time_since_epoch()).count();
+}
+
+TORRENT_EXPORT int alert_type(struct libtorrent_alert const* alert)
+{
+	auto const* a = reinterpret_cast<lt::alert const*>(alert);
+	return a->type();
+}
+
+TORRENT_EXPORT int alert_category(struct libtorrent_alert const* alert)
+{
+	auto const* a = reinterpret_cast<lt::alert const*>(alert);
+	return static_cast<int>(a->category());
+}
+
+TORRENT_EXPORT int alert_torrent_handle(struct libtorrent_alert const* alert)
+{
+	auto const* a = reinterpret_cast<lt::alert const*>(alert);
+	int const type = a->type();
+	switch (type)
+	{
+		// torrent_alert
+		case lt::torrent_added_alert::alert_type:
+		case lt::torrent_removed_alert::alert_type:
+		case lt::read_piece_alert::alert_type:
+		case lt::file_completed_alert::alert_type:
+		case lt::file_renamed_alert::alert_type:
+		case lt::file_rename_failed_alert::alert_type:
+		case lt::performance_alert::alert_type:
+		case lt::state_changed_alert::alert_type:
+		case lt::hash_failed_alert::alert_type:
+		case lt::torrent_finished_alert::alert_type:
+		case lt::piece_finished_alert::alert_type:
+		case lt::storage_moved_alert::alert_type:
+		case lt::storage_moved_failed_alert::alert_type:
+		case lt::torrent_deleted_alert::alert_type:
+		case lt::torrent_delete_failed_alert::alert_type:
+		case lt::save_resume_data_alert::alert_type:
+		case lt::save_resume_data_failed_alert::alert_type:
+		case lt::torrent_paused_alert::alert_type:
+		case lt::torrent_resumed_alert::alert_type:
+		case lt::torrent_checked_alert::alert_type:
+		case lt::url_seed_alert::alert_type:
+		case lt::file_error_alert::alert_type:
+		case lt::metadata_failed_alert::alert_type:
+		case lt::metadata_received_alert::alert_type:
+		case lt::fastresume_rejected_alert::alert_type:
+		case lt::stats_alert::alert_type:
+		case lt::cache_flushed_alert::alert_type:
+		case lt::anonymous_mode_alert::alert_type:
+		case lt::torrent_error_alert::alert_type:
+		case lt::torrent_need_cert_alert::alert_type:
+		case lt::add_torrent_alert::alert_type:
+		case lt::torrent_log_alert::alert_type:
+		// peer_alert
+		case lt::peer_ban_alert::alert_type:
+		case lt::peer_unsnubbed_alert::alert_type:
+		case lt::peer_snubbed_alert::alert_type:
+		case lt::peer_error_alert::alert_type:
+		case lt::peer_connect_alert::alert_type:
+		case lt::peer_disconnected_alert::alert_type:
+		case lt::invalid_request_alert::alert_type:
+		case lt::request_dropped_alert::alert_type:
+		case lt::block_timeout_alert::alert_type:
+		case lt::block_finished_alert::alert_type:
+		case lt::block_downloading_alert::alert_type:
+		case lt::unwanted_block_alert::alert_type:
+		case lt::peer_blocked_alert::alert_type:
+		case lt::lsd_peer_alert::alert_type:
+		case lt::peer_log_alert::alert_type:
+		case lt::incoming_request_alert::alert_type:
+		case lt::picker_log_alert::alert_type:
+		case lt::block_uploaded_alert::alert_type:
+		// tracker alert
+		case lt::tracker_error_alert::alert_type:
+		case lt::tracker_warning_alert::alert_type:
+		case lt::scrape_reply_alert::alert_type:
+		case lt::scrape_failed_alert::alert_type:
+		case lt::tracker_reply_alert::alert_type:
+		case lt::dht_reply_alert::alert_type:
+		case lt::tracker_announce_alert::alert_type:
+		case lt::trackerid_alert::alert_type:
+		{
+			lt::torrent_handle h = static_cast<lt::torrent_alert const*>(a)->handle;
+			return find_handle(h);
+		}
+		default: return -1;
+	};
+}
+
+TORRENT_EXPORT int find_metric_idx(char const* name)
+{
+	return lt::find_metric_idx(name);
+}
+
+TORRENT_EXPORT int torrent_set_settings(int tor, int tag, ...)
+{
+	lt::torrent_handle h = get_handle(tor);
+	if (!h.is_valid()) return -1;
+
+	va_list lp;
+	va_start(lp, tag);
+
+	bool flags_set = false;
+	std::uint64_t flags = 0;
+	std::uint64_t mask = UINT64_MAX;
+
+	while (tag != TAG_END)
+	{
+		switch (tag)
+		{
+			case TSET_UPLOAD_RATE_LIMIT:
+				h.set_upload_limit(va_arg(lp, int));
+				break;
+			case TSET_DOWNLOAD_RATE_LIMIT:
+				h.set_download_limit(va_arg(lp, int));
+				break;
+			case TSET_MAX_UPLOAD_SLOTS:
+				h.set_max_uploads(va_arg(lp, int));
+				break;
+			case TSET_MAX_CONNECTIONS:
+				h.set_max_connections(va_arg(lp, int));
+				break;
+			case TSET_FLAGS:
+				flags = va_arg(lp, int);
+				flags_set = true;
+				break;
+			case TSET_FLAGS_MASK:
+				mask = va_arg(lp, int);
+				break;
+			default:
+				// ignore unknown tags
+				va_arg(lp, void*);
+				break;
+		}
+
+		tag = va_arg(lp, int);
+	}
+	va_end(lp);
+
+	if (flags_set)
+		h.set_flags(lt::torrent_flags_t(flags), lt::torrent_flags_t(mask));
+	return 0;
+}
+
+TORRENT_EXPORT int torrent_get_setting(int const tor, int const tag, void* value, int* value_size)
+{
+	lt::torrent_handle h = get_handle(tor);
+	if (!h.is_valid()) return -1;
+
+	switch (tag)
+	{
+		case TSET_UPLOAD_RATE_LIMIT:
+			return set_int_value(value, value_size, h.upload_limit());
+		case TSET_DOWNLOAD_RATE_LIMIT:
+			return set_int_value(value, value_size, h.download_limit());
+		case TSET_MAX_UPLOAD_SLOTS:
+			return set_int_value(value, value_size, h.max_uploads());
+		case TSET_MAX_CONNECTIONS:
+			return set_int_value(value, value_size, h.max_connections());
+		case TSET_FLAGS:
+			return set_int_value(value, value_size, static_cast<int>(static_cast<std::uint64_t>(h.flags())));
+		default:
+			return -2;
+	}
+}
+
+} // extern "C"
+
