4b30600684ee85e419c4d1a2d7aa0ed50593cb41
Arvid Norberg <arvid@libtorrent.org>
2020-08-19 12:08:37 +0000

    fix hard-link error handling on haiku

--- src/path.cpp
+++ src/path.cpp
@@ -355,7 +355,13 @@ namespace {
 		// most errors are passed through, except for the ones that indicate that
 		// hard links are not supported and require a copy.
 		// TODO: 2 test this on a FAT volume to see what error we get!
-		if (errno != EMLINK && errno != EXDEV)
+		if (errno != EMLINK
+			&& errno != EXDEV
+#ifdef TORRENT_BEOS
+			// haiku returns EPERM when the filesystem doesn't support hard link
+			&& errno != EPERM
+#endif
+			)
 		{
 			// some error happened, report up to the caller
 			ec.assign(errno, system_category());
