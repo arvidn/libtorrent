a30ae30f417ef7651051382b945b83c1a2527b60
arvidn <arvid@libtorrent.org>
2020-08-31 09:59:37 +0200

    remove outdated sentence from documentation

--- include/libtorrent/torrent_info.hpp
+++ include/libtorrent/torrent_info.hpp
@@ -319,8 +319,7 @@ namespace libtorrent {
 		// or http seed.
 		//
 		// ``add_url_seed()`` and ``add_http_seed()`` adds one url to the list of
-		// url/http seeds. Currently, the only transport protocol supported for
-		// the url is http.
+		// url/http seeds.
 		//
 		// ``set_web_seeds()`` replaces all web seeds with the ones specified in
 		// the ``seeds`` vector.
