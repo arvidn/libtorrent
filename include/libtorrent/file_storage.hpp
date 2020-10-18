e5a9b800075f49908f2dda94a43f76782017b17c
arvidn <arvid@libtorrent.org>
2020-10-18 09:27:41 +0200

    fix incorrectly inlined move-assignment of file_storage

--- include/libtorrent/file_storage.hpp
+++ include/libtorrent/file_storage.hpp
@@ -220,7 +220,7 @@ namespace libtorrent {
 		file_storage(file_storage const&);
 		file_storage& operator=(file_storage const&);
 		file_storage(file_storage&&) noexcept;
-		file_storage& operator=(file_storage&&) = default;
+		file_storage& operator=(file_storage&&);
 
 		// returns true if the piece length has been initialized
 		// on the file_storage. This is typically taken as a proxy
