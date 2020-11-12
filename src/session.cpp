877fd60562e2b1cc26d42392f565c006ec702450
Arvid Norberg <arvid@libtorrent.org>
2020-11-12 22:06:49 +0100

    fix issue with moving the session object

--- src/session.cpp
+++ src/session.cpp
@@ -358,8 +358,9 @@ namespace {
 		if (internal_executor)
 		{
 			// start a thread for the message pump
+			auto s = m_io_service;
 			m_thread = std::make_shared<std::thread>(
-				[&] { m_io_service->run(); });
+				[=] { s->run(); });
 		}
 	}
 
