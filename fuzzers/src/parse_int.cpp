afa406f89099b8d0e4ae9fa9bb62f90d93cc6be4
arvidn <arvid@libtorrent.org>
2020-11-20 01:23:03 +0100

    fix a bug in the parse_int fuzzer, causing an uninitialized variable to be read

--- fuzzers/src/parse_int.cpp
+++ fuzzers/src/parse_int.cpp
@@ -34,7 +34,7 @@ POSSIBILITY OF SUCH DAMAGE.
 extern "C" int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size)
 {
 	lt::bdecode_errors::error_code_enum ec;
-	std::int64_t val;
+	std::int64_t val = 0;
 	lt::parse_int(reinterpret_cast<char const*>(data), reinterpret_cast<char const*>(data) + size, ':', val, ec);
 	return 0;
 }
