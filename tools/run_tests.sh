c3cb955d9d631200acecf773794b78bb465a8a6e
arvidn <arvid@libtorrent.org>
2020-12-29 01:22:06 +0100

    make tools/run_tests.sh run build with lower priority

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -1,22 +1,22 @@
 set -ex
 
 cd test
-b2 -j$(nproc) -l300 link=static deprecated-functions=on,off crypto=openssl,built-in
-b2 -j$(nproc) -l300 link=static crypto=gnutls webtorrent=on
+nice b2 -j$(nproc) -l300 link=static deprecated-functions=on,off crypto=openssl,built-in
+nice b2 -j$(nproc) -l300 link=static crypto=gnutls webtorrent=on
 
 cd ../simulation
-b2 -j$(nproc) -l300 link=static
+nice b2 -j$(nproc) -l300 link=static
 
 cd ../examples
-b2 -j$(nproc) link=static deprecated-functions=on,off crypto=openssl
+nice b2 -j$(nproc) link=static deprecated-functions=on,off crypto=openssl
 
 cd ../tools
-b2 -j$(nproc) link=static deprecated-functions=on,off crypto=openssl
+nice b2 -j$(nproc) link=static deprecated-functions=on,off crypto=openssl
 
 cd ../docs
 make spell-check
 
 cd ../bindings/python
-b2 -j$(nproc) stage_module stage_dependencies
+nice b2 -j$(nproc) stage_module stage_dependencies
 LD_LIBRARY_PATH=./dependencies python3 test.py
 
