8c51aace2dc2cbe4c3ac92c5961b396cf128f52e
AllSeeingEyeTolledEweSew <allseeingeyetolledewesew@protonmail.com>
2021-02-07 06:06:55 +0000

    Apply python formatting to a selection of tools

--- bindings/python/client.py
+++ bindings/python/client.py
@@ -5,32 +5,41 @@
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 
-import sys
 import atexit
-import libtorrent as lt
-import time
+import optparse
 import os.path
+import sys
+import time
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Union
+
+import libtorrent as lt
 
 
 class WindowsConsole:
-    def __init__(self):
+    def __init__(self) -> None:
         self.console = Console.getconsole()
 
-    def clear(self):
+    def clear(self) -> None:
         self.console.page()
 
-    def write(self, str):
-        self.console.write(str)
+    def write(self, msg: str) -> None:
+        self.console.write(msg)
 
-    def sleep_and_input(self, seconds):
+    def sleep_and_input(self, seconds: float) -> Optional[bytes]:
         time.sleep(seconds)
-        if msvcrt.kbhit():
-            return msvcrt.getch()
+        # Helps mypy
+        if sys.platform == "win32":
+            if msvcrt.kbhit():
+                return msvcrt.getch()
         return None
 
 
 class UnixConsole:
-    def __init__(self):
+    def __init__(self) -> None:
         self.fd = sys.stdin
         self.old = termios.tcgetattr(self.fd.fileno())
         new = termios.tcgetattr(self.fd.fileno())
@@ -41,179 +50,196 @@ class UnixConsole:
 
         atexit.register(self._onexit)
 
-    def _onexit(self):
+    def _onexit(self) -> None:
         termios.tcsetattr(self.fd.fileno(), termios.TCSADRAIN, self.old)
 
-    def clear(self):
-        sys.stdout.write('\033[2J\033[0;0H')
+    def clear(self) -> None:
+        sys.stdout.write("\033[2J\033[0;0H")
         sys.stdout.flush()
 
-    def write(self, str):
-        sys.stdout.write(str)
+    def write(self, msg: str) -> None:
+        sys.stdout.write(msg)
         sys.stdout.flush()
 
-    def sleep_and_input(self, seconds):
-        read, __, __ = select.select(
-            [self.fd.fileno()], [], [], seconds)
+    def sleep_and_input(self, seconds: float) -> Optional[str]:
+        read, __, __ = select.select([self.fd.fileno()], [], [], seconds)
         if len(read) > 0:
             return self.fd.read(1)
         return None
 
 
-if os.name == 'nt':
-    import Console
+ConsoleType = Union[UnixConsole, WindowsConsole]
+
+
+if os.name == "nt":
     import msvcrt
+
+    import Console
 else:
-    import termios
     import select
+    import termios
 
 
-def write_line(console, line):
-    console.write(line)
-
-
-def add_suffix(val):
-    prefix = ['B', 'kB', 'MB', 'GB', 'TB']
+def add_suffix(val: float) -> str:
+    prefix = ["B", "kB", "MB", "GB", "TB"]
     for i in range(len(prefix)):
         if abs(val) < 1000:
             if i == 0:
-                return '%5.3g%s' % (val, prefix[i])
+                return "%5.3g%s" % (val, prefix[i])
             else:
-                return '%4.3g%s' % (val, prefix[i])
+                return "%4.3g%s" % (val, prefix[i])
         val /= 1000
 
-    return '%6.3gPB' % val
+    return "%6.3gPB" % val
 
 
-def progress_bar(progress, width):
-    assert(progress <= 1)
+def progress_bar(progress: float, width: int) -> str:
+    assert progress <= 1
     progress_chars = int(progress * width + 0.5)
-    return progress_chars * '#' + (width - progress_chars) * '-'
+    return progress_chars * "#" + (width - progress_chars) * "-"
 
 
-def print_peer_info(console, peers):
+def print_peer_info(console: ConsoleType, peers: List[Any]) -> None:
 
-    out = (' down    (total )   up     (total )'
-           '  q  r flags  block progress  client\n')
+    out = " down    (total )   up     (total )" "  q  r flags  block progress  client\n"
 
     for p in peers:
 
-        out += '%s/s ' % add_suffix(p.down_speed)
-        out += '(%s) ' % add_suffix(p.total_download)
-        out += '%s/s ' % add_suffix(p.up_speed)
-        out += '(%s) ' % add_suffix(p.total_upload)
-        out += '%2d ' % p.download_queue_length
-        out += '%2d ' % p.upload_queue_length
+        out += "%s/s " % add_suffix(p.down_speed)
+        out += "(%s) " % add_suffix(p.total_download)
+        out += "%s/s " % add_suffix(p.up_speed)
+        out += "(%s) " % add_suffix(p.total_upload)
+        out += "%2d " % p.download_queue_length
+        out += "%2d " % p.upload_queue_length
 
-        out += 'I' if p.flags & lt.peer_info.interesting else '.'
-        out += 'C' if p.flags & lt.peer_info.choked else '.'
-        out += 'i' if p.flags & lt.peer_info.remote_interested else '.'
-        out += 'c' if p.flags & lt.peer_info.remote_choked else '.'
-        out += 'e' if p.flags & lt.peer_info.supports_extensions else '.'
-        out += 'l' if p.flags & lt.peer_info.local_connection else 'r'
-        out += ' '
+        out += "I" if p.flags & lt.peer_info.interesting else "."
+        out += "C" if p.flags & lt.peer_info.choked else "."
+        out += "i" if p.flags & lt.peer_info.remote_interested else "."
+        out += "c" if p.flags & lt.peer_info.remote_choked else "."
+        out += "e" if p.flags & lt.peer_info.supports_extensions else "."
+        out += "l" if p.flags & lt.peer_info.local_connection else "r"
+        out += " "
 
         if p.downloading_piece_index >= 0:
-            assert(p.downloading_progress <= p.downloading_total)
-            out += progress_bar(float(p.downloading_progress) /
-                                p.downloading_total, 15)
+            assert p.downloading_progress <= p.downloading_total
+            out += progress_bar(float(p.downloading_progress) / p.downloading_total, 15)
         else:
             out += progress_bar(0, 15)
-        out += ' '
+        out += " "
 
         if p.flags & lt.peer_info.handshake:
-            id = 'waiting for handshake'
+            id = "waiting for handshake"
         elif p.flags & lt.peer_info.connecting:
-            id = 'connecting to peer'
+            id = "connecting to peer"
         else:
             id = p.client
 
-        out += '%s\n' % id[:10]
+        out += "%s\n" % id[:10]
 
-    write_line(console, out)
+    console.write(out)
 
 
-def print_download_queue(console, download_queue):
+def print_download_queue(
+    console: ConsoleType, download_queue: List[Dict[str, Any]]
+) -> None:
 
     out = ""
 
     for e in download_queue:
-        out += '%4d: [' % e['piece_index']
-        for b in e['blocks']:
-            s = b['state']
+        out += "%4d: [" % e["piece_index"]
+        for b in e["blocks"]:
+            s = b["state"]
             if s == 3:
-                out += '#'
+                out += "#"
             elif s == 2:
-                out += '='
+                out += "="
             elif s == 1:
-                out += '-'
+                out += "-"
             else:
-                out += ' '
-        out += ']\n'
+                out += " "
+        out += "]\n"
 
-    write_line(console, out)
+    console.write(out)
 
 
-def add_torrent(ses, filename, options):
+def add_torrent(ses: lt.session, filename: str, options: optparse.Values) -> None:
     atp = lt.add_torrent_params()
-    if filename.startswith('magnet:'):
+    if filename.startswith("magnet:"):
         atp = lt.parse_magnet_uri(filename)
     else:
         ti = lt.torrent_info(filename)
-        resume_file = os.path.join(options.save_path, ti.name() + '.fastresume')
+        resume_file = os.path.join(options.save_path, ti.name() + ".fastresume")
         try:
-            atp = lt.read_resume_data(open(resume_file, 'rb').read())
+            atp = lt.read_resume_data(open(resume_file, "rb").read())
         except Exception as e:
             print('failed to open resume file "%s": %s' % (resume_file, e))
         atp.ti = ti
 
     atp.save_path = options.save_path
     atp.storage_mode = lt.storage_mode_t.storage_mode_sparse
-    atp.flags |= lt.torrent_flags.duplicate_is_error \
-        | lt.torrent_flags.auto_managed \
+    atp.flags |= (
+        lt.torrent_flags.duplicate_is_error
+        | lt.torrent_flags.auto_managed
         | lt.torrent_flags.duplicate_is_error
+    )
     ses.async_add_torrent(atp)
 
 
-def main():
-    from optparse import OptionParser
+def main() -> None:
+    parser = optparse.OptionParser()
 
-    parser = OptionParser()
-
-    parser.add_option('-p', '--port', type='int', help='set listening port')
+    parser.add_option("-p", "--port", type="int", help="set listening port")
 
     parser.add_option(
-        '-i', '--listen-interface', type='string',
-        help='set interface for incoming connections', )
+        "-i",
+        "--listen-interface",
+        type="string",
+        help="set interface for incoming connections",
+    )
 
     parser.add_option(
-        '-o', '--outgoing-interface', type='string',
-        help='set interface for outgoing connections')
+        "-o",
+        "--outgoing-interface",
+        type="string",
+        help="set interface for outgoing connections",
+    )
 
     parser.add_option(
-        '-d', '--max-download-rate', type='float',
-        help='the maximum download rate given in kB/s. 0 means infinite.')
+        "-d",
+        "--max-download-rate",
+        type="float",
+        help="the maximum download rate given in kB/s. 0 means infinite.",
+    )
 
     parser.add_option(
-        '-u', '--max-upload-rate', type='float',
-        help='the maximum upload rate given in kB/s. 0 means infinite.')
+        "-u",
+        "--max-upload-rate",
+        type="float",
+        help="the maximum upload rate given in kB/s. 0 means infinite.",
+    )
 
     parser.add_option(
-        '-s', '--save-path', type='string',
-        help='the path where the downloaded file/folder should be placed.')
+        "-s",
+        "--save-path",
+        type="string",
+        help="the path where the downloaded file/folder should be placed.",
+    )
 
     parser.add_option(
-        '-r', '--proxy-host', type='string',
-        help='sets HTTP proxy host and port (separated by \':\')')
+        "-r",
+        "--proxy-host",
+        type="string",
+        help="sets HTTP proxy host and port (separated by ':')",
+    )
 
     parser.set_defaults(
         port=6881,
-        listen_interface='0.0.0.0',
-        outgoing_interface='',
+        listen_interface="0.0.0.0",
+        outgoing_interface="",
         max_download_rate=0,
         max_upload_rate=0,
-        save_path='.',
-        proxy_host=''
+        save_path=".",
+        proxy_host="",
     )
 
     (options, args) = parser.parse_args()
@@ -230,29 +256,30 @@ def main():
         options.max_download_rate = -1
 
     settings = {
-        'user_agent': 'python_client/' + lt.__version__,
-        'listen_interfaces': '%s:%d' % (options.listen_interface, options.port),
-        'download_rate_limit': int(options.max_download_rate),
-        'upload_rate_limit': int(options.max_upload_rate),
-        'alert_mask': lt.alert.category_t.all_categories,
-        'outgoing_interfaces': options.outgoing_interface,
+        "user_agent": "python_client/" + lt.__version__,
+        "listen_interfaces": "%s:%d" % (options.listen_interface, options.port),
+        "download_rate_limit": int(options.max_download_rate),
+        "upload_rate_limit": int(options.max_upload_rate),
+        "alert_mask": lt.alert.category_t.all_categories,
+        "outgoing_interfaces": options.outgoing_interface,
     }
 
-    if options.proxy_host != '':
-        settings['proxy_hostname'] = options.proxy_host.split(':')[0]
-        settings['proxy_type'] = lt.proxy_type_t.http
-        settings['proxy_port'] = options.proxy_host.split(':')[1]
+    if options.proxy_host != "":
+        settings["proxy_hostname"] = options.proxy_host.split(":")[0]
+        settings["proxy_type"] = lt.proxy_type_t.http
+        settings["proxy_port"] = options.proxy_host.split(":")[1]
 
     ses = lt.session(settings)
 
     # map torrent_handle to torrent_status
-    torrents = {}
+    torrents: Dict[lt.torrent_handle, lt.torrent_status] = {}
     alerts_log = []
 
-    for f in args:
-        add_torrent(ses, f, options)
+    for filename in args:
+        add_torrent(ses, filename, options)
 
-    if os.name == 'nt':
+    console: ConsoleType
+    if os.name == "nt":
         console = WindowsConsole()
     else:
         console = UnixConsole()
@@ -261,56 +288,70 @@ def main():
     while alive:
         console.clear()
 
-        out = ''
+        out = ""
 
         for h, t in torrents.items():
-            out += 'name: %-40s\n' % t.name[:40]
+            out += "name: %-40s\n" % t.name[:40]
 
             if t.state != lt.torrent_status.seeding:
-                state_str = ['queued', 'checking', 'downloading metadata',
-                             'downloading', 'finished', 'seeding',
-                             '', 'checking fastresume']
-                out += state_str[t.state] + ' '
+                state_str = [
+                    "queued",
+                    "checking",
+                    "downloading metadata",
+                    "downloading",
+                    "finished",
+                    "seeding",
+                    "",
+                    "checking fastresume",
+                ]
+                out += state_str[t.state] + " "
 
-                out += '%5.4f%% ' % (t.progress * 100)
+                out += "%5.4f%% " % (t.progress * 100)
                 out += progress_bar(t.progress, 49)
-                out += '\n'
+                out += "\n"
 
-                out += 'total downloaded: %d Bytes\n' % t.total_done
-                out += 'peers: %d seeds: %d distributed copies: %d\n' % \
-                    (t.num_peers, t.num_seeds, t.distributed_copies)
-                out += '\n'
+                out += "total downloaded: %d Bytes\n" % t.total_done
+                out += "peers: %d seeds: %d distributed copies: %d\n" % (
+                    t.num_peers,
+                    t.num_seeds,
+                    t.distributed_copies,
+                )
+                out += "\n"
 
-            out += 'download: %s/s (%s) ' \
-                % (add_suffix(t.download_rate), add_suffix(t.total_download))
-            out += 'upload: %s/s (%s) ' \
-                % (add_suffix(t.upload_rate), add_suffix(t.total_upload))
+            out += "download: %s/s (%s) " % (
+                add_suffix(t.download_rate),
+                add_suffix(t.total_download),
+            )
+            out += "upload: %s/s (%s) " % (
+                add_suffix(t.upload_rate),
+                add_suffix(t.total_upload),
+            )
 
             if t.state != lt.torrent_status.seeding:
-                out += 'info-hash: %s\n' % t.info_hashes
-                out += 'next announce: %s\n' % t.next_announce
-                out += 'tracker: %s\n' % t.current_tracker
+                out += "info-hash: %s\n" % t.info_hashes
+                out += "next announce: %s\n" % t.next_announce
+                out += "tracker: %s\n" % t.current_tracker
 
-            write_line(console, out)
+            console.write(out)
 
             print_peer_info(console, t.handle.get_peer_info())
             print_download_queue(console, t.handle.get_download_queue())
 
             if t.state != lt.torrent_status.seeding:
                 try:
-                    out = '\n'
+                    out = "\n"
                     fp = h.file_progress()
                     ti = t.torrent_file
                     for f, p in zip(ti.files(), fp):
                         out += progress_bar(p / float(f.size), 20)
-                        out += ' ' + f.path + '\n'
-                    write_line(console, out)
+                        out += " " + f.path + "\n"
+                    console.write(out)
                 except Exception:
                     pass
 
-        write_line(console, 76 * '-' + '\n')
-        write_line(console, '(q)uit), (p)ause), (u)npause), (r)eannounce\n')
-        write_line(console, 76 * '-' + '\n')
+        console.write(76 * "-" + "\n")
+        console.write("(q)uit), (p)ause), (u)npause), (r)eannounce\n")
+        console.write(76 * "-" + "\n")
 
         alerts = ses.pop_alerts()
         for a in alerts:
@@ -333,7 +374,7 @@ def main():
             alerts_log = alerts_log[-20:]
 
         for a in alerts_log:
-            write_line(console, a + '\n')
+            console.write(a + "\n")
 
         c = console.sleep_and_input(0.5)
 
@@ -341,15 +382,15 @@ def main():
         if not c:
             continue
 
-        if c == 'r':
+        if c == "r":
             for h in torrents:
                 h.force_reannounce()
-        elif c == 'q':
+        elif c == "q":
             alive = False
-        elif c == 'p':
+        elif c == "p":
             for h in torrents:
                 h.pause()
-        elif c == 'u':
+        elif c == "u":
             for h in torrents:
                 h.resume()
 
@@ -367,13 +408,18 @@ def main():
                 data = lt.write_resume_data_buf(a.params)
                 h = a.handle
                 if h in torrents:
-                    open(os.path.join(options.save_path, torrents[h].name + '.fastresume'), 'wb').write(data)
+                    open(
+                        os.path.join(
+                            options.save_path, torrents[h].name + ".fastresume"
+                        ),
+                        "wb",
+                    ).write(data)
                     del torrents[h]
 
             if isinstance(a, lt.save_resume_data_failed_alert):
                 h = a.handle
                 if h in torrents:
-                    print('failed to save resume data for ', torrents[h].name)
+                    print("failed to save resume data for ", torrents[h].name)
                     del torrents[h]
         time.sleep(0.5)
 
--- bindings/python/dummy_data.py
+++ bindings/python/dummy_data.py
@@ -1,34 +1,34 @@
-import libtorrent as lt
-
 import hashlib
 import random
+
+import libtorrent as lt
 
 PIECE_LENGTH = 16384
 NAME = b"test.txt"
 LEN = PIECE_LENGTH * 9 + 1000
 # Use 7-bit data so we can test piece data as either bytes or str
 DATA = bytes(random.getrandbits(7) for _ in range(LEN))
-PIECES = [DATA[i:i + PIECE_LENGTH] for i in range(0, LEN, PIECE_LENGTH)]
+PIECES = [DATA[i : i + PIECE_LENGTH] for i in range(0, LEN, PIECE_LENGTH)]
 
 INFO_DICT = {
-        b"name": NAME,
-        b"piece length": PIECE_LENGTH,
-        b"length": len(DATA),
-        b"pieces": b"".join(hashlib.sha1(p).digest() for p in PIECES),
-    }
+    b"name": NAME,
+    b"piece length": PIECE_LENGTH,
+    b"length": len(DATA),
+    b"pieces": b"".join(hashlib.sha1(p).digest() for p in PIECES),
+}
 
 DICT = {
     b"info": INFO_DICT,
 }
 
 
-def get_infohash_bytes():
+def get_infohash_bytes() -> bytes:
     return hashlib.sha1(lt.bencode(INFO_DICT)).digest()
 
 
-def get_infohash():
+def get_infohash() -> str:
     return get_infohash_bytes().hex()
 
 
-def get_sha1_hash():
+def get_sha1_hash() -> lt.sha1_hash:
     return lt.sha1_hash(get_infohash_bytes())
