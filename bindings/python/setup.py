a92505d3f564826fa99ed1d75a3c07b8c61289c9
AllSeeingEyeTolledEweSew <allseeingeyetolledewesew@protonmail.com>
2021-02-10 21:38:35 +0000

    Default distutils mode to static linking on all platforms

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -317,11 +317,6 @@ class LibtorrentBuildExt(BuildExtBase):
     def _configure_b2_with_distutils(self):
         if os.name == "nt":
             self._maybe_add_arg("--abbreviate-paths")
-            self._maybe_add_arg("boost-link=static")
-        else:
-            self._maybe_add_arg("boost-link=shared")
-
-        self._maybe_add_arg("libtorrent-link=static")
 
         if distutils.debug.DEBUG:
             self._maybe_add_arg("--debug-configuration")
@@ -330,6 +325,8 @@ class LibtorrentBuildExt(BuildExtBase):
 
         # Default feature configuration
         self._maybe_add_arg("deprecated-functions=on")
+        self._maybe_add_arg("boost-link=static")
+        self._maybe_add_arg("libtorrent-link=static")
 
         variant = "debug" if self.debug else "release"
         self._maybe_add_arg(f"variant={variant}")
