9a6f502943182db4eed704fe6b1243e3d2024516
Arvid Norberg <arvid@libtorrent.org>
2021-02-16 12:14:11 +0100

    add missing python binding for event_t

--- include/libtorrent/tracker_manager.hpp
+++ include/libtorrent/tracker_manager.hpp
@@ -86,7 +86,8 @@ namespace aux {
 
 using tracker_request_flags_t = flags::bitfield_flag<std::uint8_t, struct tracker_request_flags_tag>;
 
-// internal
+// Kinds of tracker announces. This is typically indicated as the ``&event=``
+// HTTP query string parameter to HTTP trackers.
 enum class event_t : std::uint8_t
 {
 	none,
