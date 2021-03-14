arvidn/libtorrent

commit 5c4a4a23c7d5c0179ce8ed894c40a29ea7e8e1bb

fix issue where peers entering upload-mode cannot leave upload-mode. don't mark upload-only peers as seeds, as it breaks the num_seeds accounting

src/peer_connection.cpp
@@ -776,10 +776,8 @@ namespace libtorrent {
 		m_have_piece.resize(t->torrent_file().num_pieces(), m_have_all);
 
 		if (m_have_all)
-		{
 			m_num_pieces = t->torrent_file().num_pieces();
-			m_have_piece.set_all();
-		}
+
 #if TORRENT_USE_ASSERTS
 		TORRENT_ASSERT(!m_initialized);
 		m_initialized = true;
@@ -802,6 +800,7 @@ namespace libtorrent {
 
 			// if this is a web seed. we don't have a peer_info struct
 			t->set_seed(m_peer_info, true);
+			TORRENT_ASSERT(is_seed());
 			m_upload_only = true;
 
 			t->peer_has_all(this);
@@ -815,6 +814,8 @@ namespace libtorrent {
 			disconnect_if_redundant();
 			return;
 		}
+
+		TORRENT_ASSERT(!is_seed());
 
 		// if we're a seed, we don't keep track of piece availability
 		if (t->has_picker())
@@ -2012,6 +2013,7 @@ namespace libtorrent {
 
 			t->seen_complete();
 			t->set_seed(m_peer_info, true);
+			TORRENT_ASSERT(is_seed());
 			m_upload_only = true;
 
 #if TORRENT_USE_INVARIANT_CHECKS
@@ -2106,10 +2108,11 @@ namespace libtorrent {
 			return;
 		}
 
-		bool was_seed = is_seed();
+		bool const was_seed = is_seed();
 		m_have_piece.clear_bit(index);
 		TORRENT_ASSERT(m_num_pieces > 0);
 		--m_num_pieces;
+		m_have_all = false;
 
 		// only update the piece_picker if
 		// we have the metadata and if
@@ -2120,7 +2123,10 @@ namespace libtorrent {
 		t->peer_lost(index, this);
 
 		if (was_seed)
+		{
 			t->set_seed(m_peer_info, false);
+			TORRENT_ASSERT(!is_seed());
+		}
 	}
 
 	// -----------------------------
@@ -2197,6 +2203,7 @@ namespace libtorrent {
 			m_have_piece = bits;
 			m_num_pieces = bits.count();
 			t->set_seed(m_peer_info, m_num_pieces == bits.size());
+			TORRENT_ASSERT(is_seed() == (m_num_pieces == bits.size()));
 
 #if TORRENT_USE_INVARIANT_CHECKS
 			if (t && t->has_picker())
@@ -2221,6 +2228,7 @@ namespace libtorrent {
 			m_have_piece.set_all();
 			m_num_pieces = num_pieces;
 			t->peer_has_all(this);
+			TORRENT_ASSERT(is_seed());
 
 			TORRENT_ASSERT(m_have_piece.all_set());
 			TORRENT_ASSERT(m_have_piece.count() == m_have_piece.size());
@@ -3306,6 +3314,7 @@ namespace libtorrent {
 #endif
 
 		t->set_seed(m_peer_info, true);
+		TORRENT_ASSERT(is_seed());
 		m_upload_only = true;
 		m_bitfield_received = true;
 
@@ -3374,9 +3383,12 @@ namespace libtorrent {
 
 		t->set_seed(m_peer_info, false);
 		m_bitfield_received = true;
+		m_have_all = false;
 
 		m_have_piece.clear_all();
 		m_num_pieces = 0;
+
+		TORRENT_ASSERT(!is_seed());
 
 		// if the peer is ready to download stuff, it must have metadata
 		m_has_metadata = true;
@@ -6604,6 +6616,11 @@ namespace libtorrent {
 	bool peer_connection::is_seed() const
 	{
 		TORRENT_ASSERT(is_single_thread());
+
+		// if the peer told us it has all pieces, it doesn't matter whether we
+		// have the metadata or not, this peer is a seed.
+		if (m_have_all) return true;
+
 		// if m_num_pieces == 0, we probably don't have the
 		// metadata yet.
 		std::shared_ptr<torrent> t = m_torrent.lock();
@@ -6627,11 +6644,9 @@ namespace libtorrent {
 		TORRENT_ASSERT(is_single_thread());
 		// if the peer is a seed, don't allow setting
 		// upload_only to false
-		if (m_upload_only || is_seed()) return;
+		if (m_upload_only && is_seed()) return;
 
 		m_upload_only = u;
-		std::shared_ptr<torrent> t = associated_torrent().lock();
-		t->set_seed(m_peer_info, u);
 		disconnect_if_redundant();
 	}
 

src/torrent.cpp
@@ -8112,8 +8112,18 @@ bool is_downloading_state(int const st)
 			if (p.is_connecting() && p.peer_info_struct()->seed)
 				++num_connecting_seeds;
 
-			if (p.peer_info_struct() && p.peer_info_struct()->seed)
-				++seeds;
+			if (p.peer_info_struct())
+			{
+				if (p.peer_info_struct()->seed)
+				{
+					++seeds;
+					TORRENT_ASSERT(!p.m_bitfield_received || p.is_seed());
+				}
+				else
+				{
+					TORRENT_ASSERT(!p.is_seed());
+				}
+			}
 
 			for (auto const& j : p.request_queue())
 			{
@@ -10410,18 +10420,16 @@ bool is_downloading_state(int const st)
 
 	void torrent::set_seed(torrent_peer* p, bool const s)
 	{
-		if (p->seed != s)
+		if (p->seed == s) return;
+		if (s)
 		{
-			if (s)
-			{
-				TORRENT_ASSERT(m_num_seeds < 0xffff);
-				++m_num_seeds;
-			}
-			else
-			{
-				TORRENT_ASSERT(m_num_seeds > 0);
-				--m_num_seeds;
-			}
+			TORRENT_ASSERT(m_num_seeds < 0xffff);
+			++m_num_seeds;
+		}
+		else
+		{
+			TORRENT_ASSERT(m_num_seeds > 0);
+			--m_num_seeds;
 		}
 
 		need_peer_list();

src/web_peer_connection.cpp
@@ -170,6 +170,7 @@ void web_peer_connection::on_connected()
 			for (piece_index_t k = std::get<0>(range); k < std::get<1>(range); ++k)
 				have.clear_bit(k);
 		}
+		t->set_seed(peer_info_struct(), false);
 		if (have.none_set())
 		{
 			incoming_have_none();

commit 6d26a502d9a84294593d4996f3dbfc1852f1e312

fix m_have_piece regression introduced in 5c4a4a2

src/peer_connection.cpp
@@ -776,7 +776,10 @@ namespace libtorrent {
 		m_have_piece.resize(t->torrent_file().num_pieces(), m_have_all);
 
 		if (m_have_all)
+		{
 			m_num_pieces = t->torrent_file().num_pieces();
+			m_have_piece.set_all();
+		}
 
 #if TORRENT_USE_ASSERTS
 		TORRENT_ASSERT(!m_initialized);