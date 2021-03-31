ea591337b25ef04effe125d37009ad45d1030a9e
arvidn <arvid@libtorrent.org>
2021-03-31 17:36:48 +0200

    fix link to reference-Plugins in session_handle::add_extension documentation

--- include/libtorrent/session_handle.hpp
+++ include/libtorrent/session_handle.hpp
@@ -556,7 +556,7 @@ namespace libtorrent {
 		// 	ses.add_extension(&lt::create_smart_ban_plugin);
 		//
 		//
-		// .. _`libtorrent plugins`: libtorrent_plugins.html
+		// .. _`libtorrent plugins`: reference-Plugins.html
 		void add_extension(std::function<std::shared_ptr<torrent_plugin>(
 			torrent_handle const&, void*)> ext);
 		void add_extension(std::shared_ptr<plugin> ext);
