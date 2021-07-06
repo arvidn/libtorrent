0a36e71be8da7de831c5c339de10e59107f89dc4
FranciscoPombal <FranciscoPombal@users.noreply.github.com>
2021-07-06 17:47:19 +0100

    More improvements to pkgconfig generation code
    Everything should work, including multiconfig generator and CMake <3.19

--- cmake/Modules/GeneratePkgConfig.cmake
+++ cmake/Modules/GeneratePkgConfig.cmake
@@ -1,8 +1,13 @@
 # This module provides generate_and_install_pkg_config_file() function.
-# The function takes target name and expects a fully configured project, i.e. with set version and
-# description. The function extracts interface libraries, include dirs, definitions and options
+# The function takes target name and expects a fully configured project, i.e. with set version and description.
+# The function extracts interface libraries, include dirs, definitions and compile options
 # from the target and generates pkg-config file with install() command
-# The function expands imported targets and generator expressions
+# Arbitrary generator expressions are supported in any target property.
+# A limitation of this function is that it assumes that (and will only work when)
+# the set of link libraries of the target is exclusively comprised of library target names (such as Foo::Bar),
+# and not of any other type of input accepted by target_link_libraries()
+# (i.e. full paths to libraries, plain library names, link flags, or legacy keywords)
+# This should not be an issue if the project follows modern CMake best practices.
 
 # The pkg-config generation process consists in 2 parts:
 # 1. The first part consists of 3 steps:
@@ -31,175 +36,198 @@ set(_GeneratePkGConfigDir "${CMAKE_CURRENT_LIST_DIR}/GeneratePkgConfig")
 
 include(GNUInstallDirs)
 
-function(_get_target_property_merging_configs _var_name _target_name _propert_name)
-	get_property(prop_set TARGET ${_target_name} PROPERTY ${_propert_name} SET)
-	if (prop_set)
-		get_property(vals TARGET ${_target_name} PROPERTY ${_propert_name})
-	else()
-		if (CMAKE_BUILD_TYPE)
-			list(APPEND configs ${CMAKE_BUILD_TYPE})
-		elseif (CMAKE_CONFIGURATION_TYPES)
-			list(APPEND configs ${CMAKE_CONFIGURATION_TYPES})
+# This helper function looks at the list of interface link libraries of a target
+# and returns a modified list where each generator expression related to each
+# target is replaced with a generator expression that extracts the desired
+# property from that target. Because arbitrary generator expressions may be
+# encountered during processing, CMake may trucate list elements early
+# when they comtain ';' characters (as in the case of a generator expression that can evaluate to a list)
+# Thus, care is taken to keep '<' and '>' characters balanced in each list element
+# to ensure no generator expression is broken with the following algorithm:
+# foreach() element in list,
+# 	if element is balanced w.r.t. < and >, transform and append
+# 	if not, prepend to next element, which is certain to be the next fragment of the generator expression
+# endforeach()
+#
+# OUT_LIST_VAR: variable where the output list will be stored
+# TARGET: the target whose interface link libraries will be inspected
+# LINKED_TARGETS_PROPERTY: the property that we want to get from the interface link libraries of the TARGET
+function(get_target_prop_genexps_from_interface_link_libraries)
+
+	set(options "")
+	set(oneValueArgs OUT_LIST_VAR TARGET LINKED_TARGETS_PROPERTY)
+	set(multiValueArgs "")
+
+	cmake_parse_arguments(
+		PARSE_ARGV 0
+		ARG
+		"${options}" "${oneValueArgs}" "${multiValueArgs}"
+	)
+
+	# this is so that the TARGET_PROPERTY genexp doesn't fail due to empty target
+	if (NOT TARGET dummy)
+		add_library(dummy INTERFACE)
+	endif()
+
+	get_target_property(_iface_link_libs "${ARG_TARGET}" INTERFACE_LINK_LIBRARIES)
+	foreach(_el IN LISTS _iface_link_libs)
+
+		if(_concat_flag)
+			set(_el "${_temp}${_el}")
+			set(_concat_flag OFF)
 		endif()
-		foreach (cfg ${configs})
-			string(TOUPPER "${cfg}" UPPERCFG)
-			get_property(mapped_configs TARGET ${_target_name} PROPERTY "MAP_IMPORTED_CONFIG_${UPPERCFG}")
-			if (mapped_configs)
-				list(GET "${mapped_configs}" 0 target_cfg)
-			else()
-				set(target_cfg "${UPPERCFG}")
-			endif()
-			get_property(prop_set TARGET ${_target_name} PROPERTY ${_propert_name}_${target_cfg} SET)
-			if (prop_set)
-				get_property(val_for_cfg TARGET ${_target_name} PROPERTY ${_propert_name}_${target_cfg})
-				list(APPEND vals "$<$<CONFIG:${cfg}>:${val_for_cfg}>")
-				break()
+
+		string(LENGTH "${_el}" str_len)
+		math(EXPR last_char_index "${str_len} - 1")
+		set(_open 0)
+		set(_close 0)
+
+		# iterate over string to find if < and > are balanced
+		foreach (char_index RANGE 0 ${last_char_index} 1)
+			string(SUBSTRING "${_el}" "${char_index}" "1" char)
+			if (char STREQUAL "<")
+				math(EXPR _open "${_open} + 1")
+			elseif(char STREQUAL ">")
+				math(EXPR _close "${_close} + 1")
 			endif()
 		endforeach()
-		if (NOT prop_set)
-			get_property(imported_cfgs TARGET ${_target_name} PROPERTY IMPORTED_CONFIGURATIONS)
-			# CMake docs say we can use any of the imported configs
-			list(GET imported_cfgs 0 imported_config)
-			get_property(vals TARGET ${_target_name} PROPERTY ${_propert_name}_${imported_config})
-			# remove config generator expression. Only in this case! Notice we use such expression
-			# ourselves in the loop above
-			string(REPLACE "$<$<CONFIG:${imported_config}>:" "$<1:" vals "${vals}")
+
+		if (_open EQUAL _close)
+			list(APPEND _result "$<TARGET_PROPERTY:$<IF:$<BOOL:${_el}>,${_el},dummy>,${ARG_LINKED_TARGETS_PROPERTY}>")
+		else()
+			set(_concat_flag ON)
+			set(_temp "${_el}")
+		endif()
+	endforeach()
+
+	if (NOT ARG_LINKED_TARGETS_PROPERTY STREQUAL "LOCATION")
+		get_target_property(_own_props "${ARG_TARGET}" "${ARG_LINKED_TARGETS_PROPERTY}")
+		if(_own_props)
+			list(APPEND _result "${_own_props}")
 		endif()
 	endif()
-	# HACK for static libraries cmake populates link dependencies as $<LINK_ONLY:lib_name>.
+
+	# HACK: for static libraries cmake populates link dependencies as $<LINK_ONLY:lib_name>.
 	# pkg-config does not support special handling for static libraries and as such we will remove
 	# that generator expression
-	string(REPLACE "$<LINK_ONLY:" "$<1:" vals "${vals}")
-	# HACK file(GENERATE), which we use for expanding generator expressions, is BUILD_INTERFACE,
+	string(REPLACE "$<LINK_ONLY:" "$<1:" _result "${_result}")
+	# HACK: file(GENERATE), which we use for expanding generator expressions, is BUILD_INTERFACE,
 	# but we need INSTALL_INTERFACE here.
 	# See https://gitlab.kitware.com/cmake/cmake/issues/17984
-	string(REPLACE "$<BUILD_INTERFACE:" "$<0:" vals "${vals}")
-	string(REPLACE "$<INSTALL_INTERFACE:" "${CMAKE_INSTALL_PREFIX}/$<1:" vals "${vals}")
-	set(${_var_name} "${vals}" PARENT_SCOPE)
-endfunction()
+	string(REPLACE "$<BUILD_INTERFACE:" "$<0:" _result "${_result}")
+	string(REPLACE "$<INSTALL_INTERFACE:" "${CMAKE_INSTALL_PREFIX}/$<1:" _result "${_result}")
 
-# This helper function expands imported targets from the provided targets list, collecting their
-# interface link libraries and imported locations, include directories, compile options and definitions
-# into the specified variables
-function(_expand_targets _targets _libraries_var _include_dirs_var _compile_options_var _compile_definitions_var)
-	set(_any_target_was_expanded True)
-	set(_libs "${${_libraries_var}}")
-	set(_includes "${${_include_dirs_var}}")
-	set(_defs "${${_compile_definitions_var}}")
-	set(_options "${${_compile_options_var}}")
-
-	list(APPEND _libs "${_targets}")
-
-	while (_any_target_was_expanded)
-		set(_any_target_was_expanded False)
-		set(_new_libs "")
-		foreach (_dep ${_libs})
-			if (TARGET ${_dep})
-				set(_any_target_was_expanded True)
-
-				get_target_property(_type ${_dep} TYPE)
-				if ("${_type}" STREQUAL "INTERFACE_LIBRARY")
-					# this library may not have IMPORTED_LOCATION property
-					set(_imported_location "")
-				else()
-					_get_target_property_merging_configs(_imported_location ${_dep} IMPORTED_LOCATION)
-				endif()
-
-				_get_target_property_merging_configs(_iface_link_libraries ${_dep} INTERFACE_LINK_LIBRARIES)
-				_get_target_property_merging_configs(_iface_include_dirs ${_dep} INTERFACE_INCLUDE_DIRECTORIES)
-				_get_target_property_merging_configs(_iface_compile_options ${_dep} INTERFACE_COMPILE_OPTIONS)
-				_get_target_property_merging_configs(_iface_definitions ${_dep} INTERFACE_COMPILE_DEFINITIONS)
-
-				if (_imported_location)
-					list(APPEND _new_libs "${_imported_location}")
-				endif()
-
-				if (_iface_link_libraries)
-					list(APPEND _new_libs "${_iface_link_libraries}")
-				endif()
-
-				if (_iface_include_dirs)
-					list(APPEND _includes "${_iface_include_dirs}")
-				endif()
-
-				if (_iface_compile_options)
-					list(APPEND _options "${_iface_compile_options}")
-				endif()
-
-				if (_iface_definitions)
-					list(APPEND _defs "${_iface_definitions}")
-				endif()
-
-				list(REMOVE_DUPLICATES _new_libs)
-				list(REMOVE_DUPLICATES _includes)
-				# Options order is important, thus leave duplicates as they are, skipping duplicates removal
-				list(REMOVE_DUPLICATES _defs)
-			else()
-				list(APPEND _new_libs "${_dep}")
-			endif()
-		endforeach()
-		set(_libs "${_new_libs}")
-	endwhile()
-	set(${_libraries_var} "${_libs}" PARENT_SCOPE)
-	set(${_include_dirs_var} "${_includes}" PARENT_SCOPE)
-	set(${_compile_options_var} "${_options}" PARENT_SCOPE)
-	set(${_compile_definitions_var} "${_defs}" PARENT_SCOPE)
+	set("${ARG_OUT_LIST_VAR}" "${_result}" PARENT_SCOPE)
 endfunction()
 
 # Generates and installs a pkg-config file for a given target
 function(generate_and_install_pkg_config_file _target _packageName)
-	# collect target properties
-	_expand_targets(${_target}
-		_interface_link_libraries _interface_include_dirs
-		_interface_compile_options _interface_definitions)
 
 	get_target_property(_output_name ${_target} OUTPUT_NAME)
 	if (NOT _output_name)
 		set(_output_name "${_target}")
 	endif()
-
 	set(_package_name "${_packageName}")
-
-	# remove standard include directories
-	foreach (d IN LISTS CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES)
-		list(REMOVE_ITEM _interface_include_dirs "${d}")
-	endforeach()
-
-	set(_generate_target_dir "${CMAKE_CURRENT_BINARY_DIR}/${_target}-pkgconfig")
 	set(_pkg_config_file_template_filename "${_GeneratePkGConfigDir}/pkg-config.cmake.in")
+	set(_generate_target_dir "${CMAKE_CURRENT_BINARY_DIR}/${_target}-pkgconfig")
+	set(_genexp_variables_file "${_generate_target_dir}/compile-settings.cmake")
 
-	# Since CMake 3.18 FindThreads may include a generator expression requiring a target, which gets propagated to us through INTERFACE_OPTIONS.
-	# Before CMake 3.19 there's no way to solve this in a general way, so we work around the specific case. See #4956 and CMake bug #21074.
+	# collect the necessary interface target properties
+	get_target_prop_genexps_from_interface_link_libraries(
+		OUT_LIST_VAR _interface_link_libraries
+		TARGET "${_target}"
+		LINKED_TARGETS_PROPERTY LOCATION
+	)
+
+	get_target_prop_genexps_from_interface_link_libraries(
+		OUT_LIST_VAR _interface_include_dirs
+		TARGET "${_target}"
+		LINKED_TARGETS_PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+	)
+
+	get_target_prop_genexps_from_interface_link_libraries(
+		OUT_LIST_VAR _interface_link_options
+		TARGET "${_target}"
+		LINKED_TARGETS_PROPERTY INTERFACE_LINK_OPTIONS
+	)
+
+	get_target_prop_genexps_from_interface_link_libraries(
+		OUT_LIST_VAR _interface_compile_definitions
+		TARGET "${_target}"
+		LINKED_TARGETS_PROPERTY INTERFACE_COMPILE_DEFINITIONS
+	)
+
+	get_target_prop_genexps_from_interface_link_libraries(
+		OUT_LIST_VAR _interface_compile_options
+		TARGET "${_target}"
+		LINKED_TARGETS_PROPERTY INTERFACE_COMPILE_OPTIONS
+	)
+
+	# HACK: before CMake 3.19 file(GENERATE) can't process input that contains
+	# target-dependent generator expressions, such as $<CXX_COMPILER_ID:...>
+	# or some that Threads::Threads propagates to us via its INTERFACE_COMPILE_OPTIONS.
+	# See #4956 and CMake bug #21074.
+	# For each problematic case, we must workaround manually.
 	if (CMAKE_VERSION VERSION_GREATER_EQUAL 3.19)
 		set(_target_arg TARGET ${_target})
 	else()
-		string(REPLACE "<COMPILE_LANG_AND_ID:CUDA,NVIDIA>" "<COMPILE_LANGUAGE:CUDA>" _interface_compile_options "${_interface_compile_options}")
+		string(REPLACE
+			"<COMPILE_LANG_AND_ID:CUDA,NVIDIA>"
+			"<COMPILE_LANGUAGE:CUDA>"
+			_interface_compile_options
+			"${_interface_compile_options}"
+		)
+		string(REPLACE
+			",$<CXX_COMPILER_ID:GNU>>:_GLIBCXX_DEBUG;_GLIBCXX_DEBUG_PEDANTIC>;"
+			",1>:_GLIBCXX_DEBUG;_GLIBCXX_DEBUG_PEDANTIC>;"
+			_interface_compile_definitions
+			"${_interface_compile_definitions}"
+		)
 	endif()
 
-	# put target and project properties into a file
-	configure_file("${_GeneratePkGConfigDir}/target-compile-settings.cmake.in"
-		"${_generate_target_dir}/compile-settings.cmake" @ONLY)
+	# put non-expanded target and project properties into a file
+	configure_file(
+		"${_GeneratePkGConfigDir}/target-compile-settings.cmake.in"
+		"${_genexp_variables_file}"
+		@ONLY
+	)
 
+	# generate file with expanded target and project properties
+	# and configure the script that will use the generated file to
+	# create the pkg-config file at install-time
 	get_property(_isMultiConfig GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
 	if (NOT _isMultiConfig)
-		set(_variables_file_name "${_generate_target_dir}/compile-settings-expanded.cmake")
+		set(_expanded_variables_file "${_generate_target_dir}/compile-settings-expanded.cmake")
 
-		file(GENERATE OUTPUT "${_variables_file_name}" INPUT "${_generate_target_dir}/compile-settings.cmake" ${_target_arg})
-
-		configure_file("${_GeneratePkGConfigDir}/generate-pkg-config.cmake.in"
-			"${_generate_target_dir}/generate-pkg-config.cmake" @ONLY)
+		file(GENERATE
+			OUTPUT "${_expanded_variables_file}"
+			INPUT "${_genexp_variables_file}"
+			${_target_arg}
+		)
+		configure_file(
+			"${_GeneratePkGConfigDir}/generate-pkg-config.cmake.in"
+			"${_generate_target_dir}/generate-pkg-config.cmake"
+			@ONLY
+		)
 
 		install(SCRIPT "${_generate_target_dir}/generate-pkg-config.cmake")
 	else()
 		foreach (cfg IN LISTS CMAKE_CONFIGURATION_TYPES)
-			set(_variables_file_name "${_generate_target_dir}/${cfg}/compile-settings-expanded.cmake")
+			set(_expanded_variables_file "${_generate_target_dir}/${cfg}/compile-settings-expanded.cmake")
 
-			file(GENERATE OUTPUT "${_variables_file_name}" INPUT "${_generate_target_dir}/compile-settings.cmake" CONDITION "$<CONFIG:${cfg}>" ${_target_arg})
+			file(GENERATE
+				OUTPUT "${_expanded_variables_file}"
+				INPUT "${_genexp_variables_file}"
+				CONDITION "$<CONFIG:${cfg}>"
+				${_target_arg}
+			)
+			configure_file(
+				"${_GeneratePkGConfigDir}/generate-pkg-config.cmake.in"
+				"${_generate_target_dir}/${cfg}/generate-pkg-config.cmake"
+				@ONLY
+			)
 
-			configure_file("${_GeneratePkGConfigDir}/generate-pkg-config.cmake.in"
-				"${_generate_target_dir}/${cfg}/generate-pkg-config.cmake" @ONLY)
-
-			install(SCRIPT "${_generate_target_dir}/${cfg}/generate-pkg-config.cmake")
+			install(SCRIPT "${_generate_target_dir}/$<CONFIG>/generate-pkg-config.cmake")
 		endforeach()
 	endif()
 endfunction()
