826a22c4643028581f6c64ec72c050dafeb7c621
AllSeeingEyeTolledEweSew <allseeingeyetolledewesew@protonmail.com>
2021-08-26 07:28:12 +0000

    Change python package name to libtorrent

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -448,7 +448,7 @@ class LibtorrentBuildExt(BuildExtBase):
 
 
 setuptools.setup(
-    name="python-libtorrent",
+    name="libtorrent",
     version="1.2.14",
     author="Arvid Norberg",
     author_email="arvid@libtorrent.org",
