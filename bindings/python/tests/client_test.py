a33d1b2efe483e00133eafc8296c4ed77d081904
AllSeeingEyeTolledEweSew <allseeingeyetolledewesew@protonmail.com>
2021-08-31 08:53:36 +0000

    Clean up tests for python demo scripts

--- bindings/python/tests/client_test.py
+++ bindings/python/tests/client_test.py
@@ -1,46 +1,115 @@
-
-import unittest
-import time
+import functools
+import http.server
+import logging
 import os
-import subprocess as sub
+import pathlib
+import subprocess
 import sys
+import tempfile
+import threading
+from typing import Any
+import unittest
 
+import libtorrent as lt
 
-# include terminal interface for travis parallel executions of scripts which use
-# terminal features: fix multiple stdin assignment at termios.tcgetattr
-if os.name != 'nt':
+from . import lib
+from . import tdummy
+
+# import fails on windows
+if os.name != "nt":
     import pty
 
 
-class test_example_client(unittest.TestCase):
+class Handler(http.server.BaseHTTPRequestHandler):
+    def __init__(self, *args: Any, serve_data: bytes, **kwargs: Any) -> None:
+        self.serve_data = serve_data
+        super().__init__(*args, **kwargs)
 
-    def test_execute_client(self):
-        if os.name == 'nt':
-            # TODO: fix windows includes of client.py
-            return
-        my_stdin = sys.stdin
-        if os.name != 'nt':
-            master_fd, slave_fd = pty.openpty()
-            # slave_fd fix multiple stdin assignment at termios.tcgetattr
-            my_stdin = slave_fd
+    def do_GET(self) -> None:
+        logging.info("%s", self.requestline)
+        self.send_response(200)
+        self.send_header("content-length", str(len(self.serve_data)))
+        self.end_headers()
+        self.wfile.write(self.serve_data)
 
-        process = sub.Popen(
-            [sys.executable, "client.py", "url_seed_multi.torrent"],
-            stdin=my_stdin, stdout=sub.PIPE, stderr=sub.PIPE)
-        # python2 has no Popen.wait() timeout
-        time.sleep(5)
-        returncode = process.poll()
-        if returncode is None:
-            # this is an expected use-case
-            process.kill()
-        err = process.stderr.read().decode("utf-8")
-        self.assertEqual('', err, 'process throw errors: \n' + err)
-        # check error code if process did unexpected end
-        if returncode is not None:
-            # in case of error return: output stdout if nothing was on stderr
-            if returncode != 0:
-                print("stdout:\n" + process.stdout.read().decode("utf-8"))
-            self.assertEqual(returncode, 0, "returncode: " + str(returncode) + "\n"
-                             + "stderr: empty\n"
-                             + "some configuration does not output errors like missing module members,"
-                             + "try to call it manually to get the error message\n")
+
+@unittest.skipIf(sys.platform != "linux", "pty control only works on linux")
+class TestClient(unittest.TestCase):
+    def setUp(self) -> None:
+        # path relative to this file
+        self.script_path = pathlib.Path(__file__).parent / ".." / "client.py"
+
+        self.tempdir = tempfile.TemporaryDirectory()
+        self.tempdir_path = pathlib.Path(self.tempdir.name)
+
+        self.torrent = tdummy.get_default()
+
+        # set up a web seed to serve dummy torrent data
+        self.server = http.server.HTTPServer(
+            ("127.0.0.1", 0), functools.partial(Handler, serve_data=self.torrent.data)
+        )
+        addr, port = self.server.server_address
+        self.server_thread = threading.Thread(target=self.server.serve_forever)
+        self.server_thread.start()
+
+        # construct a .torrent to point to our web seed
+        self.tdict = dict(self.torrent.dict)
+        self.tdict[b"url-list"] = b"".join(
+            [b"http://", addr.encode(), b":", str(port).encode()]
+        )
+        self.torrent_path = self.tempdir_path / "dummy.torrent"
+        self.torrent_path.write_bytes(lt.bencode(self.tdict))
+
+        self.pty_master, self.pty_slave = pty.openpty()
+
+    def tearDown(self) -> None:
+        self.server.shutdown()
+        self.server.server_close()
+        self.tempdir.cleanup()
+        os.close(self.pty_master)
+        os.close(self.pty_slave)
+
+    def test_download_from_web_seed(self) -> None:
+        proc = subprocess.Popen(
+            [
+                sys.executable,
+                str(self.script_path),
+                "--port",
+                "0",
+                "--listen-interface",
+                "127.0.0.1",
+                str(self.torrent_path),
+            ],
+            cwd=self.tempdir_path,
+            stdin=self.pty_slave,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+
+        try:
+            # wait for the file to be downloaded from the web seed
+            file_path = self.tempdir_path / os.fsdecode(self.torrent.files[0].path)
+            for _ in lib.loop_until_timeout(15):
+                try:
+                    if file_path.read_bytes() == self.torrent.data:
+                        break
+                except FileNotFoundError:
+                    pass
+
+            # send a 'q' command
+            os.write(self.pty_master, b"q")
+            # process should terminate on its own
+            returncode = proc.wait(15)
+        finally:
+            proc.kill()
+
+        # process should complete without errors or warnings
+        self.assertEqual(returncode, 0)
+        assert proc.stderr is not None  # helps mypy
+        self.assertEqual(proc.stderr.read(), "")
+        proc.stderr.close()
+
+        # fastresume should be written
+        fastresume = pathlib.Path(f"{file_path}.fastresume")
+        lt.bdecode(fastresume.read_bytes())
--- bindings/python/tests/simple_client_test.py
+++ bindings/python/tests/simple_client_test.py
@@ -1,30 +1,75 @@
-
-import unittest
-import time
-import subprocess as sub
+import functools
+import http.server
+import logging
+import os
+import pathlib
+import subprocess
 import sys
+import tempfile
+import threading
+from typing import Any
+import unittest
+
+import libtorrent as lt
+
+from . import tdummy
 
 
-class test_example_client(unittest.TestCase):
+class Handler(http.server.BaseHTTPRequestHandler):
+    def __init__(self, *args: Any, serve_data: bytes, **kwargs: Any) -> None:
+        self.serve_data = serve_data
+        super().__init__(*args, **kwargs)
 
-    def test_execute_simple_client(self):
-        process = sub.Popen(
-            [sys.executable, "simple_client.py", "url_seed_multi.torrent"],
-            stdout=sub.PIPE, stderr=sub.PIPE)
-        # python2 has no Popen.wait() timeout
-        time.sleep(5)
-        returncode = process.poll()
-        if returncode is None:
-            # this is an expected use-case
-            process.kill()
-        err = process.stderr.read().decode("utf-8")
-        self.assertEqual('', err, 'process throw errors: \n' + err)
-        # check error code if process did unexpected end
-        if returncode is not None:
-            # in case of error return: output stdout if nothing was on stderr
-            if returncode != 0:
-                print("stdout:\n" + process.stdout.read().decode("utf-8"))
-            self.assertEqual(returncode, 0, "returncode: " + str(returncode) + "\n"
-                             + "stderr: empty\n"
-                             + "some configuration does not output errors like missing module members,"
-                             + "try to call it manually to get the error message\n")
+    def do_GET(self) -> None:
+        logging.info("%s", self.requestline)
+        self.send_response(200)
+        self.send_header("content-length", str(len(self.serve_data)))
+        self.end_headers()
+        self.wfile.write(self.serve_data)
+
+
+class TestSimpleClient(unittest.TestCase):
+    def setUp(self) -> None:
+        # path relative to this file
+        self.script_path = pathlib.Path(__file__).parent / ".." / "simple_client.py"
+
+        self.tempdir = tempfile.TemporaryDirectory()
+        self.tempdir_path = pathlib.Path(self.tempdir.name)
+
+        self.torrent = tdummy.get_default()
+
+        # set up a web seed to serve dummy torrent data
+        self.server = http.server.HTTPServer(
+            ("127.0.0.1", 0), functools.partial(Handler, serve_data=self.torrent.data)
+        )
+        addr, port = self.server.server_address
+        self.server_thread = threading.Thread(target=self.server.serve_forever)
+        self.server_thread.start()
+
+        # construct a .torrent to point to our web seed
+        self.tdict = dict(self.torrent.dict)
+        self.tdict[b"url-list"] = b"".join(
+            [b"http://", addr.encode(), b":", str(port).encode()]
+        )
+        self.torrent_path = self.tempdir_path / "dummy.torrent"
+        self.torrent_path.write_bytes(lt.bencode(self.tdict))
+
+    def tearDown(self) -> None:
+        self.server.shutdown()
+        self.server.server_close()
+        self.tempdir.cleanup()
+
+    def test_download_from_web_seed(self) -> None:
+        proc = subprocess.run(
+            [sys.executable, str(self.script_path), str(self.torrent_path)],
+            cwd=self.tempdir_path,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
+            universal_newlines=True,
+            timeout=20,
+        )
+
+        self.assertEqual(proc.stderr, "")
+        file_path = self.tempdir_path / os.fsdecode(self.torrent.files[0].path)
+        self.assertEqual(file_path.read_bytes(), self.torrent.data)
