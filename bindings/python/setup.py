3c20db6a1e0ed2d57401aa7fdf82c9c5a3cdc84d
arvidn <arvid@libtorrent.org>
2021-11-29 10:07:22 +0100

    bump version to 2.0.5

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -448,7 +448,7 @@ class LibtorrentBuildExt(BuildExtBase):
 
 setuptools.setup(
     name="libtorrent",
-    version="2.0.4",
+    version="2.0.5",
     author="Arvid Norberg",
     author_email="arvid@libtorrent.org",
     description="Python bindings for libtorrent-rasterbar",
--- docs/header.rst
+++ docs/header.rst
@@ -1,1 +1,1 @@
-:Version: 2.0.4
+:Version: 2.0.5
