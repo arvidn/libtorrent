d63b21ad028516fb29730541b92ae14bfc538430
arvidn <arvid@libtorrent.org>
2021-11-29 10:07:22 +0100

    bump version to 2.0.5

--- src/settings_pack.cpp
+++ src/settings_pack.cpp
@@ -136,7 +136,7 @@ constexpr int CLOSE_FILE_INTERVAL = 0;
 		SET(proxy_username, "", &session_impl::update_proxy),
 		SET(proxy_password, "", &session_impl::update_proxy),
 		SET(i2p_hostname, "", &session_impl::update_i2p_bridge),
-		SET(peer_fingerprint, "-LT2040-", nullptr),
+		SET(peer_fingerprint, "-LT2050-", nullptr),
 		SET(dht_bootstrap_nodes, "dht.libtorrent.org:25401", &session_impl::update_dht_bootstrap_nodes)
 	}});
 
