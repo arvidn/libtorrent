80fdff450a1dcfb285172cde660e636eaacc87fe
AllSeeingEyeTolledEweSew <asetes@pm.me>
2021-12-05 19:35:42 +0000

    Normalize export_filter() output in testing

--- bindings/python/tests/ip_filter_test.py
+++ bindings/python/tests/ip_filter_test.py
@@ -1,6 +1,24 @@
+import ipaddress
+from typing import List
+from typing import Tuple
 import unittest
 
 import libtorrent as lt
+
+
+# boost's address formatting is currently inconsistent between alpine linux and
+# other environments, even when boost is built from source
+def normalize(
+    exported_filter: Tuple[List[Tuple[str, str]], List[Tuple[str, str]]]
+) -> Tuple[List[Tuple[str, str]], List[Tuple[str, str]]]:
+    def norm(addr: str) -> str:
+        return str(ipaddress.ip_address(addr))
+
+    def norm_list(filter_list: List[Tuple[str, str]]) -> List[Tuple[str, str]]:
+        return [(norm(lo), norm(hi)) for lo, hi in filter_list]
+
+    v4, v6 = exported_filter
+    return (norm_list(v4), norm_list(v6))
 
 
 class IpFilterTest(unittest.TestCase):
@@ -12,7 +30,7 @@ class IpFilterTest(unittest.TestCase):
         self.assertEqual(ipf.access("::123"), 0)
 
         self.assertEqual(
-            ipf.export_filter(),
+            normalize(ipf.export_filter()),
             (
                 [("0.0.0.0", "255.255.255.255")],
                 [("::", "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff")],
@@ -27,7 +45,7 @@ class IpFilterTest(unittest.TestCase):
         self.assertEqual(ipf.access("::123"), 0)
 
         self.assertEqual(
-            ipf.export_filter(),
+            normalize(ipf.export_filter()),
             (
                 [("0.0.0.0", "0.255.255.255"), ("1.0.0.0", "255.255.255.255")],
                 [("::", "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff")],
@@ -42,12 +60,12 @@ class IpFilterTest(unittest.TestCase):
         self.assertEqual(ipf.access("::1:0"), 0)
 
         self.assertEqual(
-            ipf.export_filter(),
+            normalize(ipf.export_filter()),
             (
                 [("0.0.0.0", "255.255.255.255")],
                 [
                     ("::", "::ffff"),
-                    ("::0.1.0.0", "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"),
+                    ("::1:0", "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"),
                 ],
             ),
         )
@@ -55,7 +73,7 @@ class IpFilterTest(unittest.TestCase):
     def test_export(self) -> None:
         ipf = lt.ip_filter()
         self.assertEqual(
-            ipf.export_filter(),
+            normalize(ipf.export_filter()),
             (
                 [("0.0.0.0", "255.255.255.255")],
                 [("::", "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff")],
@@ -66,12 +84,12 @@ class IpFilterTest(unittest.TestCase):
         ipf.add_rule("0.0.0.0", "0.255.255.255", 123)
         ipf.add_rule("::", "::ffff", 456)
         self.assertEqual(
-            ipf.export_filter(),
+            normalize(ipf.export_filter()),
             (
                 [("0.0.0.0", "0.255.255.255"), ("1.0.0.0", "255.255.255.255")],
                 [
                     ("::", "::ffff"),
-                    ("::0.1.0.0", "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"),
+                    ("::1:0", "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff"),
                 ],
             ),
         )
