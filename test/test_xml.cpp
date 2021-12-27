2f8191cd65c8cce650d43aa443ca4f0ae63e89ef
arvidn <arvid@libtorrent.org>
2021-12-27 13:09:10 +0100

    use raw strings in upnp test

--- test/test_xml.cpp
+++ test/test_xml.cpp
@@ -39,232 +39,232 @@ POSSIBILITY OF SUCH DAMAGE.
 namespace {
 
 char upnp_xml[] =
-"<root>"
-"<specVersion>"
-"<major>1</major>"
-"<minor>0</minor>"
-"</specVersion>"
-"<URLBase>http://192.168.0.1:5678</URLBase>"
-"<device>"
-"<deviceType>"
-"urn:schemas-upnp-org:device:InternetGatewayDevice:1"
-"</deviceType>"
-"<presentationURL>http://192.168.0.1:80</presentationURL>"
-"<friendlyName>D-Link Router</friendlyName>"
-"<manufacturer>D-Link</manufacturer>"
-"<manufacturerURL>http://www.dlink.com</manufacturerURL>"
-"<modelDescription>Internet Access Router</modelDescription>"
-"<modelName>D-Link Router</modelName>"
-"<UDN>uuid:upnp-InternetGatewayDevice-1_0-12345678900001</UDN>"
-"<UPC>123456789001</UPC>"
-"<serviceList>"
-"<service>"
-"<serviceType>urn:schemas-upnp-org:service:Layer3Forwarding:1</serviceType>"
-"<serviceId>urn:upnp-org:serviceId:L3Forwarding1</serviceId>"
-"<controlURL>/Layer3Forwarding</controlURL>"
-"<eventSubURL>/Layer3Forwarding</eventSubURL>"
-"<SCPDURL>/Layer3Forwarding.xml</SCPDURL>"
-"</service>"
-"</serviceList>"
-"<deviceList>"
-"<device>"
-"<deviceType>urn:schemas-upnp-org:device:WANDevice:1</deviceType>"
-"<friendlyName>WANDevice</friendlyName>"
-"<manufacturer>D-Link</manufacturer>"
-"<manufacturerURL>http://www.dlink.com</manufacturerURL>"
-"<modelDescription>Internet Access Router</modelDescription>"
-"<modelName>D-Link Router</modelName>"
-"<modelNumber>1</modelNumber>"
-"<modelURL>http://support.dlink.com</modelURL>"
-"<serialNumber>12345678900001</serialNumber>"
-"<UDN>uuid:upnp-WANDevice-1_0-12345678900001</UDN>"
-"<UPC>123456789001</UPC>"
-"<serviceList>"
-"<service>"
-"<serviceType>"
-"urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1"
-"</serviceType>"
-"<serviceId>urn:upnp-org:serviceId:WANCommonInterfaceConfig</serviceId>"
-"<controlURL>/WANCommonInterfaceConfig</controlURL>"
-"<eventSubURL>/WANCommonInterfaceConfig</eventSubURL>"
-"<SCPDURL>/WANCommonInterfaceConfig.xml</SCPDURL>"
-"</service>"
-"</serviceList>"
-"<deviceList>"
-"<device>"
-"<deviceType>urn:schemas-upnp-org:device:WANConnectionDevice:1</deviceType>"
-"<friendlyName>WAN Connection Device</friendlyName>"
-"<manufacturer>D-Link</manufacturer>"
-"<manufacturerURL>http://www.dlink.com</manufacturerURL>"
-"<modelDescription>Internet Access Router</modelDescription>"
-"<modelName>D-Link Router</modelName>"
-"<modelNumber>1</modelNumber>"
-"<modelURL>http://support.dlink.com</modelURL>"
-"<serialNumber>12345678900001</serialNumber>"
-"<UDN>uuid:upnp-WANConnectionDevice-1_0-12345678900001</UDN>"
-"<UPC>123456789001</UPC>"
-"<serviceList>"
-"<service>"
-"<serviceType>urn:schemas-upnp-org:service:WANIPConnection:1</serviceType>"
-"<serviceId>urn:upnp-org:serviceId:WANIPConnection</serviceId>"
-"<controlURL>/WANIPConnection</controlURL>"
-"<eventSubURL>/WANIPConnection</eventSubURL>"
-"<SCPDURL>/WANIPConnection.xml</SCPDURL>"
-"</service>"
-"</serviceList>"
-"</device>"
-"</deviceList>"
-"</device>"
-"</deviceList>"
-"</device>"
-"</root>";
+R"(<root>
+<specVersion>
+<major>1</major>
+<minor>0</minor>
+</specVersion>
+<URLBase>http://192.168.0.1:5678</URLBase>
+<device>
+<deviceType>
+urn:schemas-upnp-org:device:InternetGatewayDevice:1
+</deviceType>
+<presentationURL>http://192.168.0.1:80</presentationURL>
+<friendlyName>D-Link Router</friendlyName>
+<manufacturer>D-Link</manufacturer>
+<manufacturerURL>http://www.dlink.com</manufacturerURL>
+<modelDescription>Internet Access Router</modelDescription>
+<modelName>D-Link Router</modelName>
+<UDN>uuid:upnp-InternetGatewayDevice-1_0-12345678900001</UDN>
+<UPC>123456789001</UPC>
+<serviceList>
+<service>
+<serviceType>urn:schemas-upnp-org:service:Layer3Forwarding:1</serviceType>
+<serviceId>urn:upnp-org:serviceId:L3Forwarding1</serviceId>
+<controlURL>/Layer3Forwarding</controlURL>
+<eventSubURL>/Layer3Forwarding</eventSubURL>
+<SCPDURL>/Layer3Forwarding.xml</SCPDURL>
+</service>
+</serviceList>
+<deviceList>
+<device>
+<deviceType>urn:schemas-upnp-org:device:WANDevice:1</deviceType>
+<friendlyName>WANDevice</friendlyName>
+<manufacturer>D-Link</manufacturer>
+<manufacturerURL>http://www.dlink.com</manufacturerURL>
+<modelDescription>Internet Access Router</modelDescription>
+<modelName>D-Link Router</modelName>
+<modelNumber>1</modelNumber>
+<modelURL>http://support.dlink.com</modelURL>
+<serialNumber>12345678900001</serialNumber>
+<UDN>uuid:upnp-WANDevice-1_0-12345678900001</UDN>
+<UPC>123456789001</UPC>
+<serviceList>
+<service>
+<serviceType>
+urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1
+</serviceType>
+<serviceId>urn:upnp-org:serviceId:WANCommonInterfaceConfig</serviceId>
+<controlURL>/WANCommonInterfaceConfig</controlURL>
+<eventSubURL>/WANCommonInterfaceConfig</eventSubURL>
+<SCPDURL>/WANCommonInterfaceConfig.xml</SCPDURL>
+</service>
+</serviceList>
+<deviceList>
+<device>
+<deviceType>urn:schemas-upnp-org:device:WANConnectionDevice:1</deviceType>
+<friendlyName>WAN Connection Device</friendlyName>
+<manufacturer>D-Link</manufacturer>
+<manufacturerURL>http://www.dlink.com</manufacturerURL>
+<modelDescription>Internet Access Router</modelDescription>
+<modelName>D-Link Router</modelName>
+<modelNumber>1</modelNumber>
+<modelURL>http://support.dlink.com</modelURL>
+<serialNumber>12345678900001</serialNumber>
+<UDN>uuid:upnp-WANConnectionDevice-1_0-12345678900001</UDN>
+<UPC>123456789001</UPC>
+<serviceList>
+<service>
+<serviceType>urn:schemas-upnp-org:service:WANIPConnection:1</serviceType>
+<serviceId>urn:upnp-org:serviceId:WANIPConnection</serviceId>
+<controlURL>/WANIPConnection</controlURL>
+<eventSubURL>/WANIPConnection</eventSubURL>
+<SCPDURL>/WANIPConnection.xml</SCPDURL>
+</service>
+</serviceList>
+</device>
+</deviceList>
+</device>
+</deviceList>
+</device>
+</root>)";
 
 char upnp_xml2[] =
-"<root>"
-"<specVersion>"
-"<major>1</major>"
-"<minor>0</minor>"
-"</specVersion>"
-"<URLBase>http://192.168.1.1:49152</URLBase>"
-"<device>"
-"<deviceType>"
-"urn:schemas-upnp-org:device:InternetGatewayDevice:1"
-"</deviceType>"
-"<friendlyName>LINKSYS WAG200G Gateway</friendlyName>"
-"<manufacturer>LINKSYS</manufacturer>"
-"<manufacturerURL>http://www.linksys.com</manufacturerURL>"
-"<modelDescription>LINKSYS WAG200G Gateway</modelDescription>"
-"<modelName>Wireless-G ADSL Home Gateway</modelName>"
-"<modelNumber>WAG200G</modelNumber>"
-"<modelURL>http://www.linksys.com</modelURL>"
-"<serialNumber>123456789</serialNumber>"
-"<UDN>uuid:8d401597-1dd2-11b2-a7d4-001ee5947cac</UDN>"
-"<UPC>WAG200G</UPC>"
-"<serviceList>"
-"<service>"
-"<serviceType>urn:schemas-upnp-org:service:Layer3Forwarding:1</serviceType>"
-"<serviceId>urn:upnp-org:serviceId:L3Forwarding1</serviceId>"
-"<controlURL>/upnp/control/L3Forwarding1</controlURL>"
-"<eventSubURL>/upnp/event/L3Forwarding1</eventSubURL>"
-"<SCPDURL>/l3frwd.xml</SCPDURL>"
-"</service>"
-"</serviceList>"
-"<deviceList>"
-"<device>"
-"<deviceType>urn:schemas-upnp-org:device:WANDevice:1</deviceType>"
-"<friendlyName>WANDevice</friendlyName>"
-"<manufacturer>LINKSYS</manufacturer>"
-"<manufacturerURL>http://www.linksys.com/</manufacturerURL>"
-"<modelDescription>Residential Gateway</modelDescription>"
-"<modelName>Internet Connection Sharing</modelName>"
-"<modelNumber>1</modelNumber>"
-"<modelURL>http://www.linksys.com/</modelURL>"
-"<serialNumber>0000001</serialNumber>"
-"<UDN>uuid:8d401596-1dd2-11b2-a7d4-001ee5947cac</UDN>"
-"<UPC>WAG200G</UPC>"
-"<serviceList>"
-"<service>"
-"<serviceType>"
-"urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1"
-"</serviceType>"
-"<serviceId>urn:upnp-org:serviceId:WANCommonIFC1</serviceId>"
-"<controlURL>/upnp/control/WANCommonIFC1</controlURL>"
-"<eventSubURL>/upnp/event/WANCommonIFC1</eventSubURL>"
-"<SCPDURL>/cmnicfg.xml</SCPDURL>"
-"</service>"
-"</serviceList>"
-"<deviceList>"
-"<device>"
-"<deviceType>urn:schemas-upnp-org:device:WANConnectionDevice:1</deviceType>"
-"<friendlyName>WANConnectionDevice</friendlyName>"
-"<manufacturer>LINKSYS</manufacturer>"
-"<manufacturerURL>http://www.linksys.com/</manufacturerURL>"
-"<modelDescription>Residential Gateway</modelDescription>"
-"<modelName>Internet Connection Sharing</modelName>"
-"<modelNumber>1</modelNumber>"
-"<modelURL>http://www.linksys.com/</modelURL>"
-"<serialNumber>0000001</serialNumber>"
-"<UDN>uuid:8d401597-1dd2-11b2-a7d3-001ee5947cac</UDN>"
-"<UPC>WAG200G</UPC>"
-"<serviceList>"
-"<service>"
-"<serviceType>"
-"urn:schemas-upnp-org:service:WANEthernetLinkConfig:1"
-"</serviceType>"
-"<serviceId>urn:upnp-org:serviceId:WANEthLinkC1</serviceId>"
-"<controlURL>/upnp/control/WANEthLinkC1</controlURL>"
-"<eventSubURL>/upnp/event/WANEthLinkC1</eventSubURL>"
-"<SCPDURL>/wanelcfg.xml</SCPDURL>"
-"</service>"
-"<service>"
-"<serviceType>urn:schemas-upnp-org:service:WANPPPConnection:1</serviceType>"
-"<serviceId>urn:upnp-org:serviceId:WANPPPConn1</serviceId>"
-"<controlURL>/upnp/control/WANPPPConn1</controlURL>"
-"<eventSubURL>/upnp/event/WANPPPConn1</eventSubURL>"
-"<SCPDURL>/pppcfg.xml</SCPDURL>"
-"</service>"
-"</serviceList>"
-"</device>"
-"</deviceList>"
-"</device>"
-"<device>"
-"<deviceType>urn:schemas-upnp-org:device:LANDevice:1</deviceType>"
-"<friendlyName>LANDevice</friendlyName>"
-"<manufacturer>LINKSYS</manufacturer>"
-"<manufacturerURL>http://www.linksys.com/</manufacturerURL>"
-"<modelDescription>Residential Gateway</modelDescription>"
-"<modelName>Residential Gateway</modelName>"
-"<modelNumber>1</modelNumber>"
-"<modelURL>http://www.linksys.com/</modelURL>"
-"<serialNumber>0000001</serialNumber>"
-"<UDN>uuid:8d401596-1dd2-11b2-a7d3-001ee5947cac</UDN>"
-"<UPC>WAG200G</UPC>"
-"<serviceList>"
-"<service>"
-"<serviceType>"
-"urn:schemas-upnp-org:service:LANHostConfigManagement:1"
-"</serviceType>"
-"<serviceId>urn:upnp-org:serviceId:LANHostCfg1</serviceId>"
-"<controlURL>/upnp/control/LANHostCfg1</controlURL>"
-"<eventSubURL>/upnp/event/LANHostCfg1</eventSubURL>"
-"<SCPDURL>/lanhostc.xml</SCPDURL>"
-"</service>"
-"</serviceList>"
-"</device>"
-"</deviceList>"
-"<presentationURL>http://192.168.1.1/index.htm</presentationURL>"
-"</device>"
-"</root>";
+R"(<root>
+<specVersion>
+<major>1</major>
+<minor>0</minor>
+</specVersion>
+<URLBase>http://192.168.1.1:49152</URLBase>
+<device>
+<deviceType>
+urn:schemas-upnp-org:device:InternetGatewayDevice:1
+</deviceType>
+<friendlyName>LINKSYS WAG200G Gateway</friendlyName>
+<manufacturer>LINKSYS</manufacturer>
+<manufacturerURL>http://www.linksys.com</manufacturerURL>
+<modelDescription>LINKSYS WAG200G Gateway</modelDescription>
+<modelName>Wireless-G ADSL Home Gateway</modelName>
+<modelNumber>WAG200G</modelNumber>
+<modelURL>http://www.linksys.com</modelURL>
+<serialNumber>123456789</serialNumber>
+<UDN>uuid:8d401597-1dd2-11b2-a7d4-001ee5947cac</UDN>
+<UPC>WAG200G</UPC>
+<serviceList>
+<service>
+<serviceType>urn:schemas-upnp-org:service:Layer3Forwarding:1</serviceType>
+<serviceId>urn:upnp-org:serviceId:L3Forwarding1</serviceId>
+<controlURL>/upnp/control/L3Forwarding1</controlURL>
+<eventSubURL>/upnp/event/L3Forwarding1</eventSubURL>
+<SCPDURL>/l3frwd.xml</SCPDURL>
+</service>
+</serviceList>
+<deviceList>
+<device>
+<deviceType>urn:schemas-upnp-org:device:WANDevice:1</deviceType>
+<friendlyName>WANDevice</friendlyName>
+<manufacturer>LINKSYS</manufacturer>
+<manufacturerURL>http://www.linksys.com/</manufacturerURL>
+<modelDescription>Residential Gateway</modelDescription>
+<modelName>Internet Connection Sharing</modelName>
+<modelNumber>1</modelNumber>
+<modelURL>http://www.linksys.com/</modelURL>
+<serialNumber>0000001</serialNumber>
+<UDN>uuid:8d401596-1dd2-11b2-a7d4-001ee5947cac</UDN>
+<UPC>WAG200G</UPC>
+<serviceList>
+<service>
+<serviceType>
+urn:schemas-upnp-org:service:WANCommonInterfaceConfig:1
+</serviceType>
+<serviceId>urn:upnp-org:serviceId:WANCommonIFC1</serviceId>
+<controlURL>/upnp/control/WANCommonIFC1</controlURL>
+<eventSubURL>/upnp/event/WANCommonIFC1</eventSubURL>
+<SCPDURL>/cmnicfg.xml</SCPDURL>
+</service>
+</serviceList>
+<deviceList>
+<device>
+<deviceType>urn:schemas-upnp-org:device:WANConnectionDevice:1</deviceType>
+<friendlyName>WANConnectionDevice</friendlyName>
+<manufacturer>LINKSYS</manufacturer>
+<manufacturerURL>http://www.linksys.com/</manufacturerURL>
+<modelDescription>Residential Gateway</modelDescription>
+<modelName>Internet Connection Sharing</modelName>
+<modelNumber>1</modelNumber>
+<modelURL>http://www.linksys.com/</modelURL>
+<serialNumber>0000001</serialNumber>
+<UDN>uuid:8d401597-1dd2-11b2-a7d3-001ee5947cac</UDN>
+<UPC>WAG200G</UPC>
+<serviceList>
+<service>
+<serviceType>
+urn:schemas-upnp-org:service:WANEthernetLinkConfig:1
+</serviceType>
+<serviceId>urn:upnp-org:serviceId:WANEthLinkC1</serviceId>
+<controlURL>/upnp/control/WANEthLinkC1</controlURL>
+<eventSubURL>/upnp/event/WANEthLinkC1</eventSubURL>
+<SCPDURL>/wanelcfg.xml</SCPDURL>
+</service>
+<service>
+<serviceType>urn:schemas-upnp-org:service:WANPPPConnection:1</serviceType>
+<serviceId>urn:upnp-org:serviceId:WANPPPConn1</serviceId>
+<controlURL>/upnp/control/WANPPPConn1</controlURL>
+<eventSubURL>/upnp/event/WANPPPConn1</eventSubURL>
+<SCPDURL>/pppcfg.xml</SCPDURL>
+</service>
+</serviceList>
+</device>
+</deviceList>
+</device>
+<device>
+<deviceType>urn:schemas-upnp-org:device:LANDevice:1</deviceType>
+<friendlyName>LANDevice</friendlyName>
+<manufacturer>LINKSYS</manufacturer>
+<manufacturerURL>http://www.linksys.com/</manufacturerURL>
+<modelDescription>Residential Gateway</modelDescription>
+<modelName>Residential Gateway</modelName>
+<modelNumber>1</modelNumber>
+<modelURL>http://www.linksys.com/</modelURL>
+<serialNumber>0000001</serialNumber>
+<UDN>uuid:8d401596-1dd2-11b2-a7d3-001ee5947cac</UDN>
+<UPC>WAG200G</UPC>
+<serviceList>
+<service>
+<serviceType>
+urn:schemas-upnp-org:service:LANHostConfigManagement:1
+</serviceType>
+<serviceId>urn:upnp-org:serviceId:LANHostCfg1</serviceId>
+<controlURL>/upnp/control/LANHostCfg1</controlURL>
+<eventSubURL>/upnp/event/LANHostCfg1</eventSubURL>
+<SCPDURL>/lanhostc.xml</SCPDURL>
+</service>
+</serviceList>
+</device>
+</deviceList>
+<presentationURL>http://192.168.1.1/index.htm</presentationURL>
+</device>
+</root>)";
 
 char upnp_xml3[] =
-"<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\""
-" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">"
-"<s:Body>"
-"<s:Fault>"
-"<faultcode>s:Client</faultcode>"
-"<faultstring>UPnPError</faultstring>"
-"<detail>"
-"<UPnPErrorxmlns=\"urn:schemas-upnp-org:control-1-0\">"
-"<errorCode>402</errorCode>"
-"<errorDescription>Invalid Args</errorDescription>"
-"</UPnPError>"
-"</detail>"
-"</s:Fault>"
-"</s:Body>"
-"</s:Envelope>";
+R"(<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+ s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body>
+<s:Fault>
+<faultcode>s:Client</faultcode>
+<faultstring>UPnPError</faultstring>
+<detail>
+<UPnPErrorxmlns="urn:schemas-upnp-org:control-1-0">
+<errorCode>402</errorCode>
+<errorDescription>Invalid Args</errorDescription>
+</UPnPError>
+</detail>
+</s:Fault>
+</s:Body>
+</s:Envelope>)";
 
 char upnp_xml4[] =
-"<?xml version=\"1.0\"?>"
-"<s:Envelope"
-" xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\""
-" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">"
-"<s:Body>"
-"<u:GetExternalIPAddressResponse"
-" xmlns:u=\"urn:schemas-upnp-org:service:WANIPConnection:1\">"
-"<NewExternalIPAddress>123.10.20.30</NewExternalIPAddress>"
-"</u:GetExternalIPAddressResponse>"
-"</s:Body>"
-"</s:Envelope>";
+R"(<?xml version="1.0"?>
+<s:Envelope
+ xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+ s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+<s:Body>
+<u:GetExternalIPAddressResponse
+ xmlns:u="urn:schemas-upnp-org:service:WANIPConnection:1">
+<NewExternalIPAddress>123.10.20.30</NewExternalIPAddress>
+</u:GetExternalIPAddressResponse>
+</s:Body>
+</s:Envelope>)";
 
 using namespace lt;
 using namespace std::placeholders;
