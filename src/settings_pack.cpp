2fc920dfb0be3c77671977473efd50a4825f515a
arvidn <arvid@libtorrent.org>
2022-01-22 22:56:27 +0100

    deprecate (unused) volatile_read_cache setting

--- src/settings_pack.cpp
+++ src/settings_pack.cpp
@@ -174,7 +174,7 @@ constexpr int CLOSE_FILE_INTERVAL = 0;
 		SET(disable_hash_checks, false, nullptr),
 		SET(allow_i2p_mixed, false, nullptr),
 		DEPRECATED_SET(low_prio_disk, true, nullptr),
-		SET(volatile_read_cache, false, nullptr),
+		DEPRECATED2_SET(volatile_read_cache, false, nullptr),
 		DEPRECATED_SET(guided_read_cache, false, nullptr),
 		SET(no_atime_storage, true, nullptr),
 		SET(incoming_starts_queued_torrents, false, nullptr),
