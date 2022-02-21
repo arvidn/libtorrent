9f4a0f7debf30af91602eef429b83a8fd6567a79
arvidn <arvid@libtorrent.org>
2022-02-21 12:34:45 +0100

    use a namespace for unit test framework globals rather than reserved identifiers

--- simulation/setup_swarm.cpp
+++ simulation/setup_swarm.cpp
@@ -286,7 +286,7 @@ void setup_swarm(int num_nodes
 	lt::deadline_timer timer(ios);
 
 	lt::error_code ec;
-	int const swarm_id = test_counter();
+	int const swarm_id = unit_test::test_counter();
 	std::string path = save_path(swarm_id, 0);
 
 	// #error implement a storage-free version! no_storage flag
