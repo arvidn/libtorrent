2dd58cb330e9c0f498f540c3947f83f30af84296
arvidn <arvid@libtorrent.org>
2022-07-10 18:17:07 -0400

    make file_pool_view serialize opening of files, rather than race each other. When the same file is needed by multiple threads

--- include/libtorrent/aux_/file_view_pool.hpp
+++ include/libtorrent/aux_/file_view_pool.hpp
@@ -43,10 +43,12 @@ POSSIBILITY OF SUCH DAMAGE.
 #include <mutex>
 #include <vector>
 #include <memory>
+#include <condition_variable>
 
 #include "libtorrent/aux_/time.hpp"
 #include "libtorrent/units.hpp"
 #include "libtorrent/storage_defs.hpp"
+#include "libtorrent/error_code.hpp"
 #include "libtorrent/aux_/mmap.hpp"
 
 #include "libtorrent/aux_/disable_warnings_push.hpp"
@@ -57,6 +59,8 @@ POSSIBILITY OF SUCH DAMAGE.
 #include <boost/multi_index/ordered_index.hpp>
 #include <boost/multi_index/sequenced_index.hpp>
 #include <boost/multi_index/member.hpp>
+
+#include <boost/intrusive/list.hpp>
 
 #include "libtorrent/aux_/disable_warnings_pop.hpp"
 
@@ -69,7 +73,7 @@ namespace aux {
 
 	namespace mi = boost::multi_index;
 
-	TORRENT_EXTRA_EXPORT file_open_mode_t to_file_open_mode(open_mode_t const);
+	TORRENT_EXTRA_EXPORT file_open_mode_t to_file_open_mode(open_mode_t);
 
 	// this is an internal cache of open file mappings.
 	struct TORRENT_EXTRA_EXPORT file_view_pool
@@ -165,6 +169,54 @@ namespace aux {
 #endif
 			>
 		>;
+
+		struct wait_open_entry
+		{
+			boost::intrusive::list_member_hook<> list_hook;
+
+			std::condition_variable cond;
+
+			// the open file is passed back to the waiting threads, just in case
+			// the pool size is so small that it's otherwise evicted between
+			// being notified and waking up to look for it.
+			std::shared_ptr<file_mapping> mapping;
+
+			// if opening the file fails, waiters are also notified but there
+			// won't be a mapping. Then this error code is set.
+			lt::storage_error error = {};
+		};
+
+		struct opening_file_entry
+		{
+			boost::intrusive::list_member_hook<> list_hook;
+
+			file_id file_key;
+
+			// the open mode for the file the thread is opening. A thread
+			// needing a file opened in read-write mode should not wait for a
+			// thread opening the file in read mode
+			open_mode_t mode{};
+
+			boost::intrusive::list<wait_open_entry
+				, boost::intrusive::member_hook<wait_open_entry
+				, boost::intrusive::list_member_hook<>
+				, &wait_open_entry::list_hook>
+			> waiters;
+		};
+
+		void notify_file_open(opening_file_entry& ofe, std::shared_ptr<file_mapping>, lt::storage_error const&);
+
+		// In order to avoid multiple threads opening the same file in parallel,
+		// just to race to add it to the pool. This list, also protected by
+		// m_mutex, contains files that one thread is currently opening. If
+		// another thread also need this file, it can add itself to the waiters
+		// list. The condition variable will then be notified when the file has
+		// been opened.
+		boost::intrusive::list<opening_file_entry
+			, boost::intrusive::member_hook<opening_file_entry
+			, boost::intrusive::list_member_hook<>
+			, &opening_file_entry::list_hook>
+			> m_opening_files;
 
 		// maps storage pointer, file index pairs to the lru entry for the file
 		files_container m_files;
