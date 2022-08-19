e4ee0ffb6c2a18c3b7a0b809624a61d7f6306e6c
arvidn <arvid@libtorrent.org>
2022-08-19 03:06:03 +0200

    bump version to 2.1

--- include/libtorrent/version.hpp
+++ include/libtorrent/version.hpp
@@ -15,28 +15,28 @@ see LICENSE file.
 #include <cstdint>
 
 #define LIBTORRENT_VERSION_MAJOR 2
-#define LIBTORRENT_VERSION_MINOR 0
-#define LIBTORRENT_VERSION_TINY 7
+#define LIBTORRENT_VERSION_MINOR 1
+#define LIBTORRENT_VERSION_TINY 0
 
 // the format of this version is: MMmmtt
 // M = Major version, m = minor version, t = tiny version
 #define LIBTORRENT_VERSION_NUM ((LIBTORRENT_VERSION_MAJOR * 10000) + (LIBTORRENT_VERSION_MINOR * 100) + LIBTORRENT_VERSION_TINY)
 
-#define LIBTORRENT_VERSION "2.0.7.0"
-#define LIBTORRENT_REVISION "9cbecaa64"
+#define LIBTORRENT_VERSION "2.1.0.0"
+#define LIBTORRENT_REVISION "283cea598"
 
 namespace libtorrent {
 
 	// the major, minor and tiny versions of libtorrent
 	constexpr int version_major = 2;
-	constexpr int version_minor = 0;
-	constexpr int version_tiny = 7;
+	constexpr int version_minor = 1;
+	constexpr int version_tiny = 0;
 
 	// the libtorrent version in string form
-	constexpr char const* version_str = "2.0.7.0";
+	constexpr char const* version_str = "2.1.0.0";
 
 	// the git commit of this libtorrent version
-	constexpr std::uint64_t version_revision = 0x9cbecaa64;
+	constexpr std::uint64_t version_revision = 0x283cea598;
 
 	// returns the libtorrent version as string form in this format:
 	// "<major>.<minor>.<tiny>.<tag>"
