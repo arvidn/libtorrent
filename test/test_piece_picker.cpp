2154be0102f52d919f98fcd9fd0e2b4346156b09
arvidn <arvid@libtorrent.org>
2022-09-07 20:19:16 +0200

    fix wanted_done/done accounting when force-rechecking

--- test/test_piece_picker.cpp
+++ test/test_piece_picker.cpp
@@ -612,27 +612,43 @@ TORRENT_TEST(resize)
 {
 	// make sure init preserves priorities
 	auto p = setup_picker("1111111", "       ", "1111111", "");
+	p->mark_as_pad({piece_index_t(0), 1});
+	p->mark_as_pad({piece_index_t(2), 1});
+	p->mark_as_pad({piece_index_t(2), 2});
 
 	TEST_EQUAL(p->want().num_pieces, 7);
+	TEST_EQUAL(p->want().pad_blocks, 3);
 	TEST_EQUAL(p->have_want().num_pieces, 0);
+	TEST_EQUAL(p->have_want().pad_blocks, 0);
 	TEST_EQUAL(p->have().num_pieces, 0);
+	TEST_EQUAL(p->have().pad_blocks, 0);
 
 	p->set_piece_priority(piece_index_t(0), dont_download);
 	TEST_EQUAL(p->want().num_pieces, 6);
+	TEST_EQUAL(p->want().pad_blocks, 2);
 	TEST_EQUAL(p->have_want().num_pieces, 0);
+	TEST_EQUAL(p->have_want().pad_blocks, 0);
 	TEST_EQUAL(p->have().num_pieces, 0);
+	TEST_EQUAL(p->have().pad_blocks, 0);
 
 	p->we_have(piece_index_t(0));
 
 	TEST_EQUAL(p->want().num_pieces, 6);
+	TEST_EQUAL(p->want().pad_blocks, 2);
 	TEST_EQUAL(p->have_want().num_pieces, 0);
+	TEST_EQUAL(p->have_want().pad_blocks, 0);
 	TEST_EQUAL(p->have().num_pieces, 1);
+	TEST_EQUAL(p->have().pad_blocks, 1);
 
+	// the piece priority is expected to be preserved
 	p->resize(blocks_per_piece, blocks_per_piece, blocks_per_piece * 7);
 	TEST_EQUAL(p->piece_priority(piece_index_t(0)), dont_download);
 	TEST_EQUAL(p->want().num_pieces, blocks_per_piece * 7 - 1);
+	TEST_EQUAL(p->want().pad_blocks, 2);
 	TEST_EQUAL(p->have_want().num_pieces, 0);
+	TEST_EQUAL(p->have_want().pad_blocks, 0);
 	TEST_EQUAL(p->have().num_pieces, 0);
+	TEST_EQUAL(p->have().pad_blocks, 0);
 }
 
 TORRENT_TEST(we_have_all)
