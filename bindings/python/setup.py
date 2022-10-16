f72c46b527334174862ca5875bd95308d85ecb9e
arvidn <arvid@libtorrent.org>
2022-10-16 13:23:22 +0200

    bump version

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -476,7 +476,7 @@ def find_all_files(path: str) -> Iterator[str]:
 
 setuptools.setup(
     name="libtorrent",
-    version="2.0.7",
+    version="2.0.8",
     author="Arvid Norberg",
     author_email="arvid@libtorrent.org",
     description="Python bindings for libtorrent-rasterbar",
--- docs/header.rst
+++ docs/header.rst
@@ -1,1 +1,1 @@
-:Version: 2.0.7
+:Version: 2.0.8
