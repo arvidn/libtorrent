c30946fc784c49ecca2bba2c5a238ba604d838c2
thrnz <thrnz@users.noreply.github.com>
2022-10-20 08:51:03 +1300

    Fix sending ST_FIN packet while the socket is stalled
    The send_pkt(pkt_fin) call in send_fin() will fail if the socket is currently stalled. This can be picked up once the socket becomes writable again.

--- src/utp_stream.cpp
+++ src/utp_stream.cpp
@@ -1401,9 +1401,12 @@ void utp_socket_impl::writable()
 #endif
 	if (should_delete()) return;
 
+	// this handles the case where send_fin() was called while stalled
+	if (m_state == UTP_STATE_FIN_SENT && m_outbuf.at(m_seq_nr) == nullptr)
+		send_pkt(pkt_fin);
 	// if the socket stalled while sending an ack then there will be a
 	// pending deferred ack. make sure it gets sent out
-	if (!m_deferred_ack || send_pkt(pkt_ack))
+	else if (!m_deferred_ack || send_pkt(pkt_ack))
 		while(send_pkt());
 
 	maybe_trigger_send_callback();
