8e54fd979e0b2675004880433d0cc39ddc9bd14e
arvidn <arvid@libtorrent.org>
2023-02-12 13:43:08 +0100

    improve documentation clarity around i2p settings. Slightly simplify session_interface

--- include/libtorrent/aux_/session_interface.hpp
+++ include/libtorrent/aux_/session_interface.hpp
@@ -213,7 +213,6 @@ namespace aux {
 		virtual proxy_settings proxy() const = 0;
 
 #if TORRENT_USE_I2P
-		virtual proxy_settings i2p_proxy() const = 0;
 		virtual char const* i2p_session() const = 0;
 #endif
 
