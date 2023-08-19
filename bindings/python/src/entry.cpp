625514a78471d4dbe1408d3c54388f92113ce242
Chocobo1 <Chocobo1@users.noreply.github.com>
2023-08-19 12:42:00 +0800

    Remove redundant move in return statement
    This suppresses the following warning from gcc 13.2.1:
    >/home/user/libtorrent-rasterbar-2_0-git/src/libtorrent/bindings/python/src/entry.cpp:23:25: warning: redundant move in return statement [-Wredundant-move]
       23 |         return std::move(result);
          |                ~~~~~~~~~^~~~~~~~
    
    Also refer to the manual:
    https://gcc.gnu.org/onlinedocs/gcc-13.2.0/gcc/C_002b_002b-Dialect-Options.html#index-Wredundant-move

--- bindings/python/src/entry.cpp
+++ bindings/python/src/entry.cpp
@@ -20,7 +20,7 @@ struct entry_to_python
             result.append(*i);
         }
 
-        return std::move(result);
+        return result;
     }
 
     static object convert(entry::dictionary_type const& d)
@@ -30,7 +30,7 @@ struct entry_to_python
         for (entry::dictionary_type::const_iterator i(d.begin()), e(d.end()); i != e; ++i)
             result[bytes(i->first)] = i->second;
 
-        return std::move(result);
+        return result;
     }
 
     static object convert0(entry const& e)
