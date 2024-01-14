17b4b64885763c0e9b5adb1e1a2fc3a4b0b6a91c
arvidn <arvid@libtorrent.org>
2024-01-14 18:34:56 +0100

    migrate away from aligned_storage

--- include/libtorrent/aux_/aligned_storage.hpp
+++ include/libtorrent/aux_/aligned_storage.hpp
@@ -38,9 +38,10 @@ POSSIBILITY OF SUCH DAMAGE.
 
 namespace libtorrent { namespace aux {
 
-#if defined __GNUC__ && __GNUC__ < 5 && !defined(_LIBCPP_VERSION)
+#if __cplusplus >= 202302L || defined __GNUC__ && __GNUC__ < 5 && !defined(_LIBCPP_VERSION)
 
 // this is for backwards compatibility with not-quite C++11 compilers
+// and for C++23 which deprecated std::aligned_storage
 template <std::size_t Len, std::size_t Align = alignof(void*)>
 struct aligned_storage
 {
