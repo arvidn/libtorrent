023cf9a9be679e419fe78cbfea8aeff10c6c3d8e
arvidn <arvid@libtorrent.org>
2024-02-19 01:19:55 +0100

    bump version to 2.0.10

--- include/libtorrent/version.hpp
+++ include/libtorrent/version.hpp
@@ -39,27 +39,27 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #define LIBTORRENT_VERSION_MAJOR 2
 #define LIBTORRENT_VERSION_MINOR 0
-#define LIBTORRENT_VERSION_TINY 9
+#define LIBTORRENT_VERSION_TINY 10
 
 // the format of this version is: MMmmtt
 // M = Major version, m = minor version, t = tiny version
 #define LIBTORRENT_VERSION_NUM ((LIBTORRENT_VERSION_MAJOR * 10000) + (LIBTORRENT_VERSION_MINOR * 100) + LIBTORRENT_VERSION_TINY)
 
-#define LIBTORRENT_VERSION "2.0.9.0"
-#define LIBTORRENT_REVISION "559e52ca3"
+#define LIBTORRENT_VERSION "2.0.10.0"
+#define LIBTORRENT_REVISION "dacf64c50"
 
 namespace libtorrent {
 
 	// the major, minor and tiny versions of libtorrent
 	constexpr int version_major = 2;
 	constexpr int version_minor = 0;
-	constexpr int version_tiny = 9;
+	constexpr int version_tiny = 10;
 
 	// the libtorrent version in string form
-	constexpr char const* version_str = "2.0.9.0";
+	constexpr char const* version_str = "2.0.10.0";
 
 	// the git commit of this libtorrent version
-	constexpr std::uint64_t version_revision = 0x559e52ca3;
+	constexpr std::uint64_t version_revision = 0xdacf64c50;
 
 	// returns the libtorrent version as string form in this format:
 	// "<major>.<minor>.<tiny>.<tag>"
--- src/settings_pack.cpp
+++ src/settings_pack.cpp
@@ -148,7 +148,7 @@ constexpr int DISK_WRITE_MODE = settings_pack::enable_os_cache;
 		SET(proxy_username, "", &session_impl::update_proxy),
 		SET(proxy_password, "", &session_impl::update_proxy),
 		SET(i2p_hostname, "", &session_impl::update_i2p_bridge),
-		SET(peer_fingerprint, "-LT2090-", nullptr),
+		SET(peer_fingerprint, "-LT20A0-", nullptr),
 		SET(dht_bootstrap_nodes, "dht.libtorrent.org:25401", &session_impl::update_dht_bootstrap_nodes)
 	}});
 
