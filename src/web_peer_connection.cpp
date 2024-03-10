fd2dd8167d85a0e30b66b8a4610b16d9aa34023f
Andrei Kurushin <ajax16384@gmail.com>
2024-03-10 23:04:58 +0300

    add comments

--- src/web_peer_connection.cpp
+++ src/web_peer_connection.cpp
@@ -666,6 +666,8 @@ void web_peer_connection::handle_redirect(int const bytes_left)
 		file_index_t const file_index = m_file_requests.front().file_index;
 
 		location = aux::resolve_redirect_location(m_url, location);
+
+		// work-around for buggy HTTP servers, that fail to encode the redirect URL
 		location = maybe_url_encode(location);
 #ifndef TORRENT_DISABLE_LOGGING
 		peer_log(peer_log_alert::info, "LOCATION", "%s", location.c_str());
@@ -736,6 +738,8 @@ void web_peer_connection::handle_redirect(int const bytes_left)
 	else
 	{
 		location = aux::resolve_redirect_location(m_url, location);
+
+		// work-around for buggy HTTP servers, that fail to encode the redirect URL
 		location = maybe_url_encode(location);
 #ifndef TORRENT_DISABLE_LOGGING
 		peer_log(peer_log_alert::info, "LOCATION", "%s", location.c_str());
