82fda6dd5f893a8195215dea45d4081a0f9bfde8
arvidn <arvid@libtorrent.org>
2025-01-24 23:57:10 +0100

    update manylinux docker scripts

--- tools/cibuildwheel/manylinux/build-openssl.sh
+++ tools/cibuildwheel/manylinux/build-openssl.sh
@@ -8,19 +8,20 @@ set -exuo pipefail
 MY_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 # Get build utilities
-source $MY_DIR/build_utils.sh
+# shellcheck source-path=SCRIPTDIR
+source "${MY_DIR}/build_utils.sh"
 
 # Install a more recent openssl
-check_var ${OPENSSL_ROOT}
-check_var ${OPENSSL_HASH}
-check_var ${OPENSSL_DOWNLOAD_URL}
+check_var "${OPENSSL_ROOT}"
+check_var "${OPENSSL_HASH}"
+check_var "${OPENSSL_DOWNLOAD_URL}"
 
 OPENSSL_VERSION=${OPENSSL_ROOT#*-}
 OPENSSL_MIN_VERSION=1.1.1
 
 # || test $? -eq 141 is there to ignore SIGPIPE with set -o pipefail
 # c.f. https://stackoverflow.com/questions/22464786/ignoring-bash-pipefail-for-error-code-141#comment60412687_33026977
-INSTALLED=$((openssl version | head -1 || test $? -eq 141) | awk '{ print $2 }')
+INSTALLED=$( (openssl version | head -1 || test $? -eq 141) | awk '{ print $2 }')
 SMALLEST=$(echo -e "${INSTALLED}\n${OPENSSL_MIN_VERSION}" | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | head -1 || test $? -eq 141)
 
 # Ignore letters in version numbers
@@ -29,23 +30,24 @@ if [ "${SMALLEST}" = "${OPENSSL_MIN_VERSION}" ]; then
 	exit 0
 fi
 
-if which yum; then
-	yum erase -y openssl-devel
-elif which apk; then
-	apk del openssl-dev
-else
-	apt-get remove -y libssl-dev
+if [ "${OS_ID_LIKE}" = "rhel" ];then
+	manylinux_pkg_remove openssl-devel
+elif [ "${OS_ID_LIKE}" = "alpine" ]; then
+	manylinux_pkg_remove openssl-dev
 fi
 
-fetch_source ${OPENSSL_ROOT}.tar.gz ${OPENSSL_DOWNLOAD_URL}
-check_sha256sum ${OPENSSL_ROOT}.tar.gz ${OPENSSL_HASH}
-tar -xzf ${OPENSSL_ROOT}.tar.gz
-pushd ${OPENSSL_ROOT}
-./config no-shared --prefix=/usr/local/ssl --openssldir=/usr/local/ssl CPPFLAGS="${MANYLINUX_CPPFLAGS}" CFLAGS="${MANYLINUX_CFLAGS} -fPIC" CXXFLAGS="${MANYLINUX_CXXFLAGS} -fPIC" LDFLAGS="${MANYLINUX_LDFLAGS} -fPIC" > /dev/null
+PREFIX=/opt/_internal/openssl-${OPENSSL_VERSION%.*}
+
+fetch_source "${OPENSSL_ROOT}.tar.gz" "${OPENSSL_DOWNLOAD_URL}"
+check_sha256sum "${OPENSSL_ROOT}.tar.gz" "${OPENSSL_HASH}"
+tar -xzf "${OPENSSL_ROOT}.tar.gz"
+pushd "${OPENSSL_ROOT}"
+./Configure "--prefix=${PREFIX}" "--openssldir=${PREFIX}" --libdir=lib CPPFLAGS="${MANYLINUX_CPPFLAGS}" CFLAGS="${MANYLINUX_CFLAGS}" CXXFLAGS="${MANYLINUX_CXXFLAGS}" LDFLAGS="${MANYLINUX_LDFLAGS} -Wl,-rpath,\$(LIBRPATH)" > /dev/null
 make > /dev/null
 make install_sw > /dev/null
 popd
-rm -rf ${OPENSSL_ROOT} ${OPENSSL_ROOT}.tar.gz
+rm -rf "${OPENSSL_ROOT}" "${OPENSSL_ROOT}.tar.gz"
 
+strip_ "${PREFIX}"
 
-/usr/local/ssl/bin/openssl version
+"${PREFIX}/bin/openssl" version
