a250f255d964d6adcdad227d076485720e8834fc
arvidn <arvid@libtorrent.org>
2025-01-25 00:18:38 +0100

    validate add_torrent_params::save_path at run-time

--- src/error_code.cpp
+++ src/error_code.cpp
@@ -203,7 +203,7 @@ namespace libtorrent {
 			"invalid piece index in slot list",
 			"pieces needs to be reordered",
 			"fastresume not modified since last save",
-			"",
+			"invalid save_path",
 			"",
 			"",
 			"",
