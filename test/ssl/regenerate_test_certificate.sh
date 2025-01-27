5ccc5e336f8c4b2aeee909c6ad4a8d0ca0b48bec
Arvid Norberg <arvid@libtorrent.org>
2025-01-27 12:17:34 +0100

    update test certs

--- test/ssl/regenerate_test_certificate.sh
+++ test/ssl/regenerate_test_certificate.sh
@@ -2,34 +2,32 @@
 
 set -e
 
-cp peer_certificate.pem invalid_peer_certificate.pem
-cp peer_private_key.pem invalid_peer_private_key.pem
-
 printf "\n\n   ======== \e[33muse common name: * \e[0m=========\n\n"
 
-openssl req -newkey rsa:4096 -nodes -keyout server_key.pem -x509 -days 1097 -out server_cert.pem
+openssl req -newkey rsa:4096 -nodes -keyout server_key.pem -x509 -days 99999 -out server_cert.pem
 cat server_key.pem server_cert.pem >server.pem
 
-PATH=/opt/homebrew/Cellar/openssl@3/3.0.3/bin:/opt/homebrew/etc/openssl@1.1/misc:$PATH:/usr/local/etc/openssl/misc:/usr/lib/ssl/misc
-
 rm -rf demoCA
+mkdir demoCA
+mkdir demoCA/private
+mkdir demoCA/newcerts
+touch demoCA/index.txt
 
-printf "\n\n   ======== \e[33muse passphrase: \"test\" \e[0m=========\n\n"
 printf "\n\n   ======== \e[33muse common name: \"test\" \e[0m=========\n\n"
 
-CA.pl -newca
+openssl req  -new -keyout ./demoCA/private/cakey.pem -out ./demoCA/careq.pem -passout pass:test
+openssl ca  -create_serial -out ./demoCA/cacert.pem -days 99999 -batch -keyfile ./demoCA/private/cakey.pem -selfsign -extensions v3_ca -passin pass:test -infiles ./demoCA/careq.pem
 
 cp ./demoCA/cacert.pem root_ca_cert.pem
 cp ./demoCA/private/cakey.pem root_ca_private.pem
 
-printf "\n\n   ======== \e[33muse passphrase: \"test\" \e[0m=========\n\n"
 printf "\n\n   ======== \e[33muse common name: * \e[0m=========\n\n"
 
-CA.pl -newreq
+openssl req  -new  -keyout newkey.pem -out newreq.pem -passin pass:test -passout pass:test
 
 cp newkey.pem peer_private_key.pem
 
-CA.pl -sign
+openssl ca  -policy policy_anything -out newcert.pem -days 99999 -passin pass:test -infiles newreq.pem
 
 cp newcert.pem peer_certificate.pem
 
