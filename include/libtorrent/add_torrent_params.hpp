09754d7b627378cb1a215a2d964b4876ef20c35c
James Keane <james.keane.github@gmail.com>
2025-03-17 15:42:51 -0400

    Add `root_certificate` to `add_torrent_params`.
    This is so that a peer can join an ssl swarm with only
    a signed cert and infohash.

--- include/libtorrent/add_torrent_params.hpp
+++ include/libtorrent/add_torrent_params.hpp
@@ -401,6 +401,10 @@ TORRENT_VERSION_NAMESPACE_3
 		error_code internal_resume_data_error;
 #endif // TORRENT_ABI_VERSION
 
+		// if this is an SSL torrent, specify the root certificate here.
+		// This is so that an ssl torrent can be added with only an infohash
+		// and certificate.
+		std::string root_certificate;
 	};
 
 TORRENT_VERSION_NAMESPACE_3_END
