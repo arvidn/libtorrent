7879e7113cfaf3d65f4d7ce20f4b47990095c306
Tearon2020 <bbstear@aliyun.com>
2025-06-27 12:47:02 +0800

    fix:do not use boost string utils

--- src/proxy_settings.cpp
+++ src/proxy_settings.cpp
@@ -11,9 +11,6 @@ see LICENSE file.
 #include "libtorrent/settings_pack.hpp"
 #include "libtorrent/aux_/session_settings.hpp"
 
-#include <boost/algorithm/string.hpp> 
-#include <boost/algorithm/string/trim.hpp>
-
 namespace libtorrent { namespace aux {
 
 namespace {
@@ -34,17 +31,15 @@ void init(proxy_settings& p, Settings const& sett)
 	p.proxy_tracker_list_enable = sett.get_bool(
 		settings_pack::proxy_tracker_list_enable);
 
-	std::string source = sett.get_str(settings_pack::proxy_tracker_list);
-	if (source.length()>0) {
-		std::vector<std::string> trackers;
-     	boost::split(trackers, source, boost::is_any_of(";, "));
-		//tracker trim
-		for(std::string tracker : trackers) {
-			boost::trim(tracker);
+	std::string proxy_tracker_list = sett.get_str(settings_pack::proxy_tracker_list);
+	if (proxy_tracker_list.length()>0) {
+		std::stringstream ss(proxy_tracker_list);
+		std::string tracker;
+		while (std::getline(ss, tracker, ';')) {
 			if (tracker.length()>0) {
 				p.proxy_tracker_list.push_back(tracker);
 			}
-		}
+    	}
 	}
 }
 
