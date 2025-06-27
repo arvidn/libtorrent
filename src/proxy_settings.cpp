d2fa067941f2937537ee0b887aba087b8d6878d4
Tearon2020 <bbstear@aliyun.com>
2025-06-27 17:53:11 +0800

    fix:do not use boost string utils

--- src/proxy_settings.cpp
+++ src/proxy_settings.cpp
@@ -33,17 +33,15 @@ void init(proxy_settings& p, Settings const& sett)
 	p.proxy_tracker_list_enable = sett.get_bool(
 		settings_pack::proxy_tracker_list_enable);
 	
-	std::string source = sett.get_str(settings_pack::proxy_tracker_list);
-	if (source.length()>0) {
-		std::vector<std::string> trackers;
-     	boost::split(trackers, source, boost::is_any_of(";, "));
-		//tracker trim
-		for(std::string tracker : trackers) {
-			boost::trim(tracker);
+	std::string proxy_tracker_list = sett.get_str(settings_pack::proxy_tracker_list);
+	if (proxy_tracker_list.length()>0) {
+		std::stringstream ss(proxy_tracker_list);
+		std::string tracker;
+		while (std::getline(ss, tracker, ';')) {
 			if (tracker.length()>0) {
 				p.proxy_tracker_list.push_back(tracker);
 			}
-		}
+    	}
 	}
 
 }
