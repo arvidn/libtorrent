27401c9f44ed12f98874af47ba9612d8644329a1
Arvid Norberg <arvid@libtorrent.org>
2025-11-09 11:49:03 +0100

    add high_priority flag to tracker announcements, to put them at the front of the queue. This can be done manually in force_reannounce() or will happen automatically as part of connection-boost

--- src/torrent.cpp
+++ src/torrent.cpp
@@ -2976,7 +2976,7 @@ namespace {
 		};
 	}
 
-	void torrent::announce_with_tracker(event_t e)
+	void torrent::announce_with_tracker(event_t e, bool const high_priority)
 	{
 		TORRENT_ASSERT(is_single_thread());
 		TORRENT_ASSERT(e == event_t::stopped || state() != torrent_status::checking_files);
@@ -3256,6 +3256,7 @@ namespace {
 
 					req.outgoing_socket = aep.socket;
 					req.info_hash = m_torrent_file->info_hashes().get(ih);
+					if (high_priority) req.kind |= tracker_request::high_priority;
 
 #ifndef TORRENT_DISABLE_LOGGING
 					if (should_log())
@@ -3842,7 +3843,15 @@ namespace {
 			debug_log("*** found no tracker endpoints to announce");
 		}
 #endif
-		update_tracker_timer(aux::time_now32());
+
+		if (flags & torrent_handle::high_priority)
+		{
+			announce_with_tracker(event_t::none, true);
+		}
+		else
+		{
+			update_tracker_timer(aux::time_now32());
+		}
 	}
 
 #if TORRENT_ABI_VERSION == 1
@@ -10129,7 +10138,8 @@ namespace {
 
 		update_want_tick();
 
-		announce_with_tracker();
+		bool const high_priority = m_connect_boost_counter > 0;
+		announce_with_tracker(event_t::none, high_priority);
 
 		lsd_announce();
 	}
@@ -12350,7 +12360,8 @@ namespace {
 		if ((!m_abort && !is_paused() && state() != torrent_status::checking_files)
 			|| r.event == event_t::stopped)
 		{
-			announce_with_tracker(r.event);
+			// if the tracker that failed was high-priority, make the next attempt high priority as well
+			announce_with_tracker(r.event, bool(r.kind & tracker_request::high_priority));
 		}
 		else
 		{
