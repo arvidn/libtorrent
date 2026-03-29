c6119a2059216841789f0e4531460e65f90db75d
lzhzh1 <2114663250@qq.com>
2026-03-29 21:26:53 +0800

    Merge test code

--- simulation/test_allow_multiple_connections_per_pid.cpp
+++ simulation/test_allow_multiple_connections_per_pid.cpp
@@ -30,15 +30,11 @@ POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-#include <functional>
-
 #include "libtorrent/session.hpp"
 #include "libtorrent/torrent_handle.hpp"
 #include "libtorrent/settings_pack.hpp"
 #include "libtorrent/alert_types.hpp"
-#include "libtorrent/deadline_timer.hpp"
 #include "libtorrent/disabled_disk_io.hpp"
-#include "libtorrent/random.hpp"
 #include "libtorrent/torrent_flags.hpp"
 #include "settings.hpp"
 #include "fake_peer.hpp"
@@ -51,29 +47,94 @@ POSSIBILITY OF SUCH DAMAGE.
 
 namespace {
 
-struct peer_disconnects
+struct test_result
 {
-	void operator()(lt::alert const* a)
+	std::vector<lt::error_code> disconnects;
+	std::vector<lt::tcp::endpoint> connects;
+};
+
+test_result test_allow_multiple_connections_per_pid(bool allow
+	, lt::peer_id const& pid
+	, char const* peer1_ip
+	, char const* peer2_ip)
+{
+	// setup the simulation
+	sim::default_config cfg;
+	sim::simulation sim{cfg};
+	auto ios = std::make_unique<sim::asio::io_context>(sim, lt::make_address_v4("50.0.0.1"));
+	lt::session_proxy zombie;
+
+	// setup settings pack
+	lt::session_params sp;
+	sp.settings = settings();
+	sp.settings.set_int(lt::settings_pack::alert_mask
+		, lt::alert_category::all & ~lt::alert_category::stats);
+	sp.settings.set_bool(lt::settings_pack::allow_multiple_connections_per_pid, allow);
+	sp.disk_io_constructor = lt::disabled_disk_io_constructor;
+
+	// create session
+	std::shared_ptr<lt::session> ses = std::make_shared<lt::session>(sp, *ios);
+
+	// add torrent
+	lt::add_torrent_params params = ::create_torrent(0, false);
+	lt::sha1_hash const info_hash = params.ti->info_hash();
+	params.flags &= ~lt::torrent_flags::auto_managed;
+	params.flags &= ~lt::torrent_flags::paused;
+	ses->async_add_torrent(std::move(params));
+
+	// create two fake peers with the same peer-id but different IPs
+	auto peer1 = std::make_unique<fake_peer>(sim, peer1_ip);
+	auto peer2 = std::make_unique<fake_peer>(sim, peer2_ip);
+	peer1->set_peer_id(pid);
+	peer2->set_peer_id(pid);
+
+	test_result result;
+
+	// set up a timer to fire later, to shut down
+	sim::timer t2(sim, lt::seconds(5)
+		, [&](boost::system::error_code const&)
+	{
+		zombie = ses->abort();
+		ses.reset();
+	});
+
+	print_alerts(*ses, [&](lt::session&, lt::alert const* a)
 	{
 		auto* pd = lt::alert_cast<lt::peer_disconnected_alert>(a);
-		if (!pd) return;
-		alerts.push_back(pd->error);
-	}
+		if (pd) result.disconnects.push_back(pd->error);
 
-	std::vector<lt::error_code> alerts;
-};
-
-struct peer_connects
-{
-	void operator()(lt::alert const* a)
-	{
 		auto* pa = lt::alert_cast<lt::peer_connect_alert>(a);
-		if (!pa) return;
-		alerts.push_back(pa->endpoint);
-	}
+		if (pa) result.connects.push_back(pa->endpoint);
 
-	std::vector<lt::tcp::endpoint> alerts;
-};
+		if (lt::alert_cast<lt::add_torrent_alert>(a))
+		{
+			// both peers connect immediately after the torrent is added
+			peer1->connect_to(ep("50.0.0.1", 6881), info_hash);
+			peer2->connect_to(ep("50.0.0.1", 6881), info_hash);
+		}
+	});
+
+	sim.run();
+
+	return result;
+}
+
+bool has_duplicate_peer_id_error(std::vector<lt::error_code> const& errors)
+{
+	for (auto const& err : errors)
+		if (err == lt::errors::duplicate_peer_id)
+			return true;
+	return false;
+}
+
+bool has_connected(std::vector<lt::tcp::endpoint> const& endpoints
+	, lt::address_v4 const& addr)
+{
+	for (auto const& ep : endpoints)
+		if (ep.address() == addr)
+			return true;
+	return false;
+}
 
 } // anonymous namespace
 
@@ -81,147 +142,29 @@ struct peer_connects
 // a second connection from a different IP but the same peer-id should be rejected
 TORRENT_TEST(allow_multiple_connections_per_pid_false)
 {
-	// setup the simulation
-	sim::default_config cfg;
-	sim::simulation sim{cfg};
-	auto ios = std::make_unique<sim::asio::io_context>(sim, lt::make_address_v4("50.0.0.1"));
-	lt::session_proxy zombie;
+	lt::peer_id pid;
+	std::fill(pid.data(), pid.data() + 20, char(0xAA));
 
-	// setup settings pack
-	lt::session_params sp;
-	sp.settings = settings();
-	sp.settings.set_int(lt::settings_pack::alert_mask
-		, lt::alert_category::all & ~lt::alert_category::stats);
-	sp.settings.set_bool(lt::settings_pack::allow_multiple_connections_per_pid, false);
-	sp.disk_io_constructor = lt::disabled_disk_io_constructor;
-
-	// create session
-	std::shared_ptr<lt::session> ses = std::make_shared<lt::session>(sp, *ios);
-
-	// add torrent
-	lt::add_torrent_params params = ::create_torrent(0, false);
-	lt::sha1_hash const info_hash = params.ti->info_hash();
-	params.flags &= ~lt::torrent_flags::auto_managed;
-	params.flags &= ~lt::torrent_flags::paused;
-	ses->async_add_torrent(std::move(params));
-
-	// create two fake peers with the same peer-id but different IPs
-	lt::peer_id fixed_pid;
-	std::fill(fixed_pid.data(), fixed_pid.data() + 20, 0xAA);
-
-	auto peer1 = std::make_unique<fake_peer>(sim, "60.0.0.1");
-	auto peer2 = std::make_unique<fake_peer>(sim, "60.0.0.2");
-	peer1->set_peer_id(fixed_pid);
-	peer2->set_peer_id(fixed_pid);
-
-	peer_disconnects d;
-
-	// set up a timer to fire later, to shut down
-	sim::timer t2(sim, lt::seconds(5)
-		, [&](boost::system::error_code const&)
-	{
-		zombie = ses->abort();
-		ses.reset();
-	});
-
-	print_alerts(*ses, [&](lt::session&, lt::alert const* a)
-	{
-		d(a);
-
-		if (lt::alert_cast<lt::add_torrent_alert>(a))
-		{
-			// both peers connect immediately after the torrent is added
-			peer1->connect_to(ep("50.0.0.1", 6881), info_hash);
-			peer2->connect_to(ep("50.0.0.1", 6881), info_hash);
-		}
-	});
-
-	sim.run();
+	auto result = test_allow_multiple_connections_per_pid(false, pid
+		, "60.0.0.1", "60.0.0.2");
 
 	// verify that a duplicate_peer_id disconnect occurred
-	bool has_duplicate_error = false;
-	for (auto const& err : d.alerts)
-		if (err == lt::errors::duplicate_peer_id)
-			has_duplicate_error = true;
-
-	TEST_CHECK(has_duplicate_error);
+	TEST_CHECK(has_duplicate_peer_id_error(result.disconnects));
 }
 
 // allow_multiple_connections_per_pid = true:
 // a second connection from a different IP but the same peer-id should be allowed
 TORRENT_TEST(allow_multiple_connections_per_pid_true)
 {
-	// setup the simulation
-	sim::default_config cfg;
-	sim::simulation sim{cfg};
-	auto ios = std::make_unique<sim::asio::io_context>(sim, lt::make_address_v4("50.0.0.1"));
-	lt::session_proxy zombie;
+	lt::peer_id pid;
+	std::fill(pid.data(), pid.data() + 20, char(0xBB));
 
-	// setup settings pack
-	lt::session_params sp;
-	sp.settings = settings();
-	sp.settings.set_int(lt::settings_pack::alert_mask
-		, lt::alert_category::all & ~lt::alert_category::stats);
-	sp.settings.set_bool(lt::settings_pack::allow_multiple_connections_per_pid, true);
-	sp.disk_io_constructor = lt::disabled_disk_io_constructor;
-
-	// create session
-	std::shared_ptr<lt::session> ses = std::make_shared<lt::session>(sp, *ios);
-
-	// add torrent
-	lt::add_torrent_params params = ::create_torrent(0, false);
-	lt::sha1_hash const info_hash = params.ti->info_hash();
-	params.flags &= ~lt::torrent_flags::auto_managed;
-	params.flags &= ~lt::torrent_flags::paused;
-	ses->async_add_torrent(std::move(params));
-
-	// create two fake peers with the same peer-id but different IPs
-	lt::peer_id fixed_pid;
-	std::fill(fixed_pid.data(), fixed_pid.data() + 20, 0xBB);
-
-	auto peer1 = std::make_unique<fake_peer>(sim, "60.0.0.3");
-	auto peer2 = std::make_unique<fake_peer>(sim, "60.0.0.4");
-	peer1->set_peer_id(fixed_pid);
-	peer2->set_peer_id(fixed_pid);
-
-	peer_disconnects d;
-	peer_connects c;
-
-	// set up a timer to fire later, to shut down
-	sim::timer t2(sim, lt::seconds(5)
-		, [&](boost::system::error_code const&)
-	{
-		zombie = ses->abort();
-		ses.reset();
-	});
-
-	print_alerts(*ses, [&](lt::session&, lt::alert const* a)
-	{
-		d(a);
-		c(a);
-
-		if (lt::alert_cast<lt::add_torrent_alert>(a))
-		{
-			// both peers connect immediately after the torrent is added
-			peer1->connect_to(ep("50.0.0.1", 6881), info_hash);
-			peer2->connect_to(ep("50.0.0.1", 6881), info_hash);
-		}
-	});
-
-	sim.run();
-
-	// verify that the second peer (60.0.0.4) connected successfully
-	bool second_connected = false;
-	for (auto const& ep : c.alerts)
-		if (ep.address() == lt::make_address_v4("60.0.0.4"))
-			second_connected = true;
+	auto result = test_allow_multiple_connections_per_pid(true, pid
+		, "60.0.0.3", "60.0.0.4");
 
 	// verify no duplicate_peer_id error occurred
-	bool has_duplicate_error = false;
-	for (auto const& err : d.alerts)
-		if (err == lt::errors::duplicate_peer_id)
-			has_duplicate_error = true;
+	TEST_CHECK(!has_duplicate_peer_id_error(result.disconnects));
 
-	TEST_CHECK(second_connected);
-	TEST_CHECK(!has_duplicate_error);
+	// verify that the second peer connected successfully
+	TEST_CHECK(has_connected(result.connects, lt::make_address_v4("60.0.0.4")));
 }
