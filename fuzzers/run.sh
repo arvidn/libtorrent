6686b35865740b5e1931c9ea33786df9afac2b40
Arvid Norberg <arvid@libtorrent.org>
2026-05-09 09:01:16 +0200

    update the run.sh script under fuzzers to rotate over all fuzzers, or run a single one

--- fuzzers/run.sh
+++ fuzzers/run.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+JOBS=$(nproc)
+
 function run
 {
-# run for 48 hours
-nice ./fuzzers/${1} -max_total_time=172800 -timeout=10 -artifact_prefix=./${1}- corpus/${1}
+	mkdir -p corpus/${1}
+	nice ./fuzzers/${1} -max_total_time=900 -jobs=${JOBS} -timeout=10 -artifact_prefix=./${1}- corpus/${1}
 }
 
-for file in fuzzers/*; do
-	run $(basename $file) &
-done
-
-wait
+if [ $# -gt 0 ]; then
+	run "$1"
+else
+	while true; do
+		for file in fuzzers/*; do
+			run "$(basename "$file")"
+		done
+	done
+fi
