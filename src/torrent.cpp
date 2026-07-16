d843c6c017c965d77d9049ccbfe5d7134bf8e30c
Arvid Norberg <arvid@libtorrent.org>
2026-07-16 16:53:33 +0200

    an inactive torrent that becomes active starts counting against the active limit immediately, without the delay

--- src/torrent.cpp
+++ src/torrent.cpp
@@ -10456,24 +10456,26 @@ namespace {
 
 		// this section determines whether the torrent is active or not. When it
 		// changes state, it may also trigger the auto-manage logic to reconsider
-		// which torrents should be queued and started. There is a low pass
-		// filter in order to avoid flapping (auto_manage_startup).
-		bool is_inactive = is_inactive_internal();
+		// which torrents should be queued and started.
+		bool const is_inactive = is_inactive_internal();
 
 		if (settings().get_bool(settings_pack::dont_count_slow_torrents))
 		{
-			if (is_inactive != m_inactive && !m_pending_active_change)
+			if (is_inactive == bool(m_inactive))
 			{
-				int const delay = settings().get_int(settings_pack::auto_manage_startup);
-				m_inactivity_timer.expires_after(seconds(delay));
-				m_inactivity_timer.async_wait([self](error_code const& ec) {
-					self->wrap(&torrent::on_inactivity_tick, ec); });
-				m_pending_active_change = true;
+				if (m_pending_active_change) m_inactivity_timer.cancel();
 			}
-			else if (is_inactive == m_inactive
-				&& m_pending_active_change)
+			else if (!m_pending_active_change)
 			{
-				m_inactivity_timer.cancel();
+				// becoming inactive is debounced, to avoid flapping.
+				// recovering from inactive is posted immediately (0 delay)
+				int const delay =
+					is_inactive ? settings().get_int(settings_pack::auto_manage_startup) : 0;
+				m_inactivity_timer.expires_after(seconds(delay));
+				ADD_OUTSTANDING_ASYNC("torrent::on_inactivity_tick");
+				m_inactivity_timer.async_wait(
+					[self](error_code const& ec) { self->wrap(&torrent::on_inactivity_tick, ec); });
+				m_pending_active_change = true;
 			}
 		}
 
@@ -10494,6 +10496,8 @@ namespace {
 
 	void torrent::on_inactivity_tick(error_code const& ec) try
 	{
+		COMPLETE_ASYNC("torrent::on_inactivity_tick");
+
 		m_pending_active_change = false;
 
 		if (ec) return;
