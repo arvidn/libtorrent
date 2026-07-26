06bc42a6b42ec7a01100c98c6dca56e3fa1325b6
Hassan Raza <raihassanraza10@gmail.com>
2026-07-27 00:23:48 +0500

    tighten fast message ordering

--- test/test_fast_extension.cpp
+++ test/test_fast_extension.cpp
@@ -224,19 +224,6 @@ void send_have_none(tcp::socket& s)
 	if (ec) TEST_ERROR(ec.message());
 }
 
-void send_have(tcp::socket& s, int const piece)
-{
-	log("==> have: %d", piece);
-	using namespace lt::aux;
-	char msg[] = "\0\0\0\x05\x04\0\0\0\0";
-	char* ptr = msg + 5;
-	write_int32(piece, ptr);
-	error_code ec;
-	boost::asio::write(s, boost::asio::buffer(msg, 9)
-		, boost::asio::transfer_all(), ec);
-	if (ec) TEST_ERROR(ec.message());
-}
-
 void send_dht_port(tcp::socket& s, int port)
 {
 	using namespace lt::aux;
@@ -655,14 +642,13 @@ TORRENT_TEST(allowed_fast_survives_metadata)
 	piece_index_t const invalid_piece = ti->end_piece();
 	std::string bitfield(std::size_t(ti->num_pieces()), '0');
 	bitfield[std::size_t(static_cast<int>(allowed_piece))] = '1';
-	send_bitfield(s, bitfield.c_str());
 	send_allow_fast(s, static_cast<int>(allowed_piece));
 	send_allow_fast(s, static_cast<int>(invalid_piece));
-	send_have(s, 0);
+	send_bitfield(s, bitfield.c_str());
 
-	if (!wait_for_counter(*ses, "ses.num_incoming_have", 1))
+	if (!wait_for_counter(*ses, "ses.num_incoming_bitfield", 1))
 	{
-		TEST_ERROR("expected have message");
+		TEST_ERROR("expected bitfield message");
 		s.close();
 		return;
 	}
@@ -712,14 +698,13 @@ TORRENT_TEST(suggested_piece_survives_metadata)
 	std::string bitfield(std::size_t(ti->num_pieces()), '0');
 	bitfield[0] = '1';
 	bitfield[std::size_t(static_cast<int>(suggested_piece))] = '1';
-	send_bitfield(s, bitfield.c_str());
 	send_suggest_piece(s, static_cast<int>(suggested_piece));
 	send_suggest_piece(s, static_cast<int>(invalid_piece));
-	send_have(s, 0);
+	send_bitfield(s, bitfield.c_str());
 
-	if (!wait_for_counter(*ses, "ses.num_incoming_have", 1))
+	if (!wait_for_counter(*ses, "ses.num_incoming_bitfield", 1))
 	{
-		TEST_ERROR("expected have message");
+		TEST_ERROR("expected bitfield message");
 		s.close();
 		return;
 	}
