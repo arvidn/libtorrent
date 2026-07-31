76dec60228fc98383789a2baa31b95bdc0e720fa
Arvid Norberg <arvid@libtorrent.org>
2026-07-31 04:26:33 +0200

    port test_ssl.cpp to simulations (now that the simulator supports SSL

--- simulation/disk_io.hpp
+++ simulation/disk_io.hpp
@@ -20,6 +20,7 @@ see LICENSE file.
 
 #include <array>
 #include <limits>
+#include <string>
 
 std::array<char, 0x4000> generate_block_fill(lt::piece_index_t const p, int const block);
 lt::sha1_hash generate_hash1(lt::piece_index_t const p, lt::file_storage const& fs);
@@ -27,9 +28,12 @@ lt::sha1_hash generate_hash2(lt::piece_index_t p, lt::file_storage const& fs
 	, lt::span<lt::sha256_hash> const hashes);
 lt::sha256_hash generate_block_hash(lt::piece_index_t p, int offset);
 void generate_block(char* b, lt::peer_request r, int pad_bytes, int piece_size);
-lt::add_torrent_params create_test_torrent(int piece_size
-	, int num_pieces, lt::create_flags_t flags, int num_files = 1
-	, bool bad_v1_hashes = false);
+lt::add_torrent_params create_test_torrent(int piece_size,
+	int num_pieces,
+	lt::create_flags_t flags,
+	int num_files = 1,
+	bool bad_v1_hashes = false,
+	std::string const& ssl_root_cert = std::string());
 lt::add_torrent_params create_test_torrent(
 	int num_pieces, lt::create_flags_t flags, int blocks_per_piece, int num_files = 1
 	, bool bad_v1_hashes = false);
