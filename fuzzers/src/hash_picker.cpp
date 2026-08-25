60b5b4c21923a0a5f7d9ee3f5d4972dbdd2988d4
Arvid Norberg <arvid@libtorrent.org>
2026-08-25 10:02:40 +0200

    update hash_picker fuzzer to use the new file_storage API

--- fuzzers/src/hash_picker.cpp
+++ fuzzers/src/hash_picker.cpp
@@ -52,7 +52,10 @@ extern "C" int LLVMFuzzerInitialize(int*, char***)
 	int const num_pieces = 4 * 512;
 
 	g_fs.set_piece_length(piece_size);
-	g_fs.add_file_borrow({}, "test/tmp1", std::int64_t(num_pieces) * piece_size);
+	g_fs.set_name("test");
+	error_code ec;
+	g_fs.add_file(
+		ec, "tmp1", false, aux::path_element::torrent_root, std::int64_t(num_pieces) * piece_size);
 
 	char const root[32] = {};
 	g_trees.emplace_back(
