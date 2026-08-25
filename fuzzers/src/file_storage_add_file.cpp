ffaeca90b05867bc1d4dcc64c8e0a896e04d0fe5
Arvid Norberg <arvid@libtorrent.org>
2026-08-25 10:39:15 +0200

    fix file_storage_add_file.cpp fuzzer

--- fuzzers/src/file_storage_add_file.cpp
+++ fuzzers/src/file_storage_add_file.cpp
@@ -9,14 +9,17 @@ see LICENSE file.
 
 #include "libtorrent/file_storage.hpp"
 
+// add_file_borrow() is deprecated, but this fuzzer exists specifically to
+// exercise its path-string parsing (resolve_owned_directory() and friends),
+// which has no non-deprecated string-based equivalent.
+#include "libtorrent/aux_/disable_deprecation_warnings_push.hpp"
+
 extern "C" int LLVMFuzzerTestOneInput(uint8_t const* data, size_t size)
 {
 	lt::file_storage fs;
+	lt::error_code ec;
 	// we expect this call to fail sometimes
-	try {
-		fs.add_file_borrow({}, {reinterpret_cast<char const*>(data), size}, 1);
-	}
-	catch (...) {}
+	fs.add_file_borrow(ec, {}, {reinterpret_cast<char const*>(data), size}, 1);
 	return 0;
 }
 
